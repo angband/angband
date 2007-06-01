e4020f7f2a8e54b3c81f1105a85562ba43f7cf08
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2007-06-01 19:28:40 +0000

    C89 guarantees SEEK_*; we don't need to help out systems that don't anymore.
    git-svn-id: svn://rephial.org/angband/trunk@151 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/z-file.h
+++ src/z-file.h
@@ -24,19 +24,6 @@
 
 
 /*
- * Hack -- force definitions -- see fd_seek()
- */
-#ifndef SEEK_SET
-# define SEEK_SET   0
-#endif
-#ifndef SEEK_CUR
-# define SEEK_CUR   1
-#endif
-#ifndef SEEK_END
-# define SEEK_END   2
-#endif
-
-/*
  * Hack -- force definitions -- see fd_lock()  XXX
  */
 #ifndef F_UNLCK
