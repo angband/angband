b3c60ef6587a283c36f4ffd930de21b371a54864
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2007-07-04 15:46:48 +0000

    Fix message for resist poison.
    git-svn-id: svn://rephial.org/angband/trunk@263 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/xtra2.c
+++ src/xtra2.c
@@ -56,11 +56,11 @@ static timed_effect effects[] =
 	{ "You feel righteous!", "The prayer has expired.", 0, 0, PU_BONUS, MSG_BLESSED },
 	{ "Your eyes feel very sensitive!", "Your eyes feel less sensitive.", 0, 0, (PU_BONUS | PU_MONSTERS), MSG_SEE_INVIS },
 	{ "Your eyes begin to tingle!", "Your eyes stop tingling.", 0, 0, (PU_BONUS | PU_MONSTERS), MSG_INFRARED },
-	{ "You feel resistant to poison!", "You feel less resistant to poison", PR_OPPOSE_ELEMENTS, 0, 0, MSG_RES_POIS },
 	{ "", "", 0, 0, 0, 0 },  /* acid -- handled seperately */
 	{ "", "", 0, 0, 0, 0 },  /* elec -- handled seperately */
 	{ "", "", 0, 0, 0, 0 },  /* fire -- handled seperately */
 	{ "", "", 0, 0, 0, 0 },  /* cold -- handled seperately */
+	{ "You feel resistant to poison!", "You feel less resistant to poison", PR_OPPOSE_ELEMENTS, 0, 0, MSG_RES_POIS },
 	{ "You feel your memories fade.", "Your memories come flooding back.", PR_CONFUSED, 0, 0, MSG_GENERIC },
 };
 
