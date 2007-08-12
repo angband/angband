9d9fdfcea1abbb7de3ac67ab1192e7584fdcbcf5
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2007-08-12 14:25:57 +0000

    Comment up the z-msg package.  (#340)
    git-svn-id: svn://rephial.org/angband/trunk@514 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/z-msg.h
+++ src/z-msg.h
@@ -1,201 +1,254 @@
-/* z-msg.h */
-
 #ifndef INCLUDED_Z_MSG_H
 #define INCLUDED_Z_MSG_H
 
 #include "h-basic.h"
 
-/*
- * The "message memorization" package.
- *
- * Each call to "message_add(s)" will add a new "most recent" message
- * to the "message recall list", using the contents of the string "s".
- *
- * The number of memorized messages is available as "message_num()".
- *
- * Old messages can be retrieved by "message_str(age)", where the "age"
- * of the most recently memorized message is zero, and the oldest "age"
- * which is available is "message_num() - 1".  Messages outside this
- * range are returned as the empty string.
- */
-
-errr messages_init(void);
-void messages_free(void);
-u16b messages_num(void);	
-
-const char *message_str(u16b age);
-u16b message_count(u16b age);
-u16b message_type(u16b age);
-byte message_color(u16b age);
-
-byte message_type_color(u16b type);
-errr message_color_define(u16b type, byte color);
-void message_add(const char *str, u16b type);
-
-
+/*** Constants ***/
 
 
 /*** Message constants ***/
 
-#define MSG_GENERIC          0
-#define MSG_HIT              1
-#define MSG_MISS             2
-#define MSG_FLEE             3
-#define MSG_DROP             4
-#define MSG_KILL             5
-#define MSG_LEVEL            6
-#define MSG_DEATH            7
-#define MSG_STUDY            8
-#define MSG_TELEPORT         9
-#define MSG_SHOOT           10
-#define MSG_QUAFF           11
-#define MSG_ZAP_ROD         12
-#define MSG_WALK            13
-#define MSG_TPOTHER         14
-#define MSG_HITWALL         15
-#define MSG_EAT             16
-#define MSG_STORE1          17
-#define MSG_STORE2          18
-#define MSG_STORE3          19
-#define MSG_STORE4          20
-#define MSG_DIG             21
-#define MSG_OPENDOOR        22
-#define MSG_SHUTDOOR        23
-#define MSG_TPLEVEL         24
-#define MSG_BELL            25
-#define MSG_NOTHING_TO_OPEN 26
-#define MSG_LOCKPICK_FAIL   27
-#define MSG_STAIRS_DOWN     28 
-#define MSG_HITPOINT_WARN   29
-#define MSG_ACT_ARTIFACT    30 
-#define MSG_USE_STAFF       31 
-#define MSG_DESTROY         32 
-#define MSG_MON_HIT         33 
-#define MSG_MON_TOUCH       34 
-#define MSG_MON_PUNCH       35 
-#define MSG_MON_KICK        36 
-#define MSG_MON_CLAW        37 
-#define MSG_MON_BITE        38 
-#define MSG_MON_STING       39 
-#define MSG_MON_BUTT        40 
-#define MSG_MON_CRUSH       41 
-#define MSG_MON_ENGULF      42 
-#define MSG_MON_CRAWL       43 
-#define MSG_MON_DROOL       44 
-#define MSG_MON_SPIT        45 
-#define MSG_MON_GAZE        46 
-#define MSG_MON_WAIL        47 
-#define MSG_MON_SPORE       48 
-#define MSG_MON_BEG         49 
-#define MSG_MON_INSULT      50 
-#define MSG_MON_MOAN        51 
-#define MSG_RECOVER         52 
-#define MSG_BLIND           53 
-#define MSG_CONFUSED        54 
-#define MSG_POISONED        55 
-#define MSG_AFRAID          56 
-#define MSG_PARALYZED       57 
-#define MSG_DRUGGED         58 
-#define MSG_SPEED           59 
-#define MSG_SLOW            60 
-#define MSG_SHIELD          61 
-#define MSG_BLESSED         62 
-#define MSG_HERO            63 
-#define MSG_BERSERK         64 
-#define MSG_PROT_EVIL       65 
-#define MSG_INVULN          66 
-#define MSG_SEE_INVIS       67 
-#define MSG_INFRARED        68 
-#define MSG_RES_ACID        69 
-#define MSG_RES_ELEC        70 
-#define MSG_RES_FIRE        71 
-#define MSG_RES_COLD        72 
-#define MSG_RES_POIS        73 
-#define MSG_STUN            74 
-#define MSG_CUT             75 
-#define MSG_STAIRS_UP       76 
-#define MSG_STORE_ENTER     77 
-#define MSG_STORE_LEAVE     78 
-#define MSG_STORE_HOME      79 
-#define MSG_MONEY1          80 
-#define MSG_MONEY2          81 
-#define MSG_MONEY3          82 
-#define MSG_SHOOT_HIT       83 
-#define MSG_STORE5          84 
-#define MSG_LOCKPICK        85 
-#define MSG_DISARM          86 
-#define MSG_IDENT_BAD       87 
-#define MSG_IDENT_EGO       88 
-#define MSG_IDENT_ART       89 
-#define MSG_BR_ELEMENTS     90
-#define MSG_BR_FROST        91
-#define MSG_BR_ELEC         92
-#define MSG_BR_ACID         93
-#define MSG_BR_GAS          94
-#define MSG_BR_FIRE         95
-#define MSG_BR_CONF         96
-#define MSG_BR_DISENCHANT   97
-#define MSG_BR_CHAOS        98
-#define MSG_BR_SHARDS       99
-#define MSG_BR_SOUND        100
-#define MSG_BR_LIGHT        101
-#define MSG_BR_DARK         102
-#define MSG_BR_NETHER       103
-#define MSG_BR_NEXUS        104
-#define MSG_BR_TIME         105
-#define MSG_BR_INERTIA      106
-#define MSG_BR_GRAVITY      107
-#define MSG_BR_PLASMA       108
-#define MSG_BR_FORCE        109
-#define MSG_SUM_MONSTER     110
-#define MSG_SUM_ANGEL       111
-#define MSG_SUM_UNDEAD      112
-#define MSG_SUM_ANIMAL      113
-#define MSG_SUM_SPIDER      114
-#define MSG_SUM_HOUND       115
-#define MSG_SUM_HYDRA       116
-#define MSG_SUM_DEMON       117
-#define MSG_SUM_DRAGON      118
-#define MSG_SUM_HI_UNDEAD   119
-#define MSG_SUM_HI_DRAGON   120
-#define MSG_SUM_HI_DEMON    121
-#define MSG_SUM_WRAITH      122
-#define MSG_SUM_UNIQUE      123
-#define MSG_WIELD           124
-#define MSG_CURSED          125
-#define MSG_PSEUDOID        126
-#define MSG_HUNGRY          127
-#define MSG_NOTICE          128
-#define MSG_AMBIENT_DAY     129
-#define MSG_AMBIENT_NITE    130
-#define MSG_AMBIENT_DNG1    131
-#define MSG_AMBIENT_DNG2    132
-#define MSG_AMBIENT_DNG3    133
-#define MSG_AMBIENT_DNG4    134
-#define MSG_AMBIENT_DNG5    135
-#define MSG_CREATE_TRAP     136
-#define MSG_SHRIEK          137
-#define MSG_CAST_FEAR       138
-#define MSG_HIT_GOOD        139
-#define MSG_HIT_GREAT       140
-#define MSG_HIT_SUPERB      141
-#define MSG_HIT_HI_GREAT    142
-#define MSG_HIT_HI_SUPERB   143
-#define MSG_SPELL           144
-#define MSG_PRAYER          145
-#define MSG_KILL_UNIQUE     146
-#define MSG_KILL_KING       147
-#define MSG_DRAIN_STAT      148
-#define MSG_MULTIPLY        149
+enum
+{
+	MSG_GENERIC       = 0,
+	MSG_HIT           = 1,
+	MSG_MISS          = 2,
+	MSG_FLEE          = 3,
+	MSG_DROP          = 4,
+	MSG_KILL          = 5,
+	MSG_LEVEL         = 6,
+	MSG_DEATH         = 7,
+	MSG_STUDY         = 8,
+	MSG_TELEPORT      = 9,
+	MSG_SHOOT         = 10,
+	MSG_QUAFF         = 11,
+	MSG_ZAP_ROD       = 12,
+	MSG_WALK          = 13,
+	MSG_TPOTHER       = 14,
+	MSG_HITWALL       = 15,
+	MSG_EAT           = 16,
+	MSG_STORE1        = 17,
+	MSG_STORE2        = 18,
+	MSG_STORE3        = 19,
+	MSG_STORE4        = 20,
+	MSG_DIG           = 21,
+	MSG_OPENDOOR      = 22,
+	MSG_SHUTDOOR      = 23,
+	MSG_TPLEVEL       = 24,
+	MSG_BELL          = 25,
+	MSG_NOTHING_TO_OPEN = 26,
+	MSG_LOCKPICK_FAIL = 27,
+	MSG_STAIRS_DOWN   = 28,
+	MSG_HITPOINT_WARN = 29,
+	MSG_ACT_ARTIFACT  = 30,
+	MSG_USE_STAFF     = 31,
+	MSG_DESTROY       = 32,
+	MSG_MON_HIT       = 33,
+	MSG_MON_TOUCH     = 34,
+	MSG_MON_PUNCH     = 35,
+	MSG_MON_KICK      = 36,
+	MSG_MON_CLAW      = 37,
+	MSG_MON_BITE      = 38,
+	MSG_MON_STING     = 39,
+	MSG_MON_BUTT      = 40,
+	MSG_MON_CRUSH     = 41,
+	MSG_MON_ENGULF    = 42,
+	MSG_MON_CRAWL     = 43,
+	MSG_MON_DROOL     = 44,
+	MSG_MON_SPIT      = 45,
+	MSG_MON_GAZE      = 46,
+	MSG_MON_WAIL      = 47,
+	MSG_MON_SPORE     = 48,
+	MSG_MON_BEG       = 49,
+	MSG_MON_INSULT    = 50,
+	MSG_MON_MOAN      = 51,
+	MSG_RECOVER       = 52,
+	MSG_BLIND         = 53,
+	MSG_CONFUSED      = 54,
+	MSG_POISONED      = 55,
+	MSG_AFRAID        = 56,
+	MSG_PARALYZED     = 57,
+	MSG_DRUGGED       = 58,
+	MSG_SPEED         = 59,
+	MSG_SLOW          = 60,
+	MSG_SHIELD        = 61,
+	MSG_BLESSED       = 62,
+	MSG_HERO          = 63,
+	MSG_BERSERK       = 64,
+	MSG_PROT_EVIL     = 65,
+	MSG_INVULN        = 66,
+	MSG_SEE_INVIS     = 67,
+	MSG_INFRARED      = 68,
+	MSG_RES_ACID      = 69,
+	MSG_RES_ELEC      = 70,
+	MSG_RES_FIRE      = 71,
+	MSG_RES_COLD      = 72,
+	MSG_RES_POIS      = 73,
+	MSG_STUN          = 74,
+	MSG_CUT           = 75,
+	MSG_STAIRS_UP     = 76,
+	MSG_STORE_ENTER   = 77,
+	MSG_STORE_LEAVE   = 78,
+	MSG_STORE_HOME    = 79,
+	MSG_MONEY1        = 80,
+	MSG_MONEY2        = 81,
+	MSG_MONEY3        = 82,
+	MSG_SHOOT_HIT     = 83,
+	MSG_STORE5        = 84,
+	MSG_LOCKPICK      = 85,
+	MSG_DISARM        = 86,
+	MSG_IDENT_BAD     = 87,
+	MSG_IDENT_EGO     = 88,
+	MSG_IDENT_ART     = 89,
+	MSG_BR_ELEMENTS   = 90,
+	MSG_BR_FROST      = 91,
+	MSG_BR_ELEC       = 92,
+	MSG_BR_ACID       = 93,
+	MSG_BR_GAS        = 94,
+	MSG_BR_FIRE       = 95,
+	MSG_BR_CONF       = 96,
+	MSG_BR_DISENCHANT = 97,
+	MSG_BR_CHAOS      = 98,
+	MSG_BR_SHARDS     = 99,
+	MSG_BR_SOUND      = 100,
+	MSG_BR_LIGHT      = 101,
+	MSG_BR_DARK       = 102,
+	MSG_BR_NETHER     = 103,
+	MSG_BR_NEXUS      = 104,
+	MSG_BR_TIME       = 105,
+	MSG_BR_INERTIA    = 106,
+	MSG_BR_GRAVITY    = 107,
+	MSG_BR_PLASMA     = 108,
+	MSG_BR_FORCE      = 109,
+	MSG_SUM_MONSTER   = 110,
+	MSG_SUM_ANGEL     = 111,
+	MSG_SUM_UNDEAD    = 112,
+	MSG_SUM_ANIMAL    = 113,
+	MSG_SUM_SPIDER    = 114,
+	MSG_SUM_HOUND     = 115,
+	MSG_SUM_HYDRA     = 116,
+	MSG_SUM_DEMON     = 117,
+	MSG_SUM_DRAGON    = 118,
+	MSG_SUM_HI_UNDEAD = 119,
+	MSG_SUM_HI_DRAGON = 120,
+	MSG_SUM_HI_DEMON  = 121,
+	MSG_SUM_WRAITH    = 122,
+	MSG_SUM_UNIQUE    = 123,
+	MSG_WIELD         = 124,
+	MSG_CURSED        = 125,
+	MSG_PSEUDOID      = 126,
+	MSG_HUNGRY        = 127,
+	MSG_NOTICE        = 128,
+	MSG_AMBIENT_DAY   = 129,
+	MSG_AMBIENT_NITE  = 130,
+	MSG_AMBIENT_DNG1  = 131,
+	MSG_AMBIENT_DNG2  = 132,
+	MSG_AMBIENT_DNG3  = 133,
+	MSG_AMBIENT_DNG4  = 134,
+	MSG_AMBIENT_DNG5  = 135,
+	MSG_CREATE_TRAP   = 136,
+	MSG_SHRIEK        = 137,
+	MSG_CAST_FEAR     = 138,
+	MSG_HIT_GOOD      = 139,
+	MSG_HIT_GREAT     = 140,
+	MSG_HIT_SUPERB    = 141,
+	MSG_HIT_HI_GREAT  = 142,
+	MSG_HIT_HI_SUPERB = 143,
+	MSG_SPELL         = 144,
+	MSG_PRAYER        = 145,
+	MSG_KILL_UNIQUE   = 146,
+	MSG_KILL_KING     = 147,
+	MSG_DRAIN_STAT    = 148,
+	MSG_MULTIPLY      = 149,
 
-#define MSG_MAX             150
+	MSG_MAX           = 150,
+	SOUND_MAX         = MSG_MAX
+};
+
+
+/*** Functions ***/
+
+/** Initialisation/exit **/
 
 /*
- * Hack -- maximum known sounds
- *
- * Should be the same as MSG_MAX for compatibility reasons.
+ * Initialise the messages package.  Should be called before using any other
+ * functions in the package.
  */
-#define SOUND_MAX MSG_MAX
+errr messages_init(void);
+
+/*
+ * Free the message package.
+ */
+void messages_free(void);
+
+
+/** General info **/
+
+/*
+ * Return the current number of messages stored.
+ */
+u16b messages_num(void);	
+
+
+/** Individual message handling **/
+
+/*
+ * Save a new message into the memory buffer, with text `str` and type `type`.
+ * The type should be one of the MSG_ constants defined above.
+ *
+ * The new message may not be saved if it is identical to the one saved before
+ * it, in which case the "count" of the message will be increased instead.
+ * This count can be fetched using the message_count() function.
+ */
+void message_add(const char *str, u16b type);
+
+
+/*
+ * Returns the text of the message of age `age`.  The age of the most recently
+ * saved message is 0, the one before that is of age 1, etc.
+ *
+ * Returns the empty string if the no messages of the age specified are
+ * available.
+ */
+const char *message_str(u16b age);
+
+/*
+ * Returns the number of times the message of age `age` was saved. The age of
+ * the most recently saved message is 0, the one before that is of age 1, etc.
+ *
+ * In other words, if message_add() was called five times, one after the other,
+ * with the message "The orc sets your hair on fire.", then the text will only
+ * have one age (age = 0), but will have a count of 5.
+ */
+u16b message_count(u16b age);
+
+/*
+ * Returns the type of the message of age `age`.  The age of the most recently
+ * saved message is 0, the one before that is of age 1, etc.
+ *
+ * The type is one of the MSG_ constants, defined above.
+ */
+u16b message_type(u16b age);
+
+/*
+ * Returns the display colour of the message memorised `age` messages ago.
+ * (i.e. age = 0 represents the last memorised message, age = 1 is the one
+ * before that, etc).
+ */
+byte message_color(u16b age);
+
+
+/** Message type changes **/
+
+/*
+ * Returns the colour for the message type `type`.
+ */ 
+byte message_type_color(u16b type);
+
+/*
+ * Defines the color `color` for the message type `type`.
+ */
+errr message_color_define(u16b type, byte color);
 
 
 #endif /* !INCLUDED_Z_MSG_H */
