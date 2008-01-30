62a8f35fc599831b54cc62cad1e0740f793069e3
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2008-01-30 16:05:47 +0000

    Move the remaining bits of object1.c into object2.c.
    git-svn-id: svn://rephial.org/angband/trunk@666 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/object2.c
+++ src/object2.c
@@ -16,6 +16,672 @@
  *    are included in all such copies.  Other copyrights may also apply.
  */
 #include "angband.h"
+#include "randname.h"
+
+
+
+/*
+ * Hold the titles of scrolls, 6 to 14 characters each.
+ */
+char scroll_adj[MAX_TITLES][16];
+
+
+static void flavor_assign_fixed(void)
+{
+	int i, j;
+
+	for (i = 0; i < z_info->flavor_max; i++)
+	{
+		flavor_type *flavor_ptr = &flavor_info[i];
+
+		/* Skip random flavors */
+		if (flavor_ptr->sval == SV_UNKNOWN) continue;
+
+		for (j = 0; j < z_info->k_max; j++)
+		{
+			/* Skip other objects */
+			if ((k_info[j].tval == flavor_ptr->tval) &&
+			    (k_info[j].sval == flavor_ptr->sval))
+			{
+				/* Store the flavor index */
+				k_info[j].flavor = i;
+			}
+		}
+	}
+}
+
+
+static void flavor_assign_random(byte tval)
+{
+	int i, j;
+	int flavor_count = 0;
+	int choice;
+
+	/* Count the random flavors for the given tval */
+	for (i = 0; i < z_info->flavor_max; i++)
+	{
+		if ((flavor_info[i].tval == tval) &&
+		    (flavor_info[i].sval == SV_UNKNOWN))
+		{
+			flavor_count++;
+		}
+	}
+
+	for (i = 0; i < z_info->k_max; i++)
+	{
+		/* Skip other object types */
+		if (k_info[i].tval != tval) continue;
+
+		/* Skip objects that already are flavored */
+		if (k_info[i].flavor != 0) continue;
+
+		/* HACK - Ordinary food is "boring" */
+		if ((tval == TV_FOOD) && (k_info[i].sval >= SV_FOOD_MIN_FOOD))
+			continue;
+
+		if (!flavor_count) quit_fmt("Not enough flavors for tval %d.", tval);
+
+		/* Select a flavor */
+		choice = rand_int(flavor_count);
+	
+		/* Find and store the flavor */
+		for (j = 0; j < z_info->flavor_max; j++)
+		{
+			/* Skip other tvals */
+			if (flavor_info[j].tval != tval) continue;
+
+			/* Skip assigned svals */
+			if (flavor_info[j].sval != SV_UNKNOWN) continue;
+
+			if (choice == 0)
+			{
+				/* Store the flavor index */
+				k_info[i].flavor = j;
+
+				/* Mark the flavor as used */
+				flavor_info[j].sval = k_info[i].sval;
+
+				/* One less flavor to choose from */
+				flavor_count--;
+
+				break;
+			}
+
+			choice--;
+		}
+	}
+}
+
+
+/*
+ * Prepare the "variable" part of the "k_info" array.
+ *
+ * The "color"/"metal"/"type" of an item is its "flavor".
+ * For the most part, flavors are assigned randomly each game.
+ *
+ * Initialize descriptions for the "colored" objects, including:
+ * Rings, Amulets, Staffs, Wands, Rods, Food, Potions, Scrolls.
+ *
+ * The first 4 entries for potions are fixed (Water, Apple Juice,
+ * Slime Mold Juice, Unused Potion).
+ *
+ * Scroll titles are always between 6 and 14 letters long.  This is
+ * ensured because every title is composed of whole words, where every
+ * word is from 2 to 8 letters long, and that no scroll is finished
+ * until it attempts to grow beyond 15 letters.  The first time this
+ * can happen is when the current title has 6 letters and the new word
+ * has 8 letters, which would result in a 6 letter scroll title.
+ *
+ * Hack -- make sure everything stays the same for each saved game
+ * This is accomplished by the use of a saved "random seed", as in
+ * "town_gen()".  Since no other functions are called while the special
+ * seed is in effect, so this function is pretty "safe".
+ */
+void flavor_init(void)
+{
+	int i, j;
+
+	/* Hack -- Use the "simple" RNG */
+	Rand_quick = TRUE;
+
+	/* Hack -- Induce consistant flavors */
+	Rand_value = seed_flavor;
+
+	flavor_assign_fixed();
+
+	flavor_assign_random(TV_RING);
+	flavor_assign_random(TV_AMULET);
+	flavor_assign_random(TV_STAFF);
+	flavor_assign_random(TV_WAND);
+	flavor_assign_random(TV_ROD);
+	flavor_assign_random(TV_FOOD);
+	flavor_assign_random(TV_POTION);
+	flavor_assign_random(TV_SCROLL);
+
+	/* Scrolls (random titles, always white) */
+	for (i = 0; i < MAX_TITLES; i++)
+	{
+		char buf[24];
+		char *end = buf;
+		int titlelen = 0;
+		int wordlen;
+		bool okay = TRUE;
+
+		wordlen = randname_make(RANDNAME_SCROLL, 2, 8, end, 24);
+		while (titlelen + wordlen < (int)(sizeof(scroll_adj[0]) - 1))
+		{
+			end[wordlen] = ' ';
+			titlelen += wordlen + 1;
+			end += wordlen + 1;
+			wordlen = randname_make(RANDNAME_SCROLL, 2, 8, end, 24 - titlelen);
+		}
+		buf[titlelen - 1] = '\0';
+
+		/* Check the scroll name hasn't already been generated */
+		for (j = 0; j < i; j++)
+		{
+			if (streq(buf, scroll_adj[j]))
+			{
+				okay = FALSE;
+				break;
+			}
+		}
+
+		if (okay)
+		{
+			my_strcpy(scroll_adj[i], buf, sizeof(scroll_adj[0]));
+		}
+		else
+		{
+			/* Have another go at making a name */
+			i--;
+		}
+	}
+
+	/* Hack -- Use the "complex" RNG */
+	Rand_quick = FALSE;
+
+	/* Analyze every object */
+	for (i = 1; i < z_info->k_max; i++)
+	{
+		object_kind *k_ptr = &k_info[i];
+
+		/* Skip "empty" objects */
+		if (!k_ptr->name) continue;
+
+		/* No flavor yields aware */
+		if (!k_ptr->flavor) k_ptr->aware = TRUE;
+	}
+}
+
+
+
+#ifdef ALLOW_BORG_GRAPHICS
+extern void init_translate_visuals(void);
+#endif /* ALLOW_BORG_GRAPHICS */
+
+
+/*
+ * Reset the "visual" lists
+ *
+ * This involves resetting various things to their "default" state.
+ *
+ * If the "prefs" flag is TRUE, then we will also load the appropriate
+ * "user pref file" based on the current setting of the "use_graphics"
+ * flag.  This is useful for switching "graphics" on/off.
+ *
+ * The features, objects, and monsters, should all be encoded in the
+ * relevant "font.pref" and/or "graf.prf" files.  XXX XXX XXX
+ *
+ * The "prefs" parameter is no longer meaningful.  XXX XXX XXX
+ */
+void reset_visuals(bool unused)
+{
+	int i;
+
+
+	/* Unused parameter */
+	(void)unused;
+
+	/* Extract default attr/char code for features */
+	for (i = 0; i < z_info->f_max; i++)
+	{
+		feature_type *f_ptr = &f_info[i];
+
+		/* Assume we will use the underlying values */
+		f_ptr->x_attr = f_ptr->d_attr;
+		f_ptr->x_char = f_ptr->d_char;
+	}
+
+	/* Extract default attr/char code for objects */
+	for (i = 0; i < z_info->k_max; i++)
+	{
+		object_kind *k_ptr = &k_info[i];
+
+		/* Default attr/char */
+		k_ptr->x_attr = k_ptr->d_attr;
+		k_ptr->x_char = k_ptr->d_char;
+	}
+
+	/* Extract default attr/char code for monsters */
+	for (i = 0; i < z_info->r_max; i++)
+	{
+		monster_race *r_ptr = &r_info[i];
+
+		/* Default attr/char */
+		r_ptr->x_attr = r_ptr->d_attr;
+		r_ptr->x_char = r_ptr->d_char;
+	}
+
+	/* Extract default attr/char code for flavors */
+	for (i = 0; i < z_info->flavor_max; i++)
+	{
+		flavor_type *flavor_ptr = &flavor_info[i];
+
+		/* Default attr/char */
+		flavor_ptr->x_attr = flavor_ptr->d_attr;
+		flavor_ptr->x_char = flavor_ptr->d_char;
+	}
+
+	/* Extract attr/chars for inventory objects (by tval) */
+	for (i = 0; i < (int)N_ELEMENTS(tval_to_attr); i++)
+	{
+		/* Default to white */
+		tval_to_attr[i] = TERM_WHITE;
+	}
+
+
+	/* Graphic symbols */
+	if (use_graphics)
+	{
+		/* Process "graf.prf" */
+		process_pref_file("graf.prf");
+	}
+
+	/* Normal symbols */
+	else
+	{
+		/* Process "font.prf" */
+		process_pref_file("font.prf");
+	}
+
+#ifdef ALLOW_BORG_GRAPHICS
+	/* Initialize the translation table for the borg */
+	init_translate_visuals();
+#endif /* ALLOW_BORG_GRAPHICS */
+}
+
+
+/*
+ * Obtain the "flags" for an item
+ */
+void object_flags(const object_type *o_ptr, u32b *f1, u32b *f2, u32b *f3)
+{
+	object_kind *k_ptr = &k_info[o_ptr->k_idx];
+
+	(*f1) = k_ptr->flags1;
+	(*f2) = k_ptr->flags2;
+	(*f3) = k_ptr->flags3;
+
+	if (o_ptr->name1)
+	{
+		artifact_type *a_ptr = &a_info[o_ptr->name1];
+
+		(*f1) = a_ptr->flags1;
+		(*f2) = a_ptr->flags2;
+		(*f3) = a_ptr->flags3;
+	}
+
+	if (o_ptr->name2)
+	{
+		ego_item_type *e_ptr = &e_info[o_ptr->name2];
+
+		(*f1) |= e_ptr->flags1;
+		(*f2) |= e_ptr->flags2;
+		(*f3) |= e_ptr->flags3;
+	}
+
+	(*f1) |= o_ptr->flags1;
+	(*f2) |= o_ptr->flags2;
+	(*f3) |= o_ptr->flags3;
+}
+
+
+/*
+ * Obtain the "flags" for an item which are known to the player
+ */
+void object_flags_known(const object_type *o_ptr, u32b *f1, u32b *f2, u32b *f3)
+{
+	if (!object_known_p(o_ptr))
+	{
+		(*f1) = (*f2) = (*f3) = 0L;
+		return;
+	}
+
+	object_flags(o_ptr, f1, f2, f3);
+}
+
+
+
+/*
+ * Convert an inventory index into a one character label.
+ *
+ * Note that the label does NOT distinguish inven/equip.
+ */
+char index_to_label(int i)
+{
+	/* Indexes for "inven" are easy */
+	if (i < INVEN_WIELD) return (I2A(i));
+
+	/* Indexes for "equip" are offset */
+	return (I2A(i - INVEN_WIELD));
+}
+
+
+/*
+ * Convert a label into the index of an item in the "inven".
+ *
+ * Return "-1" if the label does not indicate a real item.
+ */
+s16b label_to_inven(int c)
+{
+	int i;
+
+	/* Convert */
+	i = (islower((unsigned char)c) ? A2I(c) : -1);
+
+	/* Verify the index */
+	if ((i < 0) || (i > INVEN_PACK)) return (-1);
+
+	/* Empty slots can never be chosen */
+	if (!inventory[i].k_idx) return (-1);
+
+	/* Return the index */
+	return (i);
+}
+
+
+/*
+ * Convert a label into the index of a item in the "equip".
+ *
+ * Return "-1" if the label does not indicate a real item.
+ */
+s16b label_to_equip(int c)
+{
+	int i;
+
+	/* Convert */
+	i = (islower((unsigned char)c) ? A2I(c) : -1) + INVEN_WIELD;
+
+	/* Verify the index */
+	if ((i < INVEN_WIELD) || (i >= INVEN_TOTAL)) return (-1);
+
+	/* Empty slots can never be chosen */
+	if (!inventory[i].k_idx) return (-1);
+
+	/* Return the index */
+	return (i);
+}
+
+
+
+/*
+ * Determine which equipment slot (if any) an item likes
+ */
+s16b wield_slot(const object_type *o_ptr)
+{
+	/* Slot for equipment */
+	switch (o_ptr->tval)
+	{
+		case TV_DIGGING:
+		case TV_HAFTED:
+		case TV_POLEARM:
+		case TV_SWORD:
+		{
+			return (INVEN_WIELD);
+		}
+
+		case TV_BOW:
+		{
+			return (INVEN_BOW);
+		}
+
+		case TV_RING:
+		{
+			/* Use the right hand first */
+			if (!inventory[INVEN_RIGHT].k_idx) return (INVEN_RIGHT);
+
+			/* Use the left hand for swapping (by default) */
+			return (INVEN_LEFT);
+		}
+
+		case TV_AMULET:
+		{
+			return (INVEN_NECK);
+		}
+
+		case TV_LITE:
+		{
+			return (INVEN_LITE);
+		}
+
+		case TV_DRAG_ARMOR:
+		case TV_HARD_ARMOR:
+		case TV_SOFT_ARMOR:
+		{
+			return (INVEN_BODY);
+		}
+
+		case TV_CLOAK:
+		{
+			return (INVEN_OUTER);
+		}
+
+		case TV_SHIELD:
+		{
+			return (INVEN_ARM);
+		}
+
+		case TV_CROWN:
+		case TV_HELM:
+		{
+			return (INVEN_HEAD);
+		}
+
+		case TV_GLOVES:
+		{
+			return (INVEN_HANDS);
+		}
+
+		case TV_BOOTS:
+		{
+			return (INVEN_FEET);
+		}
+	}
+
+	/* No slot available */
+	return (-1);
+}
+
+
+/*
+ * Return a string mentioning how a given item is carried
+ */
+const char *mention_use(int slot)
+{
+	switch (slot)
+	{
+		case INVEN_WIELD:
+		{
+			if (adj_str_hold[p_ptr->stat_ind[A_STR]] < inventory[slot].weight / 10)
+				return "Just lifting";
+			else
+				return "Wielding";
+		}
+
+		case INVEN_BOW:
+		{
+			if (adj_str_hold[p_ptr->stat_ind[A_STR]] < inventory[slot].weight / 10)
+				return "Just holding";
+			else
+				return "Shooting";
+		}
+
+		case INVEN_LEFT:  return "On left hand";
+		case INVEN_RIGHT: return "On right hand";
+		case INVEN_NECK:  return "Around neck";
+		case INVEN_LITE:  return "Light source";
+		case INVEN_BODY:  return "On body";
+		case INVEN_OUTER: return "About body";
+		case INVEN_ARM:   return "On arm";
+		case INVEN_HEAD:  return "On head";
+		case INVEN_HANDS: return "On hands";
+		case INVEN_FEET:  return "On feet";
+	}
+
+	return "In pack";
+}
+
+
+/*
+ * Return a string describing how a given item is being worn.
+ * Currently, only used for items in the equipment, not inventory.
+ */
+cptr describe_use(int i)
+{
+	cptr p;
+
+	switch (i)
+	{
+		case INVEN_WIELD: p = "attacking monsters with"; break;
+		case INVEN_BOW:   p = "shooting missiles with"; break;
+		case INVEN_LEFT:  p = "wearing on your left hand"; break;
+		case INVEN_RIGHT: p = "wearing on your right hand"; break;
+		case INVEN_NECK:  p = "wearing around your neck"; break;
+		case INVEN_LITE:  p = "using to light the way"; break;
+		case INVEN_BODY:  p = "wearing on your body"; break;
+		case INVEN_OUTER: p = "wearing on your back"; break;
+		case INVEN_ARM:   p = "wearing on your arm"; break;
+		case INVEN_HEAD:  p = "wearing on your head"; break;
+		case INVEN_HANDS: p = "wearing on your hands"; break;
+		case INVEN_FEET:  p = "wearing on your feet"; break;
+		default:          p = "carrying in your pack"; break;
+	}
+
+	/* Hack -- Heavy weapon */
+	if (i == INVEN_WIELD)
+	{
+		object_type *o_ptr;
+		o_ptr = &inventory[i];
+		if (adj_str_hold[p_ptr->stat_ind[A_STR]] < o_ptr->weight / 10)
+		{
+			p = "just lifting";
+		}
+	}
+
+	/* Hack -- Heavy bow */
+	if (i == INVEN_BOW)
+	{
+		object_type *o_ptr;
+		o_ptr = &inventory[i];
+		if (adj_str_hold[p_ptr->stat_ind[A_STR]] < o_ptr->weight / 10)
+		{
+			p = "just holding";
+		}
+	}
+
+	/* Return the result */
+	return p;
+}
+
+
+
+
+
+/*
+ * Check an item against the item tester info
+ */
+bool item_tester_okay(const object_type *o_ptr)
+{
+	/* Hack -- allow listing empty slots */
+	if (item_tester_full) return (TRUE);
+
+	/* Require an item */
+	if (!o_ptr->k_idx) return (FALSE);
+
+	/* Hack -- ignore "gold" */
+	if (o_ptr->tval == TV_GOLD) return (FALSE);
+
+	/* Check the tval */
+	if (item_tester_tval)
+	{
+		if (item_tester_tval != o_ptr->tval) return (FALSE);
+	}
+
+	/* Check the hook */
+	if (item_tester_hook)
+	{
+		if (!(*item_tester_hook)(o_ptr)) return (FALSE);
+	}
+
+	/* Assume okay */
+	return (TRUE);
+}
+
+
+
+/*
+ * Get the indexes of objects at a given floor location. -TNB-
+ *
+ * Return the number of object indexes acquired.
+ *
+ * Valid flags are any combination of the bits:
+ *   0x01 -- Verify item tester
+ *   0x02 -- Marked/visible items only
+ *   0x04 -- Only the top item
+ */
+int scan_floor(int *items, int max_size, int y, int x, int mode)
+{
+	int this_o_idx, next_o_idx;
+
+	int num = 0;
+
+	/* Sanity */
+	if (!in_bounds(y, x)) return 0;
+
+	/* Scan all objects in the grid */
+	for (this_o_idx = cave_o_idx[y][x]; this_o_idx; this_o_idx = next_o_idx)
+	{
+		object_type *o_ptr;
+
+		/* XXX Hack -- Enforce limit */
+		if (num >= max_size) break;
+
+
+		/* Get the object */
+		o_ptr = &o_list[this_o_idx];
+
+		/* Get the next object */
+		next_o_idx = o_ptr->next_o_idx;
+
+		/* Item tester */
+		if ((mode & 0x01) && !item_tester_okay(o_ptr)) continue;
+
+		/* Marked */
+		if ((mode & 0x02) && (!o_ptr->marked || squelch_hide_item(o_ptr)))
+			continue;
+
+		/* Accept this item */
+		items[num++] = this_o_idx;
+
+		/* Only one */
+		if (mode & 0x04) break;
+	}
+
+	return num;
+}
+
+
 
 
 /*
