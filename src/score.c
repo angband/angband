3e7e2d2ec13fa65db7cc356dfd90130c7ae53668
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2008-01-31 09:42:06 +0000

    Move the high_score struct into score.c, since that's where it belongs.
    git-svn-id: svn://rephial.org/angband/trunk@674 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/score.c
+++ src/score.c
@@ -19,6 +19,49 @@
 
 
 /*
+ * Semi-Portable High Score List Entry (128 bytes)
+ *
+ * All fields listed below are null terminated ascii strings.
+ *
+ * In addition, the "number" fields are right justified, and
+ * space padded, to the full available length (minus the "null").
+ *
+ * Note that "string comparisons" are thus valid on "pts".
+ */
+
+typedef struct high_score high_score;
+
+struct high_score
+{
+	char what[8];		/* Version info (string) */
+
+	char pts[10];		/* Total Score (number) */
+
+	char gold[10];		/* Total Gold (number) */
+
+	char turns[10];		/* Turns Taken (number) */
+
+	char day[10];		/* Time stamp (string) */
+
+	char who[16];		/* Player Name (string) */
+
+	char uid[8];		/* Player UID (number) */
+
+	char sex[2];		/* Player Sex (string) */
+	char p_r[3];		/* Player Race (number) */
+	char p_c[3];		/* Player Class (number) */
+
+	char cur_lev[4];		/* Current Player Level (number) */
+	char cur_dun[4];		/* Current Dungeon Level (number) */
+	char max_lev[4];		/* Max Player Level (number) */
+	char max_dun[4];		/* Max Dungeon Level (number) */
+
+	char how[32];		/* Method of death (string) */
+};
+
+
+
+/*
  * Hack - save index of player's high score
  */
 int score_idx = -1;
