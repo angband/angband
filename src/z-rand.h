297af1a06a6af7726305473dfe2a9374a50cd763
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2008-02-27 04:52:27 +0000

    23-bit types would be /so/ much cooler.
    git-svn-id: svn://rephial.org/angband/trunk@731 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/z-rand.h
+++ src/z-rand.h
@@ -31,7 +31,7 @@
  * The integer X falls along a uniform distribution.
  */
 #define randint(M)		((s32b) Rand_div(M) + 1)
-#define rand_die(M)		((s23b) Rand_div(M) + 1)
+#define rand_die(M)		((s32b) Rand_div(M) + 1)
 #define randint1(M)		((s32b) Rand_div(M) + 1)
 
 /**
