b112a56b41ed4014a0db8349c0f322da74358807
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2008-10-01 23:00:09 +0000

    Allow checking knowledge in stores.
    git-svn-id: svn://rephial.org/angband/trunk@1010 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/store.c
+++ src/store.c
@@ -2840,7 +2840,7 @@ void do_cmd_store(void)
 		if (rogue_like_commands)
 		{
 			/* These two can't intersect! */
-			menu.cmd_keys = "\n\x04\x10\r?={}CEIPTdegilpswx\x8B\x8C"; /* \x10 = ^p , \x04 = ^D */
+			menu.cmd_keys = "\n\x04\x10\r?={}~CEIPTdegilpswx\x8B\x8C"; /* \x10 = ^p , \x04 = ^D */
 			menu.selections = "abcfmnoqrtuvyz13456790ABDFGH";
 		}
 
@@ -2848,7 +2848,7 @@ void do_cmd_store(void)
 		else
 		{
 			/* These two can't intersect! */
-			menu.cmd_keys = "\n\x010\r?={}CEIbdegiklpstw\x8B\x8C"; /* \x10 = ^p */
+			menu.cmd_keys = "\n\x010\r?={}~CEIbdegiklpstw\x8B\x8C"; /* \x10 = ^p */
 			menu.selections = "acfhmnoqruvxyz13456790ABDFGH";
 		}
 
