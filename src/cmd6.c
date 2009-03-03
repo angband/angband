0f4f166805732c24ff3127f30dacf3354fe67f06
magnate <magnate@98ddb578-d4ae-11db-a93f-5346e46f259c>
2009-03-03 17:37:02 +0000

    Temporary fix for activation messages until activations are randomised. Closes #705.
    git-svn-id: svn://rephial.org/angband/trunk@1279 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/cmd6.c
+++ src/cmd6.c
@@ -98,6 +98,99 @@ static int beam_chance(int tval)
 }
 
 
+typedef enum { 
+	ART_TAG_NONE, 
+	ART_TAG_NAME, 
+	ART_TAG_KIND, 
+	ART_TAG_VERB, 
+	ART_TAG_VERB_IS 
+} art_tag_t;
+
+art_tag_t art_tag_lookup(const char *tag)
+{
+	if (strncmp(tag, "name", 4) == 0)
+		return ART_TAG_NAME;
+	else if (strncmp(tag, "kind", 4) == 0)
+		return ART_TAG_KIND;
+	else if (strncmp(tag, "s", 1) == 0)
+		return ART_TAG_VERB;
+	else if (strncmp(tag, "is", 2) == 0)
+		return ART_TAG_VERB_IS;
+	else
+		return ART_TAG_NONE;
+}
+
+/*
+ * Print an artifact activation message.
+ * 
+ * In order to support randarts, with scrambled names, we re-write
+ * the message to replace instances of {name} with the artifact name
+ * and instances of {kind} with the type of object.
+ *
+ * This code deals with plural and singular forms of verbs correctly 
+ * when encountering {s}, though in fact both names and kinds are 
+ * always singular in the current code (gloves are "Set of" and boots
+ * are "Pair of")
+ */
+void activation_message(object_type *o_ptr, const char *message)
+{
+	char buf[1024] = "\0";
+	const char *next;
+	const char *s;
+	const char *tag;
+	const char *in_cursor;
+	size_t end = 0;
+ 
+	in_cursor = message;
+ 
+	next = strchr(in_cursor, '{');
+	while (next)
+	{
+		/* Copy the text leading up to this { */
+		strnfcat(buf, 1024, &end, "%.*s", next - in_cursor, in_cursor); 
+
+		s = next + 1;
+		while (*s && isalpha((unsigned char) *s)) s++;
+
+		if (*s == '}')		/* Valid tag */
+		{
+			tag = next + 1; /* Start the tag after the { */
+			in_cursor = s + 1;
+ 
+			switch(art_tag_lookup(tag))
+			{
+			case ART_TAG_NAME:
+				end += object_desc(buf, 1024, o_ptr, TRUE, ODESC_BASE); 
+				break;
+			case ART_TAG_KIND:
+				object_kind_name(&buf[end], 1024-end, o_ptr->k_idx, TRUE);
+				end += strlen(&buf[end]);
+				break;
+			case ART_TAG_VERB:
+				strnfcat(buf, 1024, &end, "s");
+				break;
+			case ART_TAG_VERB_IS:
+				if((end > 2) && (buf[end-2] == 's'))
+					strnfcat(buf, 1024, &end, "are");
+				else
+					strnfcat(buf, 1024, &end, "is");
+			default:
+				break;
+			}
+		}
+		else    /* An invalid tag, skip it */
+		{
+			in_cursor = next + 1;
+		} 
+
+		next = strchr(in_cursor, '{');
+	}
+	strnfcat(buf, 1024, &end, in_cursor);
+ 
+	msg_print(buf);
+} 
+
+
 /*
  * Use an object the right way.
  *
@@ -152,7 +245,7 @@ void do_cmd_use(object_type *o_ptr, int item, int snd, use_type use)
 	if (artifact_p(o_ptr))
 	{
 		message(snd, 0, "You activate it.");
-		msg_print(a_text + a_info[o_ptr->name1].effect_msg);
+		activation_message(o_ptr, a_text + a_info[o_ptr->name1].effect_msg);
 	}
 	else
 	{
