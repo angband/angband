6018e6e022a6e2028b7f29ae6e5020d4f4748c03
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2009-04-12 13:14:22 +0000

    Remove failed merge.
    git-svn-id: svn://rephial.org/angband/trunk@1345 98ddb578-d4ae-11db-a93f-5346e46f259c

--- 809-flag-arrays/src/game-cmd.c
+++
@@ -1,9 +0,0 @@
-
-#include "angband.h"
-#include "game-cmd.h"
-
-/*
- * A function called by the game to get a command from the UI.
- * Just a hook, with the real function supplied by the UI.
- */
-game_command (*get_game_command)(void);
--- 809-flag-arrays/src/ui-birth.h
+++
@@ -1,7 +0,0 @@
-#ifndef INCLUDED_UI_BIRTH_H
-#define INCLUDED_UI_BIRTH_H
-
-extern void ui_init_birthstate_handlers(void);
-
-#endif
-
