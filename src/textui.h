84aa6dc4b1db0dc86789195c63eeba88cfc170a4
ajps <ajps@98ddb578-d4ae-11db-a93f-5346e46f259c>
2009-04-19 09:41:23 +0000

    Fix various warnings introduced in [1364] with command and birth changes as reported by Pete Mack.
    git-svn-id: svn://rephial.org/angband/trunk@1367 98ddb578-d4ae-11db-a93f-5346e46f259c

---
+++ src/textui.h
@@ -0,0 +1,10 @@
+
+#include "game-cmd.h"
+
+/* From xtra3.c */
+
+/* Ask the textui core for a game command. */
+extern errr textui_get_cmd(cmd_context context, bool wait);
+
+/* Set up game event handlers for the textui. */
+void init_display(void);
