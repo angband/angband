angband/angband

commit 89290be01463f7a69ea94af3b2c188b6257425fd

Next step in abstracting game commands from the text user interface (#348). Previous behaviour should be preserved, except for a slight tweak to destroying items. Various issues are outstanding even on the limited scope of this change, e.g.   - not all do_cmd_* functions affect game state, and so should be renamed to textui_cmd_*.   - Repeated commands are still not implemented in the new system, and use the old repetition system   - UI and game commands are still all mixed into files in an almost random fashion.
git-svn-id: svn://rephial.org/angband/trunk@1417 98ddb578-d4ae-11db-a93f-5346e46f259c

src/dungeon.c
@@ -1332,7 +1332,7 @@ static void dungeon(void)
 	/* If autosave is pending, do it now. */
 	if (p_ptr->autosave)
 	{
-		do_cmd_save_game();
+		save_game();
 		p_ptr->autosave = FALSE;
 	}
 

src/game-cmd.c
@@ -1,6 +1,8 @@
 
 #include "angband.h"
 #include "game-cmd.h"
+#include "object/object.h"
+#include "cmds.h"
 
 errr (*cmd_get_hook)(cmd_context c, bool wait);
 
@@ -49,7 +51,9 @@ errr cmd_get(cmd_context c, game_command *cmd, bool wait)
 	return 1;
 }
 
-/* Inserts a command in the queue to be carried out. */
+/* 
+ * Inserts a command in the queue to be carried out. 
+ */
 errr cmd_insert(cmd_code c, ...)
 {
 	game_command cmd = {0};
@@ -65,13 +69,21 @@ errr cmd_insert(cmd_code c, ...)
 		/* These commands take no arguments. */
 		case CMD_NULL:
 		case CMD_QUIT:
-		case CMD_OPTIONS:
+		case CMD_SAVE:
+		case CMD_SUICIDE:
 		case CMD_LOADFILE:
 		case CMD_NEWGAME:
 		case CMD_ROLL_STATS:
 		case CMD_PREV_STATS:
 		case CMD_ACCEPT_CHARACTER:
 		case CMD_HELP:
+		case CMD_GO_UP:
+		case CMD_GO_DOWN:
+		case CMD_SEARCH:
+		case CMD_TOGGLE_SEARCH:
+		case CMD_HOLD:
+		case CMD_PICKUP:
+		case CMD_ENTER_STORE:
 		{
 			break;
 		}
@@ -84,15 +96,118 @@ errr cmd_insert(cmd_code c, ...)
 		case CMD_BUY_STAT:
 		case CMD_SELL_STAT:
 		case CMD_RESET_STATS:
+		case CMD_REST:
+		case CMD_STUDY_SPELL:
+		case CMD_CAST:
 		{
-			cmd.params.choice = va_arg(vp, int);
+			cmd.args[0].choice = va_arg(vp, int);
 			break;
 		}
 
 		/* These take a string argument. */
 		case CMD_NAME_CHOICE:
 		{
-			cmd.params.string = va_arg(vp, const char *);
+			/* Take a copy, it'll last longer. */
+			/* XXX Should we free this automatically when the slot
+			   in the queue gets reused, or just continue to let
+			   the command processor worry about it? */
+			cmd.args[0].string = string_make(va_arg(vp, const char *));
+			break;
+		}
+
+		/* These take a direction as an argument. */
+		case CMD_WALK:
+		case CMD_RUN:
+		case CMD_JUMP:
+		case CMD_OPEN:
+		case CMD_CLOSE:
+		case CMD_TUNNEL:
+		case CMD_DISARM:
+		case CMD_BASH:
+		case CMD_ALTER:
+		case CMD_JAM:
+		{
+			cmd.args[0].direction = va_arg(vp, int);
+
+			/* Direction hasn't been specified, so we ask for one. */
+			if (cmd.args[0].direction == DIR_UNKNOWN)
+			{
+				/* 
+				 * If no direction supplied, abort the command. 
+				 * XXX Eventually replace the get_rep_dir call
+				 * with something more generalised.
+				 */
+				if (!get_rep_dir(&cmd.args[0].direction))
+					return 1;
+			}
+			
+			break;
+		}
+
+		/* These take a point (y, x) on the map as an argument. */
+		case CMD_PATHFIND:
+		{
+			cmd.args[0].point.y = va_arg(vp, int);
+			cmd.args[0].point.x = va_arg(vp, int);
+			break;			
+		}
+
+		/* These take an item number. */
+		case CMD_UNINSCRIBE:
+		case CMD_WIELD:
+		case CMD_TAKEOFF:
+		case CMD_DROP:
+		case CMD_REFILL:
+		case CMD_STUDY_BOOK:
+		/* Note that if the effects change for the following, they 
+		   might need to take a target as well, as below. */
+		case CMD_USE_STAFF:
+		case CMD_EAT:
+		{
+			cmd.args[0].item = va_arg(vp, int);
+			break;
+		}
+		  
+		/* 
+		 * These take an item number and a  "target" as arguments, 
+		 * though a target isn't always actually needed, so we'll 
+		 * only prompt for it via callback
+		 * if the item being used needs it.
+		 */
+		case CMD_USE_WAND:
+		case CMD_USE_ROD:
+		case CMD_QUAFF:
+		case CMD_ACTIVATE:
+		case CMD_READ_SCROLL:
+		case CMD_FIRE:
+		case CMD_THROW:
+		{
+			cmd.args[0].item = va_arg(vp, int);
+			cmd.args[1].direction = va_arg(vp, int);
+
+			if (cmd.args[1].direction == DIR_UNKNOWN && 
+				obj_needs_aim(object_from_item_idx(cmd.args[0].choice)))
+			{
+				if (!get_aim_dir(&cmd.args[1].direction))
+					return 1;
+			}
+
+			break;
+		}
+
+		/* This takes an item number and a number of those items to destroy. */
+		case CMD_DESTROY:
+		{
+			cmd.args[0].item = va_arg(vp, int);
+			cmd.args[1].number = va_arg(vp, int);
+			break;
+		}
+
+		/* Takes an item number and a string to inscribe. */
+		case CMD_INSCRIBE:
+		{
+			cmd.args[0].item = va_arg(vp, int);
+			cmd.args[1].string = string_make(va_arg(vp, const char *));
 			break;
 		}
 	}
@@ -101,6 +216,79 @@ errr cmd_insert(cmd_code c, ...)
 	va_end(vp);
 
 	return cmd_insert_s(&cmd);
+}
+
+
+/* A simple list of commands and their handling functions. */
+static struct
+{
+	cmd_code cmd;
+	cmd_handler_fn fn;
+} game_cmds[] =
+{
+	{ CMD_GO_UP, do_cmd_go_up },
+	{ CMD_GO_DOWN, do_cmd_go_down },
+	{ CMD_SEARCH, do_cmd_search },
+	{ CMD_TOGGLE_SEARCH, do_cmd_toggle_search },
+	{ CMD_WALK, do_cmd_walk },
+	{ CMD_RUN, do_cmd_run },
+	{ CMD_JUMP, do_cmd_jump },
+	{ CMD_OPEN, do_cmd_open },
+	{ CMD_CLOSE, do_cmd_close },
+	{ CMD_TUNNEL, do_cmd_tunnel },
+	{ CMD_HOLD, do_cmd_hold },
+	{ CMD_DISARM, do_cmd_disarm },
+	{ CMD_BASH, do_cmd_bash },
+	{ CMD_ALTER, do_cmd_alter },
+	{ CMD_JAM, do_cmd_spike },
+	{ CMD_REST, do_cmd_rest },
+	{ CMD_PATHFIND, do_cmd_pathfind },
+	{ CMD_PICKUP, do_cmd_pickup },
+	{ CMD_SUICIDE, do_cmd_suicide },
+	{ CMD_SAVE, do_cmd_save_game },
+	{ CMD_QUIT, do_cmd_quit },
+	{ CMD_WIELD, do_cmd_wield },
+	{ CMD_TAKEOFF, do_cmd_takeoff },
+	{ CMD_DROP, do_cmd_drop },
+	{ CMD_UNINSCRIBE, do_cmd_uninscribe },
+	{ CMD_EAT, do_cmd_use },
+	{ CMD_QUAFF, do_cmd_use },
+	{ CMD_USE_ROD, do_cmd_use },
+	{ CMD_USE_STAFF, do_cmd_use },
+	{ CMD_USE_WAND, do_cmd_use },
+	{ CMD_READ_SCROLL, do_cmd_use },
+	{ CMD_ACTIVATE, do_cmd_use },
+	{ CMD_REFILL, do_cmd_refill },
+	{ CMD_FIRE, do_cmd_fire },
+	{ CMD_THROW, do_cmd_throw },
+	{ CMD_DESTROY, do_cmd_destroy },
+	{ CMD_ENTER_STORE, do_cmd_store },
+	{ CMD_INSCRIBE, do_cmd_inscribe },
+	{ CMD_STUDY_SPELL, do_cmd_study_spell },
+	{ CMD_STUDY_BOOK, do_cmd_study_book },
+	{ CMD_CAST, do_cmd_cast },
+};
+
+
+/* 
+ * Request a game command from the uI and carry out whatever actions
+ * go along with it.
+ */
+void process_command(bool no_request)
+{
+	int i;
+	game_command cmd;
+
+	/* If we've got a command to process, do it. */
+	if (cmd_get(CMD_GAME, &cmd, !no_request) == 0)
+	{
+		/* Obviously inefficient - can tune later if needed. */
+		for (i = 0; i < N_ELEMENTS(game_cmds); i++)
+		{
+			if (game_cmds[i].cmd == cmd.command)
+				game_cmds[i].fn(cmd.command, cmd.args);
+		}
+	}
 }
 
 

src/game-cmd.h
@@ -6,17 +6,17 @@
  */
 typedef enum cmd_code
 {
-	CMD_NULL,	/* A "do nothing" command so that there's something
-			   UIs can use as a "no command yet" sentinel. */
-
-	CMD_QUIT,
-	CMD_OPTIONS,
-
-	/* Splash screen commands */
+	CMD_NULL = 0,	/* A "do nothing" command so that there's something
+					   UIs can use as a "no command yet" sentinel. */
+	/* 
+	 * Splash screen commands 
+	 */
 	CMD_LOADFILE,
 	CMD_NEWGAME,
 
-	/* Birth commands */
+	/* 
+	 * Birth commands 
+	 */
 	CMD_BIRTH_RESET,
 	CMD_CHOOSE_SEX,
 	CMD_CHOOSE_RACE,
@@ -28,19 +28,100 @@ typedef enum cmd_code
 	CMD_PREV_STATS,
 	CMD_NAME_CHOICE,
 	CMD_ACCEPT_CHARACTER,
+
+	/* 
+	 * The main game commands
+	 */
+	CMD_GO_UP,
+	CMD_GO_DOWN,
+	CMD_SEARCH,
+	CMD_TOGGLE_SEARCH,
+	CMD_WALK,
+	CMD_JUMP,
+	CMD_PATHFIND,
+
+	CMD_INSCRIBE,
+	CMD_UNINSCRIBE,
+	CMD_TAKEOFF,
+	CMD_WIELD,
+	CMD_DROP,
+	CMD_STUDY_SPELL,
+	CMD_STUDY_BOOK,
+	CMD_CAST, /* Casting a spell /or/ praying. */
+	CMD_USE_STAFF,
+	CMD_USE_WAND,
+	CMD_USE_ROD,
+	CMD_ACTIVATE,
+	CMD_EAT,
+	CMD_QUAFF,
+	CMD_READ_SCROLL,
+	CMD_REFILL,
+	CMD_FIRE,
+	CMD_THROW,
+	CMD_PICKUP,
+	CMD_DESTROY,
+/*	CMD_SQUELCH_TYPE, -- might be a command, might have another interface */
+	CMD_DISARM,
+	CMD_REST,
+/*	CMD_TARGET, -- possibly should be a UI-level thing */
+	CMD_TUNNEL,
+	CMD_OPEN,
+	CMD_CLOSE,
+	CMD_JAM,
+	CMD_BASH,
+	CMD_RUN,
+	CMD_HOLD,
+	CMD_ENTER_STORE,
+	CMD_ALTER,
 	
+
+	/* Hors categorie Commands */
+	CMD_SUICIDE,
+	CMD_SAVE,
+
+/*	CMD_OPTIONS, -- probably won't be a command in this sense*/
+	CMD_QUIT,
 	CMD_HELP
-} cmd_code;
+}
+cmd_code;
 
 typedef enum cmd_context
 {
 	CMD_INIT,
 	CMD_BIRTH,
-	CMD_MAP,
+	CMD_GAME,
 	CMD_STORE,
 	CMD_DEATH
 } cmd_context;
 
+#define DIR_UNKNOWN 0
+
+enum 
+{
+	REST_TURNS,
+	REST_ALL,
+	REST_ALL_POINTS,
+	REST_SOME_POINTS	
+};
+
+
+typedef union 
+{
+	const char *string;
+	
+	int choice;
+	int item;
+	int number;
+	int direction;
+	
+	struct 
+	{
+		int x, y;
+	} point;
+} cmd_arg;
+
+/* Maximum number of arguments a command needs to take. */
+#define CMD_MAX_ARGS 2
 
 /*
  * The game_command type is used to return details of the command the
@@ -56,50 +137,11 @@ typedef struct game_command
 	/* A valid command code. */
 	cmd_code command;
 
-	/* 
-	 * Probably handle "special" repetitions like '*' as negative 
-	 * ones, as classically we limit repeats to 9999 anyway 
-	 */
+	/* Number of times to attempt to repeat command. */
 	int repeat; 
 
-	union 
-	{
-		const char *string;
-		int choice;
-
-		int direction;
-
-		struct 
-		{
-			int x;
-			int y;
-		} point;
-
-		struct
-		{
-			int idx;
-			int minimum;
-		} stat_min;
-
-		struct
-		{
-			object_type *object_used;
-			union 
-			{
-				object_type *object;
-				
-				struct
-				{
-					int x;
-					int y;
-				} point;
-				
-				int direction;
-			} target;
-		} object;
-
-		int stat_limits[A_MAX];
-	} params;
+	/* Arguments to the command */
+	cmd_arg args[CMD_MAX_ARGS];
 } game_command;
 
 
@@ -121,5 +163,7 @@ errr cmd_insert(cmd_code c, ...);
    the UI time to process user input, etc. if wait is TRUE */
 errr cmd_get(cmd_context c,game_command *cmd, bool wait);
 
+/* Called by the game engine to get the player's next action. */
+void process_command(bool no_request);
 
 #endif

src/store.c
@@ -2234,11 +2234,8 @@ static void store_sell(void)
 		return;
 	}
 
-	/* Get the item (in the pack) */
-	if (item >= 0)
-		o_ptr = &inventory[item];
- 	else
-		o_ptr = &o_list[0 - item];
+	/* Get the item */
+	o_ptr = object_from_item_idx(item);
 
 	/* Hack -- Cannot remove cursed objects */
 	if ((item >= INVEN_WIELD) && cursed_p(o_ptr))
@@ -2593,7 +2590,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 		/* Wear/wield equipment */
 		case 'w':
 		{
-			do_cmd_wield();
+			textui_cmd_wield();
 			redraw = TRUE;
 			
 			break;
@@ -2603,7 +2600,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 		case 'T':
 		case 't':
 		{
-			do_cmd_takeoff();
+			textui_cmd_takeoff();
 			redraw = TRUE;
 			
 			break;
@@ -2613,7 +2610,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 		case KTRL('D'):
 		case 'k':
 		{
-			do_cmd_destroy();
+			textui_cmd_destroy();
 			redraw = TRUE;
 			
 			break;
@@ -2679,7 +2676,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 		/* Inscribe an object */
 		case '{':
 		{
-			do_cmd_inscribe();
+			textui_cmd_inscribe();
 			redraw = TRUE;
 			
 			break;
@@ -2688,7 +2685,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 		/* Uninscribe an object */
 		case '}':
 		{
-			do_cmd_uninscribe();
+			textui_cmd_uninscribe();
 			redraw = TRUE;
 			
 			break;
@@ -2755,6 +2752,9 @@ static bool store_process_command(char cmd, void *db, int oid)
 		}
 	}
 
+	/* Let the game handle any core commands (equipping, etc) */
+	process_command(TRUE);
+
 	if (redraw)
 	{
 		event_signal(EVENT_INVENTORY);
@@ -2769,7 +2769,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 /*
  * Enter a store, and interact with it.
  */
-void do_cmd_store(void)
+void do_cmd_store(cmd_code code, cmd_arg args[])
 {
 	int py = p_ptr->py;
 	int px = p_ptr->px;

src/Makefile.src
@@ -62,7 +62,6 @@ ANGFILES = \
 	cmd3.o \
 	cmd4.o \
 	cmd5.o \
-	cmd6.o \
 	cmd-know.o \
 	cmd-obj.o \
 	death.o \

src/attack.c
@@ -19,7 +19,7 @@
 
 #include "object/object.h"
 #include "object/tvalsval.h"
-
+#include "game-cmd.h"
 
 /**
  * Determines how likely an object is to break on throwing or shooting.
@@ -464,7 +464,7 @@ void py_attack(int y, int x)
  * Note that Bows of "Extra Might" get extra range and an extra bonus
  * for the damage multiplier.
  */
-void do_cmd_fire(void)
+void do_cmd_fire(cmd_code code, cmd_arg args[])
 {
 	int dir, item;
 	int i, j, y, x;
@@ -488,42 +488,17 @@ void do_cmd_fire(void)
 	int path_n;
 	u16b path_g[256];
 
-	cptr q, s;
-
 	int msec = op_ptr->delay_factor * op_ptr->delay_factor;
 
+	/* Get item to fire and direction to fire in. */
+	item = args[0].item;
+	dir = args[1].direction;
 
-	/* Get the "bow" (if any) */
+	/* Get the object for the ammo */
+	o_ptr = object_from_item_idx(item);
+
+	/* Get the "bow" */
 	j_ptr = &inventory[INVEN_BOW];
-
-	/* Require a usable launcher */
-	if (!j_ptr->tval || !p_ptr->state.ammo_tval)
-	{
-		msg_print("You have nothing to fire with.");
-		return;
-	}
-
-
-	/* Require proper missile */
-	item_tester_tval = p_ptr->state.ammo_tval;
-
-	/* Get an item */
-	q = "Fire which item? ";
-	s = "You have nothing to fire.";
-	if (!get_item(&item, q, s, (USE_INVEN | USE_FLOOR))) return;
-
-	/* Get the object */
-	if (item >= 0)
-	{
-		o_ptr = &inventory[item];
-	}
-	else
-	{
-		o_ptr = &o_list[0 - item];
-	}
-
-	/* Get a direction (or cancel) */
-	if (!get_aim_dir(&dir)) return;
 
 	/* Base range XXX XXX */
 	tdis = 6 + 2 * p_ptr->state.ammo_mult;
@@ -759,7 +734,40 @@ void do_cmd_fire(void)
 	drop_near(i_ptr, j, y, x);
 }
 
+void textui_cmd_fire(void)
+{
+	object_type *j_ptr, *o_ptr;
+	int item;
+	int dir;
+	cptr q, s;
 
+
+	/* Get the "bow" (if any) */
+	j_ptr = &inventory[INVEN_BOW];
+
+	/* Require a usable launcher */
+	if (!j_ptr->tval || !p_ptr->state.ammo_tval)
+	{
+		msg_print("You have nothing to fire with.");
+		return;
+	}
+
+	/* Require proper missile */
+	item_tester_tval = p_ptr->state.ammo_tval;
+
+	/* Get an item */
+	q = "Fire which item? ";
+	s = "You have nothing to fire.";
+	if (!get_item(&item, q, s, (USE_INVEN | USE_FLOOR))) return;
+
+	/* Get the object */
+	o_ptr = object_from_item_idx(item);
+
+	/* Get a direction (or cancel) */
+	if (!get_aim_dir(&dir)) return;
+
+	cmd_insert(CMD_FIRE, item, dir);
+}
 
 /*
  * Throw an object from the pack or floor.
@@ -770,7 +778,7 @@ void do_cmd_fire(void)
  * to hit bonus of the weapon to have an effect?  Should it ever cause
  * the item to be destroyed?  Should it do any damage at all?
  */
-void do_cmd_throw(void)
+void do_cmd_throw(cmd_code code, cmd_arg args[])
 {
 	int dir, item;
 	int i, j, y, x;
@@ -793,30 +801,14 @@ void do_cmd_throw(void)
 	int path_n;
 	u16b path_g[256];
 
-	cptr q, s;
-
 	int msec = op_ptr->delay_factor * op_ptr->delay_factor;
 
-
-	/* Get an item */
-	q = "Throw which item? ";
-	s = "You have nothing to throw.";
-	if (!get_item(&item, q, s, (USE_INVEN | USE_FLOOR))) return;
+	/* Get item to throw and direction in which to throw it. */
+	item = args[0].item;
+	dir = args[1].direction;
 
 	/* Get the object */
-	if (item >= 0)
-	{
-		o_ptr = &inventory[item];
-	}
-	else
-	{
-		o_ptr = &o_list[0 - item];
-	}
-
-
-	/* Get a direction (or cancel) */
-	if (!get_aim_dir(&dir)) return;
-
+	o_ptr = object_from_item_idx(item);
 
 	/* Get local object */
 	i_ptr = &object_type_body;
@@ -1045,4 +1037,20 @@ void do_cmd_throw(void)
 
 	/* Drop (or break) near that location */
 	drop_near(i_ptr, j, y, x);
+}
+
+void textui_cmd_throw(void)
+{
+	int item, dir;
+	cptr q, s;
+
+	/* Get an item */
+	q = "Throw which item? ";
+	s = "You have nothing to throw.";
+	if (!get_item(&item, q, s, (USE_INVEN | USE_FLOOR))) return;
+
+	/* Get a direction (or cancel) */
+	if (!get_aim_dir(&dir)) return;
+
+	cmd_insert(CMD_THROW, item, dir);
 }

src/birth.c
@@ -1000,7 +1000,7 @@ static void do_birth_reset(bool use_quickstart, birther *quickstart_prev)
 void player_birth(bool quickstart_allowed)
 {
 	int i;
-	game_command cmd = { CMD_NULL, 0, {0} };
+	game_command cmd = { CMD_NULL, 0, {{0}} };
 
 	int stats[A_MAX];
 	int points_spent[A_MAX];
@@ -1059,12 +1059,12 @@ void player_birth(bool quickstart_allowed)
 		}
 		else if (cmd.command == CMD_CHOOSE_SEX)
 		{
-			p_ptr->psex = cmd.params.choice; 
+			p_ptr->psex = cmd.args[0].choice; 
 			generate_player();
 		}
 		else if (cmd.command == CMD_CHOOSE_RACE)
 		{
-			p_ptr->prace = cmd.params.choice;
+			p_ptr->prace = cmd.args[0].choice;
 			generate_player();
 
 			reset_stats(stats, points_spent, &points_left);
@@ -1073,7 +1073,7 @@ void player_birth(bool quickstart_allowed)
 		}
 		else if (cmd.command == CMD_CHOOSE_CLASS)
 		{
-			p_ptr->pclass = cmd.params.choice;
+			p_ptr->pclass = cmd.args[0].choice;
 			generate_player();
 
 			reset_stats(stats, points_spent, &points_left);
@@ -1084,20 +1084,20 @@ void player_birth(bool quickstart_allowed)
 		{
 			/* .choice is the stat to buy */
 			if (!rolled_stats)
-				buy_stat(cmd.params.choice, stats, points_spent, &points_left);
+				buy_stat(cmd.args[0].choice, stats, points_spent, &points_left);
 		}
 		else if (cmd.command == CMD_SELL_STAT)
 		{
 			/* .choice is the stat to sell */
 			if (!rolled_stats)
-				sell_stat(cmd.params.choice, stats, points_spent, &points_left);
+				sell_stat(cmd.args[0].choice, stats, points_spent, &points_left);
 		}
 		else if (cmd.command == CMD_RESET_STATS)
 		{
 			/* .choice is whether to regen stats */
 			reset_stats(stats, points_spent, &points_left);
 
-			if (cmd.params.choice)
+			if (cmd.args[0].choice)
 				generate_stats(stats, points_spent, &points_left);
 
 			rolled_stats = FALSE;
@@ -1156,23 +1156,16 @@ void player_birth(bool quickstart_allowed)
 		else if (cmd.command == CMD_NAME_CHOICE)
 		{
 			/* Set player name */
-			my_strcpy(op_ptr->full_name, cmd.params.string, 
+			my_strcpy(op_ptr->full_name, cmd.args[0].string, 
 					  sizeof(op_ptr->full_name));
 			
-			string_free((void *) cmd.params.string);
+			string_free((void *) cmd.args[0].string);
 			
 			/* Don't change savefile name.  If the UI
 			   wants it changed, they can do it. XXX (Good idea?) */
 			process_player_name(FALSE);
 		}
 		/* Various not-specific-to-birth commands. */
-		else if (cmd.command == CMD_OPTIONS) 
-		{
-			/* TODO: Change this to use whatever sort of message passing
-			   system we eventually decide on for options.  That might
-			   still be calling do_cmd_option. :) */
-			do_cmd_options();
-		}
 		else if (cmd.command == CMD_HELP)
 		{
 			char buf[80];

commit db65f0114bfd2f94e892672589f6a77e33261ed7

Fix for bug which caused some wrong "game commands" to be used in "stores", introduced with command changes in r1417.
git-svn-id: svn://rephial.org/angband/trunk@1419 98ddb578-d4ae-11db-a93f-5346e46f259c

src/dungeon.c
@@ -1098,7 +1098,7 @@ static void process_player(void)
 			prt("", 0, 0);
 
 			/* Process the command */
-			process_command(TRUE);
+			process_command(CMD_GAME, TRUE);
 
 			/* Count this execution */
 			if (p_ptr->command_rep)
@@ -1121,7 +1121,7 @@ static void process_player(void)
 			move_cursor_relative(p_ptr->py, p_ptr->px);
 
 			/* Get and process a command */
-			process_command(FALSE);
+			process_command(CMD_GAME, FALSE);
 		}
 
 

src/game-cmd.c
@@ -274,13 +274,13 @@ static struct
  * Request a game command from the uI and carry out whatever actions
  * go along with it.
  */
-void process_command(bool no_request)
+void process_command(cmd_context ctx, bool no_request)
 {
 	int i;
 	game_command cmd;
 
 	/* If we've got a command to process, do it. */
-	if (cmd_get(CMD_GAME, &cmd, !no_request) == 0)
+	if (cmd_get(ctx, &cmd, !no_request) == 0)
 	{
 		/* Obviously inefficient - can tune later if needed. */
 		for (i = 0; i < N_ELEMENTS(game_cmds); i++)

src/game-cmd.h
@@ -164,6 +164,6 @@ errr cmd_insert(cmd_code c, ...);
 errr cmd_get(cmd_context c,game_command *cmd, bool wait);
 
 /* Called by the game engine to get the player's next action. */
-void process_command(bool no_request);
+void process_command(cmd_context c, bool no_request);
 
 #endif

src/store.c
@@ -2753,7 +2753,7 @@ static bool store_process_command(char cmd, void *db, int oid)
 	}
 
 	/* Let the game handle any core commands (equipping, etc) */
-	process_command(TRUE);
+	process_command(CMD_STORE, TRUE);
 
 	if (redraw)
 	{