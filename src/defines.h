99186fbb9eb092e2837669f8665bd01e63240460
takkaria <takkaria@98ddb578-d4ae-11db-a93f-5346e46f259c>
2009-07-26 11:36:55 +0000

    Rev version number, nuke changes file
    git-svn-id: svn://rephial.org/angband/trunk@1578 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/defines.h
+++ src/defines.h
@@ -40,9 +40,9 @@
 #define SAVEFILE_NAME  "VNLA"
 
 #ifdef BUILD_ID
-# define VERSION_STRING "3.1.1 dev (" BUILD_ID ")"
+# define VERSION_STRING "3.1.2 dev (" BUILD_ID ")"
 #else
-# define VERSION_STRING "3.1.1 dev"
+# define VERSION_STRING "3.1.2 dev"
 #endif
 
 
