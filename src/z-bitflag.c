2db13dfa0c9c08e91e5c323ea33ef6bd069ecf23
MarbleDice <MarbleDice@98ddb578-d4ae-11db-a93f-5346e46f259c>
2010-03-03 02:23:03 +0000

    Fix warnings for r1952
    git-svn-id: svn://rephial.org/angband/trunk@1953 98ddb578-d4ae-11db-a93f-5346e46f259c

--- src/z-bitflag.c
+++ src/z-bitflag.c
@@ -48,7 +48,7 @@ bool flag_has_dbg(const bitflag *flags, const size_t size, const int flag, const
 
 	if (flag_offset >= size)
 	{
-		quit_fmt("Error in flag_has(%s, %s): FlagID[%d] Size[%u] FlagOff[%d] FlagBV[%u]\n",
+		quit_fmt("Error in flag_has(%s, %s): FlagID[%d] Size[%u] FlagOff[%u] FlagBV[%d]\n",
 		         fi, fl, flag, (unsigned int) size, (unsigned int) flag_offset, flag_binary);
 	}
 
@@ -193,7 +193,7 @@ bool flag_on_dbg(bitflag *flags, const size_t size, const int flag, const char *
 
 	if (flag_offset >= size)
 	{
-		quit_fmt("Error in flag_on(%s, %s): FlagID[%d] Size[%u] FlagOff[%d] FlagBV[%u]\n",
+		quit_fmt("Error in flag_on(%s, %s): FlagID[%d] Size[%u] FlagOff[%u] FlagBV[%d]\n",
 		         fi, fl, flag, (unsigned int) size, (unsigned int) flag_offset, flag_binary);
 	}
 
