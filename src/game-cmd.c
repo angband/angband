angband/angband

commit 1fae9110903e03962d42faea5e38bc51d602b0d0

Remove p_ptr->command_rep in favour of including the number of repeats desired when the command is put into the queue.
git-svn-id: svn://rephial.org/angband/trunk@2032 98ddb578-d4ae-11db-a93f-5346e46f259c

src/game-cmd.c
@@ -27,7 +27,9 @@ errr (*cmd_get_hook)(cmd_context c, bool wait);
 static int cmd_head = 0;
 static int cmd_tail = 0;
 static game_command cmd_queue[CMD_QUEUE_SIZE];
+
 static bool repeat_prev_allowed = FALSE;
+static bool repeating = FALSE;
 
 enum cmd_arg_type {
 	arg_STRING,
@@ -116,7 +118,9 @@ static struct
 };
 
 
-
+/*
+ * Insert the given command into the command queue.
+ */
 errr cmd_insert_s(game_command *cmd)
 {
 	/* If queue full, return error */
@@ -156,6 +160,13 @@ errr cmd_insert_s(game_command *cmd)
  */
 errr cmd_get(cmd_context c, game_command *cmd, bool wait)
 {
+	/* If we're repeating, just pull the last command again. */
+	if (repeating)
+	{
+		*cmd = cmd_queue[(cmd_tail - 1) % CMD_QUEUE_SIZE];
+		return 0;
+	}
+
 	/* If there are no commands queued, ask the UI for one. */
 	if (cmd_head == cmd_tail) 
 		cmd_get_hook(c, wait);
@@ -173,7 +184,8 @@ errr cmd_get(cmd_context c, game_command *cmd, bool wait)
 	return 1;
 }
 
-int cmd_idx(cmd_code code)
+/* Return the index of the given command in the command array. */
+static int cmd_idx(cmd_code code)
 {
 	size_t i;
 
@@ -188,20 +200,17 @@ int cmd_idx(cmd_code code)
 	return -1;
 }
 
-/* 
- * Inserts a command in the queue to be carried out. 
+/*
+ * Process the arguments to cmd_insert, etc, into a command struct and
+ * attempt to insert it into the queue.
  */
-errr cmd_insert(cmd_code c, ...)
+static errr vcmd_insert_repeated(cmd_code c, int nrepeats, va_list vp)
 {
 	game_command cmd = {CMD_NULL, 0, {{0}} };
-	va_list vp;
 	size_t j = 0;
 	int idx = cmd_idx(c);
 
 	if (idx == -1) return 1;
-
-	/* Begin the Varargs Stuff */
-	va_start(vp, c);
 
 	cmd.command = c;
 
@@ -255,35 +264,42 @@ errr cmd_insert(cmd_code c, ...)
 		}
 	}
 
-	/* End the Varargs Stuff */
-	va_end(vp);
+	cmd.nrepeats = nrepeats;
 
 	return cmd_insert_s(&cmd);
 }
 
+/* 
+ * Inserts a command in the queue to be carried out. 
+ */
+errr cmd_insert(cmd_code c, ...)
+{
+	va_list vp;
+	errr retcode = 0;
+
+	va_start(vp, c);
+	retcode = vcmd_insert_repeated(c, 0, vp);
+	va_end(vp);
+
+	return retcode;
+}
 
 /*
- * Mark a command as "allowed to be repeated".
- *
- * When a command is executed, the user has the option to request that
- * it be repeated by the UI setting p_ptr->command_arg.  If the command
- * permits repetition, then it calls this function to set 
- * p_ptr->command_rep to make it repeat until an interruption.
+ * Inserts a command in the queue to be carried out, with the given
+ * number of repeats.
  */
-static void allow_repeated_command(void)
+errr cmd_insert_repeated(cmd_code c, int nrepeats, ...)
 {
-	if (p_ptr->command_arg)
-	{
-		/* Set repeat count */
-		p_ptr->command_rep = p_ptr->command_arg - 1;
-		
-		/* Redraw the state */
-		p_ptr->redraw |= (PR_STATE);
-		
-		/* Cancel the arg */
-		p_ptr->command_arg = 0;
-	}
+	va_list vp;
+	errr retcode = 0;
+
+	va_start(vp, nrepeats);
+	retcode = vcmd_insert_repeated(c, nrepeats, vp);
+	va_end(vp);
+
+	return retcode;
 }
+
 
 /* 
  * Request a game command from the uI and carry out whatever actions
@@ -386,24 +402,79 @@ void process_command(cmd_context ctx, bool no_request)
 			}
 		}
 
-
 		/* Command repetition */
 		if (game_cmds[idx].repeat_allowed)
 		{
-			/* Auto-repeat */
-			if (game_cmds[idx].auto_repeat_n > 0 && p_ptr->command_arg == 0 && p_ptr->command_rep == 0)
-				p_ptr->command_arg = game_cmds[idx].auto_repeat_n;
-
-			allow_repeated_command();
+			/* Auto-repeat only if their isn't already a repeat length. */
+			if (game_cmds[idx].auto_repeat_n > 0 && cmd.nrepeats == 0)
+				cmd.nrepeats = game_cmds[idx].auto_repeat_n;
+		}
+		else
+		{
+			cmd.nrepeats = 0; repeating = FALSE;
 		}
 
+		/* 
+		 * The command gets to unset this if it isn't appropriate for
+		 * the user to repeat it.
+		 */
 		repeat_prev_allowed = TRUE;
 
 		if (game_cmds[idx].fn)
 			game_cmds[idx].fn(cmd.command, cmd.args);
+
+		/* If the command hasn't changed nrepeats, count this execution. */
+		if (cmd.nrepeats > 0 && cmd.nrepeats == cmd_get_nrepeats())
+			cmd_set_repeat(cmd.nrepeats - 1);
 	}
 }
 
+/* 
+ * Remove any pending repeats from the current command. 
+ */
+void cmd_cancel_repeat(void)
+{
+	game_command *cmd = &cmd_queue[(cmd_tail - 1) % CMD_QUEUE_SIZE];
+
+	if (cmd->nrepeats || repeating)
+	{
+		/* Cancel */
+		cmd->nrepeats = 0;
+		repeating = FALSE;
+		
+		/* Redraw the state (later) */
+		p_ptr->redraw |= (PR_STATE);
+	}
+}
+
+/* 
+ * Update the number of repeats pending for the current command. 
+ */
+void cmd_set_repeat(int nrepeats)
+{
+	game_command *cmd = &cmd_queue[(cmd_tail - 1) % CMD_QUEUE_SIZE];
+
+	cmd->nrepeats = nrepeats;
+	if (nrepeats) repeating = TRUE;
+	else repeating = FALSE;
+
+	/* Redraw the state (later) */
+	p_ptr->redraw |= (PR_STATE);
+}
+
+/* 
+ * Return the number of repeats pending for the current command. 
+ */
+int cmd_get_nrepeats(void)
+{
+	game_command *cmd = &cmd_queue[(cmd_tail - 1) % CMD_QUEUE_SIZE];
+	return cmd->nrepeats;
+}
+
+/*
+ * Do not allow the current command to be repeated by the user using the
+ * "repeat last command" command.
+ */
 void cmd_disable_repeat(void)
 {
 	repeat_prev_allowed = FALSE;

src/cave.c
@@ -18,6 +18,7 @@
 #include "angband.h"
 #include "object/tvalsval.h"
 #include "game-event.h"
+#include "game-cmd.h"
 
 /*
  * Support for Adam Bolt's tileset, lighting and transparency effects
@@ -3921,14 +3922,7 @@ void disturb(int stop_search, int unused_flag)
 	/* p_ptr->command_new = 0; */
 
 	/* Cancel repeated commands */
-	if (p_ptr->command_rep)
-	{
-		/* Cancel */
-		p_ptr->command_rep = 0;
-
-		/* Redraw the state (later) */
-		p_ptr->redraw |= (PR_STATE);
-	}
+	cmd_cancel_repeat();
 
 	/* Cancel Resting */
 	if (p_ptr->resting)

src/cmd0.c
@@ -666,7 +666,7 @@ void textui_process_command(bool no_request)
 		assert(p_ptr->command_cmd >= CHAR_MIN && p_ptr->command_cmd <= CHAR_MAX);
 		/* Execute the command */
 		if (converted_list[(unsigned char) p_ptr->command_cmd].cmd != CMD_NULL)
-			cmd_insert(converted_list[(unsigned char) p_ptr->command_cmd].cmd);
+			cmd_insert_repeated(converted_list[(unsigned char) p_ptr->command_cmd].cmd, p_ptr->command_arg);
 
 		else if (converted_list[(unsigned char) p_ptr->command_cmd].hook)
 			converted_list[(unsigned char) p_ptr->command_cmd].hook();

src/cmd1.c
@@ -620,13 +620,8 @@ void move_player(int dir)
 			(cave_feat[y][x] <= FEAT_DOOR_TAIL))
 	{
 		/* Auto-repeat if not already repeating */
-		if (!p_ptr->command_rep && (p_ptr->command_arg <= 0))
-		{
-			p_ptr->command_rep = 99;
-
-			/* Reset the command count */
-			p_ptr->command_arg = 0;
-		}
+		if (cmd_get_nrepeats() == 0)
+			cmd_set_repeat(99);
 
 		do_cmd_alter_aux(dir);
 	}

src/cmd2.c
@@ -818,7 +818,7 @@ void textui_cmd_open(void)
 		}
 	}
 
-	cmd_insert(CMD_OPEN, dir);
+	cmd_insert_repeated(CMD_OPEN, p_ptr->command_arg, dir);
 }
 
 
@@ -967,7 +967,7 @@ void textui_cmd_close(void)
 			return;
 	}
 
-	cmd_insert(CMD_CLOSE, dir);
+	cmd_insert_repeated(CMD_CLOSE, p_ptr->command_arg, dir);
 }
 
 
@@ -1289,7 +1289,7 @@ void textui_cmd_tunnel(void)
 {
 	int dir;
 	if (!get_rep_dir(&dir)) return;
-	cmd_insert(CMD_TUNNEL, dir);
+	cmd_insert_repeated(CMD_TUNNEL, p_ptr->command_arg, dir);
 }
 
 
@@ -1511,7 +1511,7 @@ void textui_cmd_disarm(void)
 			return;
 	}
 
-	cmd_insert(CMD_DISARM, dir);
+	cmd_insert_repeated(CMD_DISARM, p_ptr->command_arg, dir);
 }
 
 /*
@@ -1700,7 +1700,7 @@ void textui_cmd_bash(void)
 	if (!get_rep_dir(&dir))
 		return;
 
-	cmd_insert(CMD_BASH, dir);
+	cmd_insert_repeated(CMD_BASH, p_ptr->command_arg, dir);
 }
 
 
@@ -1810,7 +1810,7 @@ void textui_cmd_alter(void)
 	if (!get_rep_dir(&dir))
 		return;
 
-	cmd_insert(CMD_ALTER, dir);
+	cmd_insert_repeated(CMD_ALTER, p_ptr->command_arg, dir);
 }
 
 /*
@@ -1963,7 +1963,7 @@ void textui_cmd_spike(void)
 	if (!get_rep_dir(&dir))
 		return;
 
-	cmd_insert(CMD_JAM, dir);
+	cmd_insert_repeated(CMD_JAM, p_ptr->command_arg, dir);
 }
 
 
@@ -2086,7 +2086,7 @@ void textui_cmd_walk(void)
 	if (!get_rep_dir(&dir))
 		return;
 
-	cmd_insert(CMD_WALK, dir);
+	cmd_insert_repeated(CMD_WALK, p_ptr->command_arg, dir);
 }
 
 
@@ -2270,9 +2270,6 @@ void do_cmd_rest(cmd_code code, cmd_arg args[])
 	
 	/* Take a turn XXX XXX XXX (?) */
 	p_ptr->energy_use = 100;
-
-	/* Cancel the arg */
-	p_ptr->command_arg = 0;
 
 	/* Cancel searching */
 	p_ptr->searching = FALSE;

src/dungeon.c
@@ -947,7 +947,7 @@ static void process_player(void)
 
 	/* Check for "player abort" */
 	if (p_ptr->running ||
-	    p_ptr->command_rep ||
+	    cmd_get_nrepeats() > 0 ||
 	    (p_ptr->resting && !(turn & 0x7F)))
 	{
 		/* Do not wait */
@@ -1054,7 +1054,7 @@ static void process_player(void)
 		}
 
 		/* Repeated command */
-		else if (p_ptr->command_rep)
+		else if (cmd_get_nrepeats() > 0)
 		{
 			/* Hack -- Assume messages were seen */
 			msg_flag = FALSE;
@@ -1064,16 +1064,6 @@ static void process_player(void)
 
 			/* Process the command */
 			process_command(CMD_GAME, TRUE);
-
-			/* Count this execution */
-			if (p_ptr->command_rep)
-			{
-				/* Count this execution */
-				p_ptr->command_rep--;
-
-				/* Redraw the state */
-				p_ptr->redraw |= (PR_STATE);
-			}
 		}
 
 		/* Normal command */
@@ -1353,7 +1343,6 @@ static void dungeon(void)
 	/* Reset the "command" vars */
 	p_ptr->command_cmd = 0;
 	p_ptr->command_new = 0;
-	p_ptr->command_rep = 0;
 	p_ptr->command_arg = 0;
 	p_ptr->command_dir = 0;
 

src/game-cmd.h
@@ -137,7 +137,7 @@ typedef struct game_command
 	cmd_code command;
 
 	/* Number of times to attempt to repeat command. */
-	int repeat; 
+	int nrepeats; 
 
 	/* Arguments to the command */
 	cmd_arg args[CMD_MAX_ARGS];
@@ -153,18 +153,37 @@ extern errr (*cmd_get_hook)(cmd_context c, bool wait);
 errr cmd_insert_s(game_command *cmd);
 
 /* 
- * Convenience function.
- * Inserts a command with params in the queue to be carried out. 
+ * Convenience functions.
+ * Insert a command with params in the queue to be carried out.
  */
 errr cmd_insert(cmd_code c, ...);
+errr cmd_insert_repeated(cmd_code c, int nrepeats,  ...);
 
-/* Gets the next command from the queue, optionally waiting to allow
-   the UI time to process user input, etc. if wait is TRUE */
+/* 
+ * Gets the next command from the queue, optionally waiting to allow
+ * the UI time to process user input, etc. if wait is TRUE 
+ */
 errr cmd_get(cmd_context c,game_command *cmd, bool wait);
 
 /* Called by the game engine to get the player's next action. */
 void process_command(cmd_context c, bool no_request);
 
+/* Remove any pending repeats from the current command. */
+void cmd_cancel_repeat(void);
+
+/* Update the number of repeats pending for the current command. */
+void cmd_set_repeat(int nrepeats);
+
+/*
+ * Call to disallow the current command from being repeated with the
+ * "Repeat last command" command.
+ */
 void cmd_disable_repeat(void);
+
+/* 
+ * Returns the number of repeats left for the current command.
+ * i.e. zero if not repeating.
+ */
+int cmd_get_nrepeats(void);
 
 #endif

src/player/types.h
@@ -213,7 +213,6 @@ typedef struct
 
 	s16b command_cmd;		/* Gives identity of current command */
 	s16b command_arg;		/* Gives argument of current command */
-	s16b command_rep;		/* Gives repetition of current command */
 	s16b command_dir;		/* Gives direction of current command */
 	int  command_inv;		/* Gives item of current command */
 	ui_event_data command_cmd_ex; /* Gives additional information of current command */

commit 970293e9bd3984b3f1098f956b8914757f768775

Make autorepeated commands work properly again (fixes bug introduced in r2032)
git-svn-id: svn://rephial.org/angband/trunk@2033 98ddb578-d4ae-11db-a93f-5346e46f259c

src/game-cmd.c
@@ -407,7 +407,10 @@ void process_command(cmd_context ctx, bool no_request)
 		{
 			/* Auto-repeat only if their isn't already a repeat length. */
 			if (game_cmds[idx].auto_repeat_n > 0 && cmd.nrepeats == 0)
+			{
 				cmd.nrepeats = game_cmds[idx].auto_repeat_n;
+				cmd_set_repeat(cmd.nrepeats);
+			}
 		}
 		else
 		{