c2e180465f95d8432500f45395100e6703615fac
Elly <elly@leptoquark.net>
2010-08-25 21:50:36 -0400

    Add a few example commands to main-test.

--- src/main-test.c
+++ src/main-test.c
@@ -5,15 +5,64 @@
 
 #include "angband.h"
 
+static int verbose = 0;
+
+static void c_quit(char *rest) {
+	quit(NULL);
+}
+
+static void c_verbose(char *rest) {
+	if (!strcmp(rest, "0")) {
+		printf("cmd-verbose: off\n");
+		verbose = 0;
+	} else {
+		printf("cmd-verbose: on\n");
+		verbose = 1;
+	}
+}
+
+static void c_version(char *rest) {
+	printf("cmd-version: %s %s\n", VERSION_NAME, VERSION_STRING);
+}
+
+typedef struct {
+	const char *name;
+	void (*func)(char *args);
+} test_cmd;
+
+static test_cmd cmds[] = {
+	{ "quit", c_quit },
+	{ "verbose", c_verbose },
+	{ "version", c_version },
+	{ NULL, NULL }
+};
+
 static errr test_docmd(void) {
 	char buf[1024];
+	char *cmd;
+	char *rest;
+	int i;
+
+	printf("test> ");
+	fflush(stdout);
 	if (!fgets(buf, sizeof(buf), stdin)) {
 		return -1;
 	}
 	if (strchr(buf, '\n')) {
 		*strchr(buf, '\n') = '\0';
 	}
-	printf("test-docmd: %s\n", buf);
+
+	if (verbose) printf("test-docmd: %s\n", buf);
+	cmd = strtok(buf, " ");
+	rest = strtok(NULL, "");
+
+	for (i = 0; cmds[i].name; i++) {
+		if (!strcmp(cmds[i].name, cmd)) {
+			cmds[i].func(rest);
+			return 0;
+		}
+	}
+
 	return 0;
 }
 
@@ -29,60 +78,60 @@ typedef struct {
 } term_xtra_func;
 
 static void term_init_test(term *t) {
-	printf("term-init %s %s\n", VERSION_NAME, VERSION_STRING);
+	if (verbose) printf("term-init %s %s\n", VERSION_NAME, VERSION_STRING);
 }
 
 static void term_nuke_test(term *t) {
-	printf("term-end\n");
+	if (verbose) printf("term-end\n");
 }
 
 static errr term_user_test(int n) {
-	printf("term-user %d\n", n);
+	if (verbose) printf("term-user %d\n", n);
 	return 0;
 }
 
 static errr term_xtra_clear(int v) {
-	printf("term-xtra-clear %d\n", v);
+	if (verbose) printf("term-xtra-clear %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_noise(int v) {
-	printf("term-xtra-noise %d\n", v);
+	if (verbose) printf("term-xtra-noise %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_fresh(int v) {
-	printf("term-xtra-fresh %d\n", v);
+	if (verbose) printf("term-xtra-fresh %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_shape(int v) {
-	printf("term-xtra-shape %d\n", v);
+	if (verbose) printf("term-xtra-shape %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_alive(int v) {
-	printf("term-xtra-alive %d\n", v);
+	if (verbose) printf("term-xtra-alive %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_event(int v) {
-	printf("term-xtra-event %d\n", v);
+	if (verbose) printf("term-xtra-event %d\n", v);
 	return test_docmd();
 }
 
 static errr term_xtra_flush(int v) {
-	printf("term-xtra-flush %d\n", v);
+	if (verbose) printf("term-xtra-flush %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_delay(int v) {
-	printf("term-xtra-delay %d\n", v);
+	if (verbose) printf("term-xtra-delay %d\n", v);
 	return 0;
 }
 
 static errr term_xtra_react(int v) {
-	printf("term-xtra-react\n");
+	if (verbose) printf("term-xtra-react\n");
 	return 0;
 }
 
@@ -106,22 +155,22 @@ static errr term_xtra_test(int n, int v) {
 			return xtras[i].func(v);
 		}
 	}
-	printf("term-xtra-unknown %d %d\n", n, v);
+	if (verbose) printf("term-xtra-unknown %d %d\n", n, v);
 	return 0;
 }
 
 static errr term_curs_test(int x, int y) {
-	printf("term-curs %d %d\n", x, y);
+	if (verbose) printf("term-curs %d %d\n", x, y);
 	return 0;
 }
 
 static errr term_wipe_test(int x, int y, int n) {
-	printf("term-wipe %d %d %d\n", x, y, n);
+	if (verbose) printf("term-wipe %d %d %d\n", x, y, n);
 	return 0;
 }
 
 static errr term_text_test(int x, int y, int n, byte a, const char *s) {
-	printf("term-text %d %d %d %02x %s\n", x, y, n, a, s);
+	if (verbose) printf("term-text %d %d %d %02x %s\n", x, y, n, a, s);
 	return 0;
 }
 
