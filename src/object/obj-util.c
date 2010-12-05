22b5991c6d0bf6ae1ab2cbe95df79d06735bf563
Peter Denison <angband@marshadder.org>
2010-12-05 22:51:50 +0000

    Allow any number of objects to stack in stores (fixes #1238)
    Note that the other bits of the store code will truncate the stack at 99
    objects, but the object will be accepted, and not form another stack, as it
    would elsewhere
    
    Signed-off-by: Peter Denison <angband@marshadder.org>

--- src/object/obj-util.c
+++ src/object/obj-util.c
@@ -1595,8 +1595,8 @@ bool object_similar(const object_type *o_ptr, const object_type *j_ptr,
 {
 	int total = o_ptr->number + j_ptr->number;
 
-	/* Check against stacking limit */
-	if (total >= MAX_STACK_SIZE) return FALSE;
+	/* Check against stacking limit - except in stores which absorb anyway */
+	if (!(mode & OSTACK_STORE) && (total >= MAX_STACK_SIZE)) return FALSE;
 
 	/* Hack -- identical items cannot be stacked */
 	if (o_ptr == j_ptr) return FALSE;
