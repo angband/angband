6c6bda06fbcc101a6598defc49924b3f59638d04
Elly Jones <elly@leptoquark.net>
2010-12-05 17:09:01 -0500

    Fix wrong args to place_object(). (cherry picked from commit e6f3fc7a90f54a69ce169bc6b106fa4f25c7971f)

--- src/generate.h
+++ src/generate.h
@@ -3,7 +3,7 @@
 #ifndef GENERATE_H
 #define GENERATE_H
 
-void place_object(int y, int x, int level, bool good, bool great);
+void place_object(struct cave *c, int y, int x, int level, bool good, bool great);
 void place_gold(int y, int x, int level);
 void place_secret_door(int y, int x);
 void place_closed_door(int y, int x);
