288cf040c4aa8b73404ccc7172879c7367c9bc7b
Erik Osheim <d_m@plastic-idolatry.com>
2010-12-22 23:52:22 -0500

    Make infinite range on gold drops friendlier

--- src/object/obj-make.c
+++ src/object/obj-make.c
@@ -1315,10 +1315,9 @@ void make_gold(object_type *j_ptr, int lev, int coin_type)
 	s32b spread = lev + 10;
 	s32b value = rand_spread(avg, spread);
 
-	/* Increase variance to infinity, retain same mean */
-	while (one_in_(2) && (value * 1414) / 1000 <= MAX_SHORT) /* 1414/1000 = sqrt(2) */
-		value = (value * 1414) / 1000;
-	value = (value * 414) / 1414;
+	/* Increase the range to infinite, moving the average to 110% */
+	while (one_in_(100) && value * 10 <= MAX_SHORT)
+		value *= 10;
 
 	/* Pick a treasure variety scaled by level, or force a type */
 	if (coin_type != SV_GOLD_ANY)
