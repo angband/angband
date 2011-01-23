f0b7d032a9f30a7b6bb203a3d8f19820e3a5e97a
Erik Osheim <d_m@plastic-idolatry.com>
2011-01-23 13:05:39 -0500

    First pass at refactoring shooting/throwing code in attack.c

--- src/attack.h
+++ src/attack.h
@@ -7,4 +7,17 @@ extern int breakage_chance(const object_type *o_ptr);
 extern bool test_hit(int chance, int ac, int vis);
 extern void py_attack(int y, int x);
 
+struct attack_result {
+    bool hit;
+    int dmg;
+    u32b msg_type;
+    const char *hit_verb;
+};
+
+/*
+* cave_builder is a function pointer which builds a level.
+*/
+typedef bool (*ranged_attack) (object_type *o_ptr, int y, int x, int *dmg, u32b *msg_type, const char *hit_verb);
+typedef struct attack_result (*ranged_attack2) (object_type *o_ptr, int y, int x);
+
 #endif /* !ATTACK_H */
