7cfa88a7187fe02b344d08fd18595b94cd2c5e71
myshkin <myshkin@durak.net>
2011-04-09 18:58:59 -0700

    Added proper file header to main-stats.c.

--- src/main-stats.c
+++ src/main-stats.c
@@ -1,6 +1,19 @@
-/** @file main-stats.c
- *  @brief Pseudo-UI for stats generation (borrows heavily from main-test.c)
- *  @author Robert Au <myshkin+angband@durak.net>
+/*
+ * File: main-stats.c
+ * Purpose: Pseudo-UI for stats generation (borrows heavily from main-test.c)
+ *
+ * Copyright (c) 2010-11 Robert Au <myshkin+angband@durak.net>
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
  */
 
 #include "angband.h"
