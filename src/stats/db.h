9d9a1b9bcb9c412ecba6d008d7710ce273e04ccf
myshkin <myshkin@durak.net>
2011-06-01 02:38:12 -0700

    Added sqlite3 DB interface. configure now checks for sqlite3 headers and library with --enable-stats.

---
+++ src/stats/db.h
@@ -0,0 +1,31 @@
+/*
+ * File: stats/db.h
+ * Purpose: interface to SQLite3 database manipulation
+ *
+ * Copyright (c) 2011 Robert Au
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational,
+ *    research,
+ *    and not for profit purposes provided that this copyright and
+ *    statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ */
+
+#ifndef STATS_DB_H
+#define STATS_DB_H
+
+#include <sqlite3.h>
+
+extern bool stats_db_open(void);
+extern bool stats_db_close(void);
+extern int stats_db_exec(char *sql_str);
+extern int stats_db_stmt_prep(sqlite3_stmt **sql_stmt, char *sql_str);
+
+#endif /* STATS_DB_H */
