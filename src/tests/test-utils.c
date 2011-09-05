005ced97f4a0ecfa4ca7b9bf6de046385af675ac
Erik Osheim <d_m@plastic-idolatry.com>
2011-09-05 02:18:49 -0400

    fix egregiously broken 'make install' procedure

--- src/tests/test-utils.c
+++ src/tests/test-utils.c
@@ -10,6 +10,7 @@
 #include "config.h"
 #include "init.h"
 #include "z-util.h"
+#include "externs.h"
 
 /*
  * Call this function to simulate init_stuff() and populate the *_info arrays
@@ -18,8 +19,8 @@ void read_edit_files(void) {
 	char configpath[512], libpath[512], datapath[512];
 
 	my_strcpy(configpath, DEFAULT_CONFIG_PATH, sizeof(configpath));
-	my_strcpy(libpath,    DEFAULT_CONFIG_PATH, sizeof(libpath));
-	my_strcpy(datapath,   DEFAULT_CONFIG_PATH, sizeof(datapath));
+	my_strcpy(libpath, DEFAULT_LIB_PATH, sizeof(libpath));
+	my_strcpy(datapath, DEFAULT_DATA_PATH, sizeof(datapath));
 
 	configpath[511] = libpath[511] = datapath[511] = '\0';
 
