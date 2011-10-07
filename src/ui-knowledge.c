c3ba3dd2d62ef80ea3335b6723b852f6843225d0
Jonas Lith <artes.liberales@spray.se>
2011-10-08 00:18:12 +0200

    #1499 Replace indices in function calls with pointers

--- src/ui-knowledge.c
+++ src/ui-knowledge.c
@@ -1036,8 +1036,14 @@ static const char *race_name(int gid) { return monster_group[gid].name; }
 
 static void mon_lore(int oid)
 {
+	int r_idx;
+	const monster_race *r_ptr;
+	const monster_lore *l_ptr;
+
+	r_idx = default_join[oid].oid;
+
 	/* Update the monster recall window */
-	monster_race_track(default_join[oid].oid);
+	monster_race_track(r_idx);
 	handle_stuff(p_ptr);
 
 	/* Save the screen */
@@ -1047,9 +1053,12 @@ static void mon_lore(int oid)
 	text_out_hook = text_out_to_screen;
 
 	/* Recall monster */
-	roff_top(default_join[oid].oid);
+	assert(r_idx);
+	r_ptr = &r_info[r_idx];
+	l_ptr = &l_list[r_idx];
+	roff_top(r_ptr);
 	Term_gotoxy(0, 2);
-	describe_monster(default_join[oid].oid, FALSE);
+	describe_monster(r_ptr, l_ptr, FALSE);
 
 	text_out_c(TERM_L_BLUE, "\n[Press any key to continue]\n");
 	(void)anykey();
