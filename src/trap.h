b7dc631ad61635f38b708d73c5e5bb7eac7febd6
Jonas Lith <artes.liberales@spray.se>
2011-10-14 22:35:40 +0200

    #778 Objects on the floor should not stop trap creation

--- src/trap.h
+++ src/trap.h
@@ -7,5 +7,6 @@ bool trap_check_hit(int power);
 extern void hit_trap(int y, int x);
 extern void pick_trap(int y, int x);
 extern void place_trap(struct cave *c, int y, int x);
+extern void create_trap(struct cave *c, int y, int x);
 
 #endif /* !TRAP_H */
