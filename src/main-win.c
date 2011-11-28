3df4370db6af29bd41faa18ed64f280599e72cd7
Brett Reid <baronblu@gmail.com>
2011-11-28 05:46:55 -0500

    Comment out Windows define that was preventing some utf8 defines from compiling.

--- src/main-win.c
+++ src/main-win.c
@@ -284,7 +284,7 @@
  * Exclude parts of WINDOWS.H that are not needed (Win32)
  */
 #define WIN32_LEAN_AND_MEAN
-#define NONLS             /* All NLS defines and routines */
+/*#define NONLS*/             /* All NLS defines and routines */
 #define NOSERVICE         /* All Service Controller routines, SERVICE_ equates, etc. */
 #define NOKANJI           /* Kanji support stuff. */
 #define NOMCX             /* Modem Configuration Extensions */
