angband/angband

commit 883bab22ca2dbaf7ea1d092831269b60f5de4776

Remove compiler warnings and fix midx to m_ptr conversion in main-gtk and main-stats.

src/gtk/main-gtk.c
@@ -27,13 +27,13 @@
 #include "files.h"
 #include "init.h"
 
-/* this is used to draw the various terrain characters */
+/* this is used to draw the various terrain characters
 static unsigned int graphics_table[32] = {
 	000, '*', '#', '?', '?', '?', '?', '.',
 	'+', '?', '?', '+', '+', '+', '+', '+',
 	'~', '-', '-', '-', '_', '+', '+', '+',
 	'+', '|', '?', '?', '?', '?', '?', '.',
-};
+}; */
 
 /* 
  *Add a bunch of debugger message, to trace where problems are. 
@@ -2502,7 +2502,7 @@ static void handle_sidebar(game_event_type type, game_event_data *data, void *us
 	
 	xtra_win_data *xd = &xdata[5];
 	long xp = (long)p_ptr->exp;
-	monster_type *m_ptr = cave_monster(cave, p_ptr->health_who);
+	monster_type *m_ptr = cave_monster(cave, p_ptr->health_who->midx);
 	int i = 0, sidebar_length = 12;
 
 	/* Calculate XP for next level */

src/main-gcu.c
@@ -768,7 +768,7 @@ errr init_gcu(int argc, char **argv) {
 	int i;
 	int rows, cols, y, x;
 	int next_win = 0;
-	bool graphics = TRUE;
+/*	bool graphics = TRUE; */
 
 	/* Initialize info about terminal capabilities */
 	termtype = getenv("TERM");
@@ -780,8 +780,8 @@ errr init_gcu(int argc, char **argv) {
 			use_big_screen = TRUE;
 		else if (prefix(argv[i], "-B"))
 			bold_extended = TRUE;
-		else if (prefix(argv[i], "-a"))
-			graphics = FALSE;
+/*		else if (prefix(argv[i], "-a"))
+			graphics = FALSE; */
 		else
 			plog_fmt("Ignoring option: %s", argv[i]);
 	}

src/main-stats.c
@@ -241,12 +241,12 @@ static void kill_all_monsters(int level)
 	int i;
 
 	for (i = cave_monster_max(cave) - 1; i >= 1; i--) {
-		const monster_type *m_ptr = cave_monster(cave, i);
+		monster_type *m_ptr = cave_monster(cave, i);
 		monster_race *r_ptr = &r_info[m_ptr->r_idx];
 
 		level_data[level].monsters[m_ptr->r_idx]++;
 
-		monster_death(i, TRUE);
+		monster_death(m_ptr, TRUE);
 
 		if (rf_has(r_ptr->flags, RF_UNIQUE))
 			r_ptr->max_num = 0;

commit 6f00fa8c1dc7bdfb1215cbad4c3bfd00c3be5497

Fix GTK crash on startup / new game
    The midx-to-m_ptr conversion change in 883bab22 caused a null
    pointer crash during player generation (eg, on a clean install).

src/gtk/main-gtk.c
@@ -2502,7 +2502,8 @@ static void handle_sidebar(game_event_type type, game_event_data *data, void *us
 	
 	xtra_win_data *xd = &xdata[5];
 	long xp = (long)p_ptr->exp;
-	monster_type *m_ptr = cave_monster(cave, p_ptr->health_who->midx);
+	monster_type *m_ptr = p_ptr->health_who != NULL ?
+            cave_monster(cave, p_ptr->health_who->midx) : NULL;
 	int i = 0, sidebar_length = 12;
 
 	/* Calculate XP for next level */