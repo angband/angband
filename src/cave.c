8de424aaae07cb1e5605608382678cd3a62c9aed
Fizzix <aaroncbader@gmail.com>
2012-04-09 17:43:50 -0500

    change name of cave_isstair to cave_isstairs to keep in line with v4

--- src/cave.c
+++ src/cave.c
@@ -3795,7 +3795,7 @@ bool cave_istrap(struct cave *c, int y, int x) {
 /**
  * True if cave is an up or down stair
  */
- bool cave_isstair(struct cave*c, int y, int x) {
+ bool cave_isstairs(struct cave*c, int y, int x) {
     return c->feat[y][x] == FEAT_LESS || c->feat[y][x] == FEAT_MORE;
 }
 
