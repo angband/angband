1b83c447b19d1a795fac71a38de299f471578073
Brett Reid <baronblu@gmail.com>
2012-05-12 02:46:07 -0400

    remove a mci define to allow new mp3 playing to compile.

--- src/main-win.c
+++ src/main-win.c
@@ -197,7 +197,7 @@
 #define MMNOAUX          /* Auxiliary audio support */
 #define MMNOTIMER        /* Timer support */
 #define MMNOJOY          /* Joystick support */
-#define MMNOMCI          /* MCI support */
+/*#define MMNOMCI */         /* MCI support */
 #define MMNOMMIO         /* Multimedia file I/O support */
 #define MMNOMMSYSTEM     /* General MMSYSTEM functions */
 
