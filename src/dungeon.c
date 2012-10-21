ce09dcc019566692111cb5ab3392c4377f0f3f0c
Nick McConnell <nckmccnnll@gmail.com>
2012-10-21 19:49:46 +1100

    Cursor fix in bigtile mode

--- src/dungeon.c
+++ src/dungeon.c
@@ -1260,7 +1260,7 @@ void idle_update(void)
 {
 	if (!character_dungeon) return;
 
-	if (!OPT(animate_flicker)) return;
+	if (!OPT(animate_flicker) || (use_graphics != GRAPHICS_NONE)) return;
 
 	/* Animate and redraw if necessary */
 	do_animation();
