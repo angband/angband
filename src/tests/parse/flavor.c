8075e498fb7916d29e59ae76c1c443dc9561a0b0
Andi Sidwell <andi@takkaria.org>
2013-04-12 22:41:38 +0100

    Fix flavor.txt tests

--- src/tests/parse/flavor.c
+++ src/tests/parse/flavor.c
@@ -18,21 +18,15 @@ int teardown_tests(void *state) {
 	return 0;
 }
 
-int test_n0(void *state) {
-	enum parser_error r = parser_parse(state, "N:1:3:5");
-	struct flavor *f;
+int test_kind0(void *state) {
+	enum parser_error r = parser_parse(state, "kind:light:&");
 
 	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	eq(f->fidx, 1);
-	eq(f->tval, 3);
-	eq(f->sval, 5);
 	ok;
 }
 
-int test_n1(void *state) {
-	enum parser_error r = parser_parse(state, "N:2:light");
+int test_flavor0(void *state) {
+	enum parser_error r = parser_parse(state, "flavor:2:blue:Fishy");
 	struct flavor *f;
 
 	eq(r, PARSE_ERROR_NONE);
@@ -40,42 +34,15 @@ int test_n1(void *state) {
 	require(f);
 	eq(f->fidx, 2);
 	eq(f->tval, TV_LIGHT);
-	eq(f->sval, SV_UNKNOWN);
-	ok;
-}
-
-int test_g0(void *state) {
-	enum parser_error r = parser_parse(state, "G:!:y");
-	struct flavor *f;
-
-	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	eq(f->fidx, 2);
-	eq(f->d_char, L'!');
-	eq(f->d_attr, TERM_YELLOW);
-	ok;
-}
-
-int test_d0(void *state) {
-	enum parser_error r = parser_parse(state, "D:foo");
-	struct flavor *f;
-
-	eq(r, PARSE_ERROR_NONE);
-	r = parser_parse(state, "D: bar");
-	eq(r, PARSE_ERROR_NONE);
-	f = parser_priv(state);
-	require(f);
-	require(streq(f->text, "foo bar"));
-	eq(f->fidx, 2);
+	eq(f->d_char, L'&');
+	eq(f->d_attr, TERM_BLUE);
+	require(streq(f->text, "Fishy"));
 	ok;
 }
 
 const char *suite_name = "parse/flavor";
 struct test tests[] = {
-	{ "n0", test_n0 },
-	{ "n1", test_n1 },
-	{ "g0", test_g0 },
-	{ "d0", test_d0 },
+	{ "kind0", test_kind0 },
+	{ "flavor0", test_flavor0 },
 	{ NULL, NULL }
 };
