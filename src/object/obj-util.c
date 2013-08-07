angband/angband

commit 2a40e742db7fc1f37af03b3b1dfe584f5fa40a74

Remove several of the cave_bold macros.
Conflicts:
	src/cave.c
	src/cave.h

src/object/obj-util.c
@@ -2145,14 +2145,11 @@ void drop_near(struct cave *c, object_type *j_ptr, int chance, int y, int x, boo
 		}
 
 		/* Require floor space */
-		if (cave->feat[ty][tx] != FEAT_FLOOR) continue;
+		if (!cave_canputitem(cave, y, x)) continue;
 
 		/* Bounce to that location */
 		by = ty;
 		bx = tx;
-
-		/* Require floor space */
-		if (!cave_clean_bold(by, bx)) continue;
 
 		/* Okay */
 		flag = TRUE;

src/cave.c
@@ -332,7 +332,8 @@ bool cave_valid_bold(int y, int x)
 	object_type *o_ptr;
 
 	/* Forbid perma-grids */
-	if (cave_perma_bold(y, x)) return (FALSE);
+	if (cave_isperm(cave, y, x) || cave_isshop(cave, y, x) || 
+		cave_isstairs(cave, y, x)) return (FALSE);
 
 	/* Check objects */
 	for (o_ptr = get_first_object(y, x); o_ptr; o_ptr = get_next_object(o_ptr))
@@ -3790,6 +3791,9 @@ bool cave_isdownstairs(struct cave *c, int y, int x) {
     return c->feat[y][x] == FEAT_MORE;
 }
 
+/*
+ * True if the square is a shop entrance.
+ */
 bool cave_isshop(struct cave *c, int y, int x) {
 	return c->feat[y][x] >= FEAT_SHOP_HEAD
 	    && c->feat[y][x] <= FEAT_SHOP_TAIL;

src/defines.h
@@ -786,54 +786,6 @@ enum
 	(!(cave->info[Y][X] & (CAVE_WALL)))
 
 /*
- * Determine if a "legal" grid is a "clean" floor grid
- *
- * Line 1 -- forbid non-floors
- * Line 2 -- forbid normal objects
- */
-#define cave_clean_bold(Y,X) \
-	((cave->feat[Y][X] == FEAT_FLOOR) && \
-	 (cave->o_idx[Y][X] == 0))
-
-/*
- * Determine if a "legal" grid is an "empty" floor grid
- *
- * Line 1 -- forbid doors, rubble, seams, walls
- * Line 2 -- forbid player/monsters
- */
-#define cave_empty_bold(Y,X) \
-	(cave_floor_bold(Y,X) && \
-	 (cave->m_idx[Y][X] == 0))
-
-/*
- * Determine if a "legal" grid is an "naked" floor grid
- *
- * Line 1 -- forbid non-floors
- * Line 2 -- forbid normal objects
- * Line 3 -- forbid player/monsters
- */
-#define cave_naked_bold(Y,X) \
-	((cave->feat[Y][X] == FEAT_FLOOR) && \
-	 (cave->o_idx[Y][X] == 0) && \
-	 (cave->m_idx[Y][X] == 0))
-
-
-/*
- * Determine if a "legal" grid is "permanent"
- *
- * Line 1 -- perma-walls
- * Line 2-3 -- stairs
- * Line 4-5 -- shop doors
- */
-#define cave_perma_bold(Y,X) \
-	((cave->feat[Y][X] >= FEAT_PERM_EXTRA) || \
-	 ((cave->feat[Y][X] == FEAT_LESS) || \
-	  (cave->feat[Y][X] == FEAT_MORE)) || \
-	 ((cave->feat[Y][X] >= FEAT_SHOP_HEAD) && \
-	  (cave->feat[Y][X] <= FEAT_SHOP_TAIL)))
-
-
-/*
  * Determine if a "legal" grid is within "los" of the player
  *
  * Note the use of comparison to zero to force a "boolean" result

src/generate.c
@@ -963,7 +963,7 @@ static void vault_monsters(struct cave *c, int y1, int x1, int depth, int num)
 			scatter(&y, &x, y1, x1, d, 0);
 
 			/* Require "empty" floor grids */
-			if (!cave_empty_bold(y, x)) continue;
+			if (!cave_isempty(cave, y, x)) continue;
 
 			/* Place the monster (allow groups) */
 			pick_and_place_monster(c, y, x, depth, TRUE, TRUE, ORIGIN_DROP_SPECIAL);

src/monster/melee2.c
@@ -143,7 +143,7 @@ static bool summon_possible(int y1, int x1)
 			if (cave->feat[y][x] == FEAT_GLYPH) continue;
 
 			/* Require empty floor grid in line of sight */
-			if (cave_empty_bold(y, x) && los(y1, x1, y, x))
+			if (cave_isempty(cave, y, x) && los(y1, x1, y, x))
 			{
 				return (TRUE);
 			}
@@ -1020,7 +1020,7 @@ static bool find_hiding(struct monster *m_ptr, int *yp, int *xp)
 			if (!in_bounds_fully(y, x)) continue;
 
 			/* Skip occupied locations */
-			if (!cave_empty_bold(y, x)) continue;
+			if (!cave_isempty(cave, y, x)) continue;
 
 			/* Check for hidden, available grid */
 			if (!player_has_los_bold(y, x) && (clean_shot(fy, fx, y, x)))
@@ -1150,7 +1150,7 @@ static bool get_moves(struct cave *c, struct monster *m_ptr, int mm[5])
 				x2 = px + ddx_ddd[(tmp + i) & 7];
 				
 				/* Ignore filled grids */
-				if (!cave_empty_bold(y2, x2)) continue;
+				if (!cave_isempty(cave, y2, x2)) continue;
 				
 				/* Try to fill this hole */
 				break;

src/monster/mon-make.c
@@ -871,7 +871,7 @@ static bool place_new_monster_one(int y, int x, monster_race *race,
 	assert(race && race->name);
 
 	/* Require empty space */
-	if (!cave_empty_bold(y, x)) return (FALSE);
+	if (!cave_isempty(cave, y, x)) return (FALSE);
 
 	/* No creation on glyph of warding */
 	if (cave->feat[y][x] == FEAT_GLYPH) return (FALSE);
@@ -1042,7 +1042,7 @@ static bool place_new_monster_group(struct cave *c, int y, int x,
 			int my = hy + ddy_ddd[i];
 
 			/* Walls and Monsters block flow */
-			if (!cave_empty_bold(my, mx)) continue;
+			if (!cave_isempty(cave, my, mx)) continue;
 
 			/* Attempt to place another monster */
 			if (place_new_monster_one(my, mx, r_ptr, sleep, origin)) {
@@ -1147,7 +1147,7 @@ bool place_new_monster(struct cave *c, int y, int x, monster_race *race, bool sl
 			scatter(&ny, &nx, y, x, d, 0);
 
 			/* Require empty grids */
-			if (!cave_empty_bold(ny, nx)) continue;
+			if (!cave_isempty(cave, ny, nx)) continue;
 
 			/* Set the escort index */
 			place_monster_race = race;

src/monster/mon-util.c
@@ -1361,7 +1361,7 @@ int summon_specific(int y1, int x1, int lev, int type, int delay)
 		scatter(&y, &x, y1, x1, d, 0);
 
 		/* Require "empty" floor grid */
-		if (!cave_empty_bold(y, x)) continue;
+		if (!cave_isempty(cave, y, x)) continue;
 
 		/* Hack -- no summon on glyph of warding */
 		if (cave->feat[y][x] == FEAT_GLYPH) continue;
@@ -1433,7 +1433,7 @@ bool multiply_monster(const monster_type *m_ptr)
 		scatter(&y, &x, m_ptr->fy, m_ptr->fx, d, 0);
 
 		/* Require an "empty" floor grid */
-		if (!cave_empty_bold(y, x)) continue;
+		if (!cave_isempty(cave, y, x)) continue;
 
 		/* Create a new monster (awake, no groups) */
 		result = place_new_monster(cave, y, x, m_ptr->race, FALSE, FALSE,

src/spells1.c
@@ -230,7 +230,7 @@ void teleport_away(struct monster *m_ptr, int dis)
 			if (!in_bounds_fully(ny, nx)) continue;
 
 			/* Require "empty" floor space */
-			if (!cave_empty_bold(ny, nx)) continue;
+			if (!cave_isempty(cave, ny, nx)) continue;
 
 			/* Hack -- no teleport onto glyph of warding */
 			if (cave->feat[ny][nx] == FEAT_GLYPH) continue;
@@ -305,7 +305,7 @@ void teleport_player(int dis)
 			if (!in_bounds_fully(y, x)) continue;
 
 			/* Require "naked" floor space */
-			if (!cave_naked_bold(y, x)) continue;
+			if (!cave_isempty(cave, y, x)) continue;
 
 			/* No teleporting into vaults and such */
 			if (cave->info[y][x] & (CAVE_ICKY)) continue;
@@ -367,7 +367,7 @@ void teleport_player_to(int ny, int nx)
 		}
 
 		/* Accept "naked" floor grids */
-		if (cave_naked_bold(y, x)) break;
+		if (cave_isempty(cave, y, x)) break;
 
 		/* Occasionally advance the distance */
 		if (++ctr > (4 * dis * dis + 4 * dis + 1))
@@ -1328,7 +1328,7 @@ static bool project_f(int who, int r, int y, int x, int dam, int typ, bool obvio
 		case GF_MAKE_TRAP:
 		{
 			/* Require an "empty" floor grid */
-			if (!cave_empty_bold(y, x)) break;
+			if (!cave_isempty(cave, y, x)) break;
 
 			/* Create a trap */
 			create_trap(cave, y, x);

commit dd53aa515c1bd7f2bc3354aa5fd9998c851eb997

Fix drop_near() cave_canputitem check; bug introduced in 2a40e742 and found by magnate when running stats (and encountering a hang).

src/object/obj-util.c
@@ -2148,7 +2148,7 @@ void drop_near(struct cave *c, object_type *j_ptr, int chance, int y, int x, boo
 		}
 
 		/* Require floor space */
-		if (!cave_canputitem(cave, y, x)) continue;
+		if (!cave_canputitem(cave, ty, tx)) continue;
 
 		/* Bounce to that location */
 		by = ty;