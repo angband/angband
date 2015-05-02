angband/angband

commit 45e8c8a1ea79e6501cc733a9c922e3a4a75b44b9

Make GF_ICE use cold resistance for damage calcs

src/project-player.c
@@ -41,10 +41,12 @@ int adjust_dam(struct player *p, int type, int dam, aspect dam_aspect, int resis
 
 	/* If an actual player exists, get their actual resist */
 	if (p && p->race) {
-		resist = p->state.el_info[type].res_level;
+		/* Ice is a special case */
+		int res_type = GF_ICE ? GF_COLD: type;
+		resist = p->state.el_info[res_type].res_level;
 
 		/* Notice element stuff */
-		equip_notice_element(p, type);
+		equip_notice_element(p, res_type);
 	}
 
 	if (resist == 3) /* immune */

commit bdd602d3f826f0a3a6bad4bf989225de0a692096

Fix horrendous projection bug introduced in 45e8c8a

src/project-player.c
@@ -43,7 +43,7 @@ int adjust_dam(struct player *p, int type, int dam, aspect dam_aspect, int resis
 	/* If an actual player exists, get their actual resist */
 	if (p && p->race) {
 		/* Ice is a special case */
-		int res_type = GF_ICE ? GF_COLD: type;
+		int res_type = (type == GF_ICE) ? GF_COLD: type;
 		resist = p->state.el_info[res_type].res_level;
 
 		/* Notice element stuff */