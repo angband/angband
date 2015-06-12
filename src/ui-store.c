20be85c629e5e5102276a07408a8ac65aa22f4f0
Nick McConnell <nckmccnnll@gmail.com>
2015-06-12 22:03:19 +1000

    Fix shop messages again

--- src/ui-store.c
+++ src/ui-store.c
@@ -776,6 +776,10 @@ static bool store_process_command_key(struct keypress kp)
 {
 	int cmd = 0;
 
+	/* Hack -- no flush needed */
+	prt("", 0, 0);
+	msg_flag = FALSE;
+
 	/* Process the keycode */
 	switch (kp.code) {
 		case 'T': /* roguelike */
