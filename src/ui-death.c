eb01411d08e050e3afb5973defab2783ee9eba7c
github-ben@chickenonmyplate.com <ben@inkstonehq.com>
2016-03-27 19:20:08 -0700

    Change tomb preposition for date.

--- src/ui-death.c
+++ src/ui-death.c
@@ -103,7 +103,7 @@ static void print_tomb(void)
 
 	line++;
 
-	put_str_centred(line++, 8, 8+31, "by %-.24s", ctime(&death_time));
+	put_str_centred(line++, 8, 8+31, "on %-.24s", ctime(&death_time));
 }
 
 
