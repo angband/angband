11e1b29ade69b895b40f3595e0ac4b11a84c1975
Andi Sidwell <andi@takkaria.org>
2016-04-23 02:40:38 +0100

    Debug mode trap creation now prompts for name

--- src/wiz-debug.c
+++ src/wiz-debug.c
@@ -41,6 +41,7 @@
 #include "player-util.h"
 #include "project.h"
 #include "target.h"
+#include "trap.h"
 #include "ui-command.h"
 #include "ui-event.h"
 #include "ui-display.h"
@@ -2250,12 +2251,25 @@ void get_debug_command(void)
 		/* Create a trap */
 		case 'T':
 		{
-			if (!square_isfloor(cave, player->py, player->px))
+			if (!square_isfloor(cave, player->py, player->px)) {
 				msg("You can't place a trap there!");
-			else if (player->depth == 0)
+				break;
+			} else if (player->depth == 0) {
 				msg("You can't place a trap in the town!");
-			else
-				square_add_trap(cave, player->py, player->px);
+				break;
+			}
+
+			char buf[40];
+			if (!get_string("Create which trap? ", buf, sizeof(buf)))
+				break;
+
+			struct trap_kind *trap = lookup_trap(buf);
+			if (trap) {
+				place_trap(cave, player->py, player->px, trap->tidx, 0);
+			} else {
+				msg("Trap not found.");
+			}
+
 			break;
 		}
 
