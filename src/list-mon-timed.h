d33feb579c1507a2f827fdba57e4c14d3332ff00
Andi Sidwell <andi@takkaria.org>
2017-03-19 20:11:26 +0000

    Tweak maximum durations

--- src/list-mon-timed.h
+++ src/list-mon-timed.h
@@ -14,10 +14,10 @@
  */
 /*    name  gets_save  can_stack  resist_flag  max_time  message_begin           message_end             message_increase       */
 MON_TMD(SLEEP,  true,   false,    RF_NO_SLEEP, 10000,    MON_MSG_FALL_ASLEEP,    MON_MSG_WAKES_UP,       0                      )
-MON_TMD(STUN,   true,   true,     RF_NO_STUN,  200,      MON_MSG_DAZED,          MON_MSG_NOT_DAZED,      MON_MSG_MORE_DAZED     )
-MON_TMD(CONF,   true,   true,     RF_NO_CONF,  200,      MON_MSG_CONFUSED,       MON_MSG_NOT_CONFUSED,   MON_MSG_MORE_CONFUSED  )
+MON_TMD(STUN,   true,   true,     RF_NO_STUN,  50,       MON_MSG_DAZED,          MON_MSG_NOT_DAZED,      MON_MSG_MORE_DAZED     )
+MON_TMD(CONF,   true,   true,     RF_NO_CONF,  50,       MON_MSG_CONFUSED,       MON_MSG_NOT_CONFUSED,   MON_MSG_MORE_CONFUSED  )
 MON_TMD(FEAR,   true,   true,     RF_NO_FEAR,  10000,    MON_MSG_FLEE_IN_TERROR, MON_MSG_NOT_AFRAID,     MON_MSG_MORE_AFRAID    )
 MON_TMD(SLOW,   true,   true,     RF_NO_SLOW,  50,       MON_MSG_SLOWED,         MON_MSG_NOT_SLOWED,     MON_MSG_MORE_SLOWED    )
 MON_TMD(FAST,   false,  true,     0,           50,       MON_MSG_HASTED,         MON_MSG_NOT_HASTED,     MON_MSG_MORE_HASTED    )
-MON_TMD(HOLD,   false,  false,    RF_NO_SLEEP, 20,       MON_MSG_HELD,           MON_MSG_NOT_HELD,       0                      )
+MON_TMD(HOLD,   false,  false,    RF_NO_SLEEP, 50,       MON_MSG_HELD,           MON_MSG_NOT_HELD,       0                      )
 MON_TMD(MAX,    true,   true,     0,           0,        0,                      0,                      0                      )
