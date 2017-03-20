894150408b29faab019bd0a35b841dafe4b10ed9
Andi Sidwell <andi@takkaria.org>
2017-03-20 01:22:16 +0000

    Add new NO_HOLD flag for monsters immune to holding

--- src/list-mon-timed.h
+++ src/list-mon-timed.h
@@ -20,5 +20,5 @@ MON_TMD(CONF,   false,  MAX,   RF_NO_CONF,  50,       MON_MSG_CONFUSED,       MO
 MON_TMD(FEAR,   true,   INCR,  RF_NO_FEAR,  10000,    MON_MSG_FLEE_IN_TERROR, MON_MSG_NOT_AFRAID,     MON_MSG_MORE_AFRAID    )
 MON_TMD(SLOW,   false,  INCR,  RF_NO_SLOW,  50,       MON_MSG_SLOWED,         MON_MSG_NOT_SLOWED,     MON_MSG_MORE_SLOWED    )
 MON_TMD(FAST,   false,  INCR,  0,           50,       MON_MSG_HASTED,         MON_MSG_NOT_HASTED,     MON_MSG_MORE_HASTED    )
-MON_TMD(HOLD,   false,  MAX,   RF_NO_SLEEP, 50,       MON_MSG_HELD,           MON_MSG_NOT_HELD,       0                      )
+MON_TMD(HOLD,   false,  MAX,   RF_NO_HOLD,  50,       MON_MSG_HELD,           MON_MSG_NOT_HELD,       0                      )
 MON_TMD(MAX,    true,   INCR,  0,           0,        0,                      0,                      0                      )
