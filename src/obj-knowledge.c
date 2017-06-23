913f21dda3c5c9344c506b41e64c16419783bcd5
Nick McConnell <nckmccnnll@gmail.com>
2017-06-24 08:26:03 +1000

    Learn object weight in object_set_base_known(), fixing store prices

--- src/obj-knowledge.c
+++ src/obj-knowledge.c
@@ -772,6 +772,7 @@ void object_set_base_known(struct object *obj)
 	obj->known->kind = obj->kind;
 	obj->known->tval = obj->tval;
 	obj->known->sval = obj->sval;
+	obj->known->weight = obj->weight;
 	obj->known->number = obj->number;
 
 	/* Generic dice and ac, to_h for armor, and launcher multipliers */
