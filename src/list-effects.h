db75d1ded2772e5cca46a6793512f65fbf3dbcd8
Nick McConnell <nckmccnnll@gmail.com>
2018-01-27 23:15:33 +1100

    Add READ_MINDS effect

--- src/list-effects.h
+++ src/list-effects.h
@@ -40,6 +40,7 @@ EFFECT(RECALL,						false,	NULL,		0,		EFINFO_NONE,	"returns you from the dungeon
 EFFECT(DEEP_DESCENT,				false,	NULL,		0,		EFINFO_NONE,	"teleports you up to five dungeon levels lower than the lowest point you have reached so far")
 EFFECT(ALTER_REALITY,				false,	NULL,		0,		EFINFO_NONE,	"creates a new dungeon level")
 EFFECT(MAP_AREA,					false,	NULL,		0,		EFINFO_NONE,	"maps the area around you")
+EFFECT(READ_MINDS,					false,	NULL,		0,		EFINFO_NONE,	"maps the area around recently detected monsters")
 EFFECT(DETECT_TRAPS,				false,	NULL,		0,		EFINFO_NONE,	"detects traps nearby")
 EFFECT(DETECT_DOORS,				false,	NULL,		0,		EFINFO_NONE,	"detects doors nearby")
 EFFECT(DETECT_STAIRS,				false,	NULL,		0,		EFINFO_NONE,	"detects stairs nearby")
