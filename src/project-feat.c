c9a2d70f6ad4f8cdd6ba550440bd913605d08624
Nick McConnell <nckmccnnll@gmail.com>
2018-03-15 08:43:04 +1100

    Add TURN_LIVING

--- src/project-feat.c
+++ src/project-feat.c
@@ -606,6 +606,10 @@ static void project_feature_handler_TURN_EVIL(project_feature_handler_context_t 
 {
 }
 
+static void project_feature_handler_TURN_LIVING(project_feature_handler_context_t *context)
+{
+}
+
 static void project_feature_handler_TURN_ALL(project_feature_handler_context_t *context)
 {
 }
