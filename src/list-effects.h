cd5727f3120792877b4b4780d93fdc1112febe49
Nick McConnell <nckmccnnll@gmail.com>
2018-03-20 22:59:24 +1100

    Fix earthquakes

--- src/list-effects.h
+++ src/list-effects.h
@@ -70,7 +70,7 @@ EFFECT(TELEPORT_TO,					false,	NULL,		0,		EFINFO_NONE,	"teleports you to a monst
 EFFECT(TELEPORT_LEVEL,				false,	NULL,		0,		EFINFO_NONE,	"teleports you one level up or down")
 EFFECT(RUBBLE,						false,	NULL,		0,		EFINFO_NONE,	"causes rubble to fall around you")
 EFFECT(DESTRUCTION,					false,	NULL,		1,		EFINFO_QUAKE,	"destroys an area around you in the shape of a circle radius %d, and blinds you for 1d10+10 turns")
-EFFECT(EARTHQUAKE,					true,	NULL,		1,		EFINFO_QUAKE,	"causes an earthquake around you of radius %d")
+EFFECT(EARTHQUAKE,					false,	NULL,		1,		EFINFO_QUAKE,	"causes an earthquake around you of radius %d")
 EFFECT(LIGHT_LEVEL,					false,	NULL,		0,		EFINFO_NONE,	"completely lights up and magically maps the level")
 EFFECT(LIGHT_AREA,					false,	NULL,		0,		EFINFO_NONE,	"lights up the surrounding area")
 EFFECT(DARKEN_AREA,					false,	NULL,		0,		EFINFO_NONE,	"darkens the surrounding area")
