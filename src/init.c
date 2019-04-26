bc76dd91ac9739663d99d445a9657ab815fad21c
Nick McConnell <nckmccnnll@gmail.com>
2019-04-27 09:23:29 +1000

    Make p_race.txt more readable, adjust experience factors

--- src/init.c
+++ src/init.c
@@ -1995,12 +1995,26 @@ static enum parser_error parse_p_race_skill_dig(struct parser *p) {
 	return PARSE_ERROR_NONE;
 }
 
-static enum parser_error parse_p_race_info(struct parser *p) {
+static enum parser_error parse_p_race_hitdie(struct parser *p) {
 	struct player_race *r = parser_priv(p);
 	if (!r)
 		return PARSE_ERROR_MISSING_RECORD_HEADER;
 	r->r_mhp = parser_getint(p, "mhp");
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_p_race_exp(struct parser *p) {
+	struct player_race *r = parser_priv(p);
+	if (!r)
+		return PARSE_ERROR_MISSING_RECORD_HEADER;
 	r->r_exp = parser_getint(p, "exp");
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_p_race_infravision(struct parser *p) {
+	struct player_race *r = parser_priv(p);
+	if (!r)
+		return PARSE_ERROR_MISSING_RECORD_HEADER;
 	r->infra = parser_getint(p, "infra");
 	return PARSE_ERROR_NONE;
 }
@@ -2010,8 +2024,15 @@ static enum parser_error parse_p_race_history(struct parser *p) {
 	if (!r)
 		return PARSE_ERROR_MISSING_RECORD_HEADER;
 	r->history = findchart(histories, parser_getuint(p, "hist"));
-	r->b_age = parser_getint(p, "b-age");
-	r->m_age = parser_getint(p, "m-age");
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_p_race_age(struct parser *p) {
+	struct player_race *r = parser_priv(p);
+	if (!r)
+		return PARSE_ERROR_MISSING_RECORD_HEADER;
+	r->b_age = parser_getint(p, "base_age");
+	r->m_age = parser_getint(p, "mod_age");
 	return PARSE_ERROR_NONE;
 }
 
@@ -2116,8 +2137,11 @@ struct parser *init_parse_p_race(void) {
 	parser_reg(p, "skill-shoot int shoot", parse_p_race_skill_shoot);
 	parser_reg(p, "skill-throw int throw", parse_p_race_skill_throw);
 	parser_reg(p, "skill-dig int dig", parse_p_race_skill_dig);
-	parser_reg(p, "info int mhp int exp int infra", parse_p_race_info);
-	parser_reg(p, "history uint hist int b-age int m-age", parse_p_race_history);
+	parser_reg(p, "hitdie int mhp", parse_p_race_hitdie);
+	parser_reg(p, "exp int exp", parse_p_race_exp);
+	parser_reg(p, "infravision int infra", parse_p_race_infravision);
+	parser_reg(p, "history uint hist", parse_p_race_history);
+	parser_reg(p, "age int base_age int mod_age", parse_p_race_age);
 	parser_reg(p, "height int base_hgt int mod_hgt", parse_p_race_height);
 	parser_reg(p, "weight int base_wgt int mod_wgt", parse_p_race_weight);
 	parser_reg(p, "obj-flags ?str flags", parse_p_race_obj_flags);
