9197c8a6b364fd8d377993f8014d2fe4af63b6b5
Nick McConnell <nckmccnnll@gmail.com>
2019-06-17 07:31:43 +1000

    Increase ranger extra shots *again*

--- src/player-calcs.c
+++ src/player-calcs.c
@@ -2163,7 +2163,7 @@ void calc_bonuses(struct player *p, struct player_state *state, bool known_only,
 			state->num_shots += extra_shots;
 			state->ammo_mult += extra_might;
 			if (player_has(p, PF_FAST_SHOT) && (state->ammo_tval == TV_ARROW)) {
-				state->num_shots += p->lev / 5;
+				state->num_shots += p->lev / 3;
 			}
 		}
 
