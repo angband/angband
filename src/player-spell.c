c348323e75d8e46f072d24af8696ebf8780a0317
Nick McConnell <nckmccnnll@gmail.com>
2020-01-25 13:43:41 +1100

    Halve displayed SPHERE damage, fixes #4208

--- src/player-spell.c
+++ src/player-spell.c
@@ -587,6 +587,21 @@ static void spell_effect_append_value_info(const struct effect *effect,
 			if (rv.m_bonus) special = "random";
 			break;
 		case EF_SPHERE:
+			/* Halve damage */
+			rv.base /= 2;
+			rv.sides /= 2;
+
+			/* Append radius */
+			if (effect->radius) {
+				int rad = effect->radius;
+				if (effect->other) {
+					rad += player->lev / effect->other;
+				}
+				special = format(", rad %d", rad);
+			} else {
+				special = ", rad 2";
+			}
+			break;
 		case EF_BALL:
 			/* Append radius */
 			if (effect->radius) {
