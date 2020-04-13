c170790471486500d78603b698c4ca24d91bc5ee
Eric Branlund <ebranlund@fastmail.com>
2020-04-12 22:10:23 -0700

    Added check_for_inscrip_with_int() to check an object's inscription for a string followed by an integer value without a sign character.

--- src/tests/object/util.c
+++ src/tests/object/util.c
@@ -16,10 +16,13 @@ int setup_tests(void **state) {
 	z_info->fuel_torch = 5000;
 	z_info->fuel_lamp = 15000;
 	z_info->default_lamp = 7500;
+
+	quarks_init();
     return 0;
 }
 
 int teardown_tests(void **state) {
+	quarks_free();
 	mem_free(z_info);
 	return 0;
 }
@@ -60,8 +63,54 @@ int test_obj_can_refill(void *state) {
     ok;
 }
 
+/* Test basic functionality of check_for_inscrip_with_int(). */
+int test_basic_check_for_inscrip_with_int(void *state) {
+    struct object obj;
+    int dummy = 8974;
+    int inarg;
+
+    /* No inscription should return zero and leave inarg unchanged. */
+    memset(&obj, 0, sizeof(obj));
+    inarg = dummy;
+    eq(check_for_inscrip_with_int(&obj, "=g", &inarg), 0);
+    eq(inarg, dummy);
+
+    /* An inscription not containing the search string should return zero and
+     * leave inarg unchanged. */
+    obj.note = quark_add("@m1@b1@G1");
+    inarg = dummy;
+    eq(check_for_inscrip_with_int(&obj, "=g", &inarg), 0);
+    eq(inarg, dummy);
+
+    /* An inscription containing the search string but not followed by an
+     * integer should return zero and leave inarg unchanged. */
+    obj.note = quark_add("=g@m1@b1@G1");
+    inarg = dummy;
+    eq(check_for_inscrip_with_int(&obj, "=g", &inarg), 0);
+    eq(inarg, dummy);
+
+    /* An inscription containing one instance of the search string followed
+     * by a nonnegative integer should return one and set inarg to the value
+     * of the integer. */
+    obj.note = quark_add("=g5@m1@b1@G1");
+    inarg = dummy;
+    eq(check_for_inscrip_with_int(&obj, "=g", &inarg), 1);
+    eq(inarg, 5);
+
+    /* An inscription containing two instances of the search string followed
+     * by a nonnegative integer should return two and set inarg to the value
+     * of the integer following the first instance. */
+    obj.note = quark_add("@m1@b1=g8@G1=g5");
+    inarg = dummy;
+    eq(check_for_inscrip_with_int(&obj, "=g", &inarg), 2);
+    eq(inarg, 8);
+
+    ok;
+}
+
 const char *suite_name = "object/util";
 struct test tests[] = {
     { "obj_can_refill", test_obj_can_refill },
+    { "basic_check_for_inscrip_with_uint", test_basic_check_for_inscrip_with_int },
     { NULL, NULL }
 };
