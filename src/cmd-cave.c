4489981e6752973225e7e47c6753a0c8d0f63079
Cuboideb <cuboideb@gmail.com>
2020-05-24 12:52:56 -0300

    The alter command closes doors and open chests.

--- src/cmd-cave.c
+++ src/cmd-cave.c
@@ -870,6 +870,7 @@ void do_cmd_alter_aux(int dir)
 {
 	struct loc grid;
 	bool more = false;
+	struct object *obj_chest;
 
 	/* Get location */
 	grid = loc_sum(player->grid, ddgrid[dir]);
@@ -882,6 +883,9 @@ void do_cmd_alter_aux(int dir)
 		/* Get location */
 		grid = loc_sum(player->grid, ddgrid[dir]);
 	}
+
+	/* Check for chest */
+	obj_chest = chest_check(grid, CHEST_OPENABLE);
 
 	/* Action depends on what's there */
 	if (square(cave, grid).mon > 0) {
@@ -896,6 +900,12 @@ void do_cmd_alter_aux(int dir)
 	} else if (square_isdisarmabletrap(cave, grid)) {
 		/* Disarm traps */
 		more = do_cmd_disarm_aux(grid);
+	} else if (obj_chest) {
+        	/* Open chest */
+        	more = do_cmd_open_chest(grid, obj_chest);
+	} else if (square_isopendoor(cave, grid)) {
+		/* Close door */
+        	more = do_cmd_close_aux(grid);
 	} else {
 		/* Oops */
 		msg("You spin around.");
