e228c45ed41256d40b56448c05db7cc0b6efd2a9
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2020-06-30 22:46:18 +0300

    trampling of shapechangers

--- src/mon-move.c
+++ src/mon-move.c
@@ -126,8 +126,8 @@ static bool monster_can_smell(struct chunk *c, struct monster *mon)
 static int compare_monsters(const struct monster *mon1,
 							const struct monster *mon2)
 {
-	u32b mexp1 = mon1->race->mexp;
-	u32b mexp2 = mon2->race->mexp;
+	u32b mexp1 = (mon1->original_race) ? mon1->original_race->mexp : mon1->race->mexp;
+	u32b mexp2 = (mon2->original_race) ? mon2->original_race->mexp : mon2->race->mexp;
 
 	/* Compare */
 	if (mexp1 < mexp2) return (-1);
