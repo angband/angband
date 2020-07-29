7b05ca8017ac8990148af81a316a6f68d1cb7c1c
Eric Branlund <ebranlund@fastmail.com>
2020-07-28 20:16:50 -0700

    Only forget floors when going dark in cave_illuminate().  That's one way to resolve https://github.com/NickMcConnell/FirstAgeAngband/issues/49 .

--- src/cave-map.c
+++ src/cave-map.c
@@ -582,7 +582,9 @@ void cave_illuminate(struct chunk *c, bool daytime)
 				if(light) square_memorize(c, grid);
 			} else if (!square_isbright(c, grid)) {
 				sqinfo_off(square(c, grid)->info, SQUARE_GLOW);
-				square_forget(c, grid);
+				/* Hack -- like cave_unlight(), forget "boring" grids */
+				if (square_isfloor(c, grid))
+					square_forget(cave, grid);
 			}
 		}
 	}
