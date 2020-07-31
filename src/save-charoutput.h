e3b74eac9b0b1266fc3d875e6bde00d4a71349fa
Eric Branlund <ebranlund@fastmail.com>
2020-07-31 10:12:40 -0700

    Is the initial draft of changes to save CharOutput.txt for angband.live.

---
+++ src/save-charoutput.h
@@ -0,0 +1,24 @@
+/**
+ * \file save-charoutput.h
+ * \brief Declare interface for saving CharOutput.txt for angband.live
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ */
+#ifndef INCLUDED_SAVE_CHAROUTPUT_H
+#define INCLUDED_SAVE_CHAROUTPUT_H
+
+#include "h-basic.h"
+
+bool save_charoutput(void);
+
+#endif /* INCLUDED_SAVE_CHAROUTPUT_H */
+
