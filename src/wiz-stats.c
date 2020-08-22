031c95edb865064e1c9c6b9fbf88aefdbe9ce54c
Eric Branlund <ebranlund@fastmail.com>
2020-08-21 20:11:08 -0700

    For statistics generation with clearing and regneration of the randart set, use the standard artifacts as the starting point for the randart set at each iteration to mimic normal randart generation and avoid crashes in randart generation during the statistics generation.  Also, once done with statistics generation in that mode, reset to the artifact set from before statistics generation.

--- src/wiz-stats.c
+++ src/wiz-stats.c
@@ -24,6 +24,7 @@
 #include "init.h"
 #include "mon-make.h"
 #include "monster.h"
+#include "obj-init.h"
 #include "obj-pile.h"
 #include "obj-randart.h"
 #include "obj-tval.h"
@@ -1531,6 +1532,11 @@ static void clearing_stats(void)
 			/* Get seed */
 			int seed_randart = randint0(0x10000000);
 
+			/* Restore the standard artifacts */
+			cleanup_parser(&randart_parser);
+			deactivate_randart_file();
+			run_parser(&artifact_parser);
+
 			/* regen randarts */
 			do_randart(seed_randart, false);
 		}
@@ -1551,6 +1557,18 @@ static void clearing_stats(void)
 		}
 
 		msg("Iteration %d complete",iter);
+	}
+
+	/* Restore original artifacts */
+	if (regen) {
+		cleanup_parser(&randart_parser);
+		if (OPT(player, birth_randarts)) {
+			activate_randart_file();
+			run_parser(&randart_parser);
+			deactivate_randart_file();
+		} else {
+			run_parser(&artifact_parser);
+		}
 	}
 
 	/* Print to file */
