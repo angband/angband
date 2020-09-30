91bf38a65b0cdfe7dd9063374d9749455a277646
Eric Branlund <ebranlund@fastmail.com>
2020-09-30 13:38:07 -0700

    Removed extraneous semicolon.

--- src/main-cocoa.m
+++ src/main-cocoa.m
@@ -354,7 +354,7 @@ static __strong AngbandSoundCatalog* gSharedSounds = nil;
     if (gSharedSounds == nil) {
 	gSharedSounds = [[AngbandSoundCatalog alloc] init];
     }
-    return gSharedSounds;;
+    return gSharedSounds;
 }
 
 + (void)clearSharedSounds {
