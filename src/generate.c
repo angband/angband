1fe9f587228c6f6ea1682e953732576fcaccad54
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2020-10-13 02:11:11 -0700

    Adjust test so hard centre levels (last profile) won't be generated… (#4561)
    * Adjusted test so hard centre levels (last profile) won't be generated at less than their minimum depth.
    
    * Plugged the much rarer case where the last profile could be used at less than its minimum depth.

--- src/generate.c
+++ src/generate.c
@@ -821,10 +821,10 @@ const struct cave_profile *choose_profile(struct player *p)
 				if (p->depth < profile->min_level) continue;
 				if (profile->cutoff >= pick) break;
 			}
-			if (profile) break;
+			if (profile && i < z_info->profile_max) break;
 			tries--;
 		}
-		if (!profile) {
+		if (!profile || !tries) {
 			profile = find_cave_profile("classic");
 		}
 	}
