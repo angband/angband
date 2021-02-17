angband/angband

commit 15cae6653301aaae07f9f5282c5c2b3749e85ceb

Account for throwing weapons in the quiver within pack_slots_used() and … (#4613)
* For consistency with calc_inventory(), account for throwing weapons in the quiver within pack_slots_used() and quiver_absorb_num().  Appears to resolve https://github.com/angband/angband/issues/4606 .

* Don't check inscription in quiver_absorb_num():  want an uninscribed dagger to be able to stack with an inscribed one already in the quiver.

src/obj-gear.c
@@ -171,10 +171,12 @@ int pack_slots_used(struct player *p)
 		/* Equipment doesn't count */
 		if (!object_is_equipped(p->body, obj)) {
 			/* Check if it is in the quiver */
-			if (tval_is_ammo(obj)) {
+			if (tval_is_ammo(obj) ||
+					of_has(obj->flags, OF_THROWING)) {
 				for (i = 0; i < z_info->quiver_size; i++) {
 					if (p->upkeep->quiver[i] == obj) {
-						quiver_ammo += obj->number;
+						quiver_ammo += obj->number *
+							(tval_is_ammo(obj) ? 1 : 5);
 						found = true;
 						break;
 					}
@@ -490,17 +492,20 @@ struct object *gear_object_for_use(struct object *obj, int num, bool message,
  */
 static int quiver_absorb_num(const struct object *obj)
 {
-	/* Must be ammo */
-	if (tval_is_ammo(obj)) {
+	/* Must be ammo or good for throwing */
+	if (tval_is_ammo(obj) || of_has(obj->flags, OF_THROWING)) {
 		int i, quiver_count = 0, space_free = 0;
 
 		/* Count the current space this object could go into */
 		for (i = 0; i < z_info->quiver_size; i++) {
 			struct object *quiver_obj = player->upkeep->quiver[i];
 			if (quiver_obj) {
-				quiver_count += quiver_obj->number;
+				int mult = tval_is_ammo(quiver_obj) ? 1 : 5;
+
+				quiver_count += quiver_obj->number * mult;
 				if (object_stackable(quiver_obj, obj, OSTACK_PACK))
-					space_free += z_info->quiver_slot_size - quiver_obj->number;
+					space_free += z_info->quiver_slot_size
+						- quiver_obj->number * mult;
 			} else {
 				space_free += z_info->quiver_slot_size;
 			}
@@ -514,7 +519,8 @@ static int quiver_absorb_num(const struct object *obj)
 
 			/* Return the number, or the number that will fit */
 			space_free = MIN(space_free, z_info->quiver_slot_size - quiver_count);
-			return MIN(obj->number, space_free);
+			return MIN(obj->number,
+				space_free / (tval_is_ammo(obj) ? 1 : 5));
 		}
 	}
 

commit 4d216322b0391c33a0b7a27ac97682c90a5a62f3

Tighten the tests for throwing weapons in quiver_absorb_num() so they match the inscription requirements imposed by calc_inventory().  Should resolve https://github.com/angband/angband/issues/4642 , which was a regression introduced by https://github.com/angband/angband/commit/15cae6653301aaae07f9f5282c5c2b3749e85ceb .

src/obj-gear.c
@@ -492,8 +492,10 @@ struct object *gear_object_for_use(struct object *obj, int num, bool message,
  */
 static int quiver_absorb_num(const struct object *obj)
 {
+	bool throwing = of_has(obj->flags, OF_THROWING);
+
 	/* Must be ammo or good for throwing */
-	if (tval_is_ammo(obj) || of_has(obj->flags, OF_THROWING)) {
+	if (tval_is_ammo(obj) || throwing) {
 		int i, quiver_count = 0, space_free = 0;
 
 		/* Count the current space this object could go into */
@@ -506,8 +508,22 @@ static int quiver_absorb_num(const struct object *obj)
 				if (object_stackable(quiver_obj, obj, OSTACK_PACK))
 					space_free += z_info->quiver_slot_size
 						- quiver_obj->number * mult;
-			} else {
+			} else if (!throwing) {
 				space_free += z_info->quiver_slot_size;
+			} else if (obj->note) {
+				/*
+				 * Per calc_inventory(), throwing weapons
+				 * will be added to the quiver if inscribed
+				 * to go into an unoccupied slot.  The
+				 * inscription test should match what
+				 * calc_inventory() uses.
+				 */
+				const char *s = strchr(quark_str(obj->note), '@');
+
+				if (s && (s[1] == 'f' || s[1] == 'v') &&
+						s[2] - '0' == i) {
+					space_free += z_info->quiver_slot_size;
+				}
 			}
 		}
 