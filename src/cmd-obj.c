b6c6f9614438cb26c54fe6cbfe33b71c12715ad0
Eric Branlund <ebranlund@fastmail.com>
2021-03-19 10:52:36 -0700

    Allow fuel to be drawn from the quiver since flasks of oil can be stored there if inscribed.  Reported by will_asher in http://angband.oook.cz/forum/showpost.php?p=151764&postcount=60 .

--- src/cmd-obj.c
+++ src/cmd-obj.c
@@ -970,7 +970,7 @@ void do_cmd_refill(struct command *cmd)
 			"Refuel with with fuel source? ",
 			"You have nothing you can refuel with.",
 			obj_can_refill,
-			USE_INVEN | USE_FLOOR) != CMD_OK) return;
+			USE_INVEN | USE_FLOOR | USE_QUIVER) != CMD_OK) return;
 
 	/* Check what we're wielding. */
 	if (!light || !tval_is_light(light)) {
