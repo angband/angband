4f3f7fb956de3b3d5752381fed48768c5f700f71
Eric Branlund <ebranlund@fastmail.com>
2021-06-03 10:42:07 -0700

    Fix so runs successfully in the GitHub tests workflow.

--- src/tests/effects/chain.c
+++ src/tests/effects/chain.c
@@ -30,6 +30,10 @@ struct simple_effect {
 int setup_tests(void **state) {
 	set_file_paths();
 	init_angband();
+#ifdef UNIX
+	/* Necessary for creating the randart file. */
+	create_needed_dirs();
+#endif
 	/* Set up the player so there's a target available for the effects. */
 	if (!player_make_simple(NULL, NULL, "Tester")) {
 		cleanup_angband();
