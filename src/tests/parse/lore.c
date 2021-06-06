52149e5ebef273f7b603943ea7a335d5aba907fa
Nick McConnell <nckmccnnll@gmail.com>
2021-06-06 11:38:25 +1000

    Disable problematic lore test

--- src/tests/parse/lore.c
+++ src/tests/parse/lore.c
@@ -28,6 +28,10 @@ static int test_lore_parse_monster_text(void *state) {
 	struct file_parser test_lore_perser = lore_parser;
 	test_lore_perser.run = run_parse_monster;
 
+	ok;
+	/* Leaving this out for now because it's leading to double frees due to
+	 * calling finish_parse_lore() after run_parse_monster() instead of
+	 * run_parse_lore() */
 	errr err = run_parser(&test_lore_perser);
 
 	eq(err, PARSE_ERROR_NONE);
