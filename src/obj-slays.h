f45f3b5c9de2a5d71b30e8a6c1fb9710e823f345
Eric Branlund <ebranlund@fastmail.com>
2021-08-02 19:59:38 -0700

    Split off the learning and lore part from improve_attack_modifier() into their own functions.  For convenience, there's one function for melee attacks (learn_brand_slay_from_melee()), one for ranged attacks with a launcher (learn_brand_slay_from_launch()), and one for thrown attacks (learn_brand_slay_from_throw()).  Keep the declarations and implementations for those in obj-slays.{h,c} since they combine both object and monster learning and so aren't a natural fit for obj-knowledge.{h,c} or mon-lore.{h,c}.  Resolves https://github.com/angband/angband/issues/3668 .  Add a player argument to improve_attack_modifier(), player_has_temporary_brand(), and player_has_temporary_slay().  Add a Boolean argument to get_monster_brand_multiplier() to flag whether or not O-combat is desired.  Those changes resolve https://github.com/angband/angband/issues/4993 , https://github.com/angband/angband/issues/4994 , and https://github.com/angband/angband/issues/4995 (all related to https://github.com/angband/angband/issues/4934 ) where some of the caller of those functions took player arguments but those functions accessed the player global.

--- src/obj-slays.h
+++ src/obj-slays.h
@@ -31,12 +31,20 @@ bool append_random_brand(bool **current, struct brand **brand);
 bool append_random_slay(bool **current, struct slay **slay);
 int brand_count(const bool *brands_on);
 int slay_count(const bool *slays_on);
-bool player_has_temporary_brand(int idx);
-bool player_has_temporary_slay(int idx);
-int get_monster_brand_multiplier(const struct monster *mon, const struct brand *b);
-void improve_attack_modifier(struct object *obj, const struct monster *mon, 
-							 int *brand_used, int *slay_used, char *verb,
-							 bool range);
+bool player_has_temporary_brand(const struct player *p, int idx);
+bool player_has_temporary_slay(const struct player *p, int idx);
+int get_monster_brand_multiplier(const struct monster *mon,
+	const struct brand *b, bool is_o_combat);
+void improve_attack_modifier(struct player *p, struct object *obj,
+	const struct monster *mon, int *brand_used, int *slay_used, char *verb,
+	bool range);
 bool react_to_slay(struct object *obj, const struct monster *mon);
+
+void learn_brand_slay_from_melee(struct player *p, struct object *weapon,
+	const struct monster *mon);
+void learn_brand_slay_from_launch(struct player *p, struct object *missile,
+	struct object *launcher, const struct monster *mon);
+void learn_brand_slay_from_throw(struct player *p, struct object *missile,
+	const struct monster *mon);
 
 #endif /* OBJECT_SLAYS_H */
