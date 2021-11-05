2e485939192ee25e8263f9e660c9dedc3ef1d19f
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2021-11-05 14:42:24 -0700

    Add unit tests for combine_pack() (#5114)
    * Add unit tests for combine_pack().
    
    * Extend combine_pack() test case so object_absorb_partial()  will be called internally.

--- src/tests/player/suite.mk
+++ src/tests/player/suite.mk
@@ -1,5 +1,6 @@
 TESTPROGS += player/birth \
              player/calc-inventory \
+             player/combine-pack \
              player/history \
              player/inven-carry-num \
              player/inven-wield \
