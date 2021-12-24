angband/angband

commit 79de41ebf968b0f50aa0253601e228c0e6369c3e

GCU: fix regression that prevented player's color table from taking effect

src/main-gcu.c
@@ -793,8 +793,8 @@ static errr Term_xtra_gcu(int n, int v) {
 		/* Delay */
 		case TERM_XTRA_DELAY: if (v > 0) usleep(1000 * v); return 0;
 
-		/* React to events; nothing special is done */
-		case TERM_XTRA_REACT: return 0;
+		/* React to events */
+		case TERM_XTRA_REACT: handle_extended_color_tables(); return 0;
 	}
 
 	/* Unknown event */

commit 545a552a3ba5ca86d1cb00ee67ab81c4fa70a0be

GCU: preprocess out body, rather than entire declaration, of handle_extended_color_tables() if A_COLOR not set.  Fixes regression introduced by https://github.com/angband/angband/pull/5137 : will get a compiler error if A_COLOR not set.

src/main-gcu.c
@@ -727,11 +727,11 @@ static int create_color(int i, int scale) {
 }
 
 
-#ifdef A_COLOR
 /**
  * Adjust the color tables if there's more than 16 available.
  */
 static void handle_extended_color_tables(void) {
+#ifdef A_COLOR
 	if (COLORS == 256 || COLORS == 88) {
 		/* If we have more than 16 colors, find the best matches. These numbers
 		 * correspond to xterm/rxvt's builtin color numbers--they do not
@@ -776,8 +776,8 @@ static void handle_extended_color_tables(void) {
 			}
 		}
 	}
-}
 #endif
+}
 
 
 /**