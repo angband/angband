angband/angband

commit 3a29c447d18a8c1596f9bffbd6c0cc58b178c2eb

Print total number in pack/quiver when dropping, picking up, or consuming an item that's in the pack/quiver or ends up there.  If there's multiple stacks of the object, preface the printed slot index by "1st ".  Drop recombining messages except when a stack is completely combined with another.  Tries to resolve Bogatyr's complaint here, http://angband.oook.cz/forum/showpost.php?p=156776&postcount=151 , without resurrecting PowerWyrm's issue, https://github.com/angband/angband/issues/4882 .

src/obj-gear.c
@@ -174,6 +174,81 @@ bool object_is_in_quiver(struct player *p, const struct object *obj)
 }
 
 /**
+ * Get the total number of objects in the pack or quiver that are like the
+ * given object.
+ *
+ * \param player is the player whose inventory is used for the calculation.
+ * \param obj is the template for the objects to look for.
+ * \param ignore_inscrip if true, ignore the inscriptions when testing whether
+ * an object is similar; otherwise, test the inscriptions as well.
+ * \param first if not NULL, set to the first stack like obj (by ordering in
+ * the quiver or pack with quiver taking precedence over pack; if the pack
+ * and quiver haven't been computed, it will be the first non-equipped stack
+ * in the gear).
+ */
+uint16_t object_pack_total(struct player *p, const struct object *obj,
+		bool ignore_inscrip, struct object **first)
+{
+	uint16_t total = 0;
+	char first_label = '\0';
+	struct object *cursor;
+
+	if (first) {
+		*first = NULL;
+	}
+	for (cursor = p->gear; cursor; cursor = cursor->next) {
+		bool like;
+
+		if (cursor == obj) {
+			/*
+			 * object_similar() excludes cursor == obj so if
+			 * obj is not equipped, account for it here.
+			 */
+			like = !object_is_equipped(p->body, obj);
+		} else if (ignore_inscrip) {
+			like = object_similar(obj, cursor, OSTACK_PACK);
+		} else {
+			like = object_stackable(obj, cursor, OSTACK_PACK);
+		}
+		if (like) {
+			total += cursor->number;
+			if (first) {
+				char test_label = gear_to_label(p, cursor);
+
+				if (!*first) {
+					*first = cursor;
+					first_label = test_label;
+				} else {
+					if (test_label >= 'a'
+							&& test_label <= 'z') {
+						if (first_label == '\0'
+								|| (first_label >= 'a'
+								&& first_label <= 'z'
+								&& test_label < first_label)) {
+							*first = cursor;
+							first_label = test_label;
+						}
+					} else if (test_label >= '0'
+							&& test_label <= '9') {
+						if (first_label == '\0'
+								|| (first_label >= 'a'
+								&& first_label <= 'z')
+								|| (first_label >= '0'
+								&& first_label <= '9'
+								&& test_label < first_label)) {
+							*first = cursor;
+							first_label = test_label;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return total;
+}
+
+/**
  * Calculate the number of pack slots used by the current gear.
  *
  * Note that this function does not check that there are adequate slots in the
@@ -446,6 +521,7 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 	int num, bool message, bool *none_left)
 {
 	struct object *usable;
+	struct object *first_remainder = NULL;
 	char name[80];
 	char label = gear_to_label(p, obj);
 	bool artifact = (obj->known->artifact != NULL);
@@ -461,8 +537,21 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 		p->upkeep->total_weight -= (num * obj->weight);
 
 		if (message) {
+			uint16_t total;
+
+			if (object_is_equipped(p->body, obj)) {
+				total = obj->number;
+			} else {
+				total = object_pack_total(p, obj, false,
+					&first_remainder);
+				assert(total >= first_remainder->number);
+				if (total == first_remainder->number) {
+					first_remainder = NULL;
+				}
+			}
 			object_desc(name, sizeof(name), obj,
-				ODESC_PREFIX | ODESC_FULL, p);
+				ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
+				(total << 16), p);
 		}
 	} else {
 		if (message) {
@@ -470,11 +559,18 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 				object_desc(name, sizeof(name), obj,
 					ODESC_FULL | ODESC_SINGULAR, p);
 			} else {
-				/* Describe zero amount */
-				obj->number = 0;
+				uint16_t total = (object_is_equipped(p->body, obj)) ?
+					obj->number :
+					object_pack_total(p, obj, false, &first_remainder);
+
+				assert(total >= num);
+				total -= num;
+				if (!total || total <= first_remainder->number) {
+					first_remainder = NULL;
+				}
 				object_desc(name, sizeof(name), obj,
-					ODESC_PREFIX | ODESC_FULL, p);
-				obj->number = num;
+					ODESC_PREFIX | ODESC_FULL |
+					ODESC_ALTNUM | (total << 16), p);
 			}
 		}
 
@@ -498,10 +594,14 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 
 	/* Print a message if desired */
 	if (message) {
-		if (artifact)
+		if (artifact) {
 			msg("You no longer have the %s (%c).", name, label);
-		else
+		} else if (first_remainder) {
+			label = gear_to_label(p, first_remainder);
+			msg("You have %s (1st %c).", name, label);
+		} else {
 			msg("You have %s (%c).", name, label);
+		}
 	}
 
 	return usable;
@@ -754,9 +854,21 @@ void inven_carry(struct player *p, struct object *obj, bool absorb,
 
 	if (message) {
 		char o_name[80];
+		struct object *first;
+		uint16_t total = object_pack_total(p, obj, false, &first);
+		char label;
+
+		assert(first && total >= first->number);
 		object_desc(o_name, sizeof(o_name), obj,
-			ODESC_PREFIX | ODESC_FULL, p);
-		msg("You have %s (%c).", o_name, gear_to_label(p, obj));
+			ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
+			(total << 16), p);
+		label = gear_to_label(p, first);
+		if (total > first->number) {
+			msg("You have %s (1st %c).", o_name, label);
+		} else {
+			assert(first == obj);
+			msg("You have %s (%c).", o_name, label);
+		}
 	}
 
 	if (object_is_in_quiver(p, obj))
@@ -917,7 +1029,8 @@ void inven_drop(struct object *obj, int amt)
 {
 	struct object *dropped;
 	bool none_left = false;
-	bool quiver = false;
+	bool equipped = false;
+	bool quiver;
 
 	char name[80];
 	char label;
@@ -936,15 +1049,16 @@ void inven_drop(struct object *obj, int amt)
 	label = gear_to_label(player, obj);
 
 	/* Is it in the quiver? */
-	if (object_is_in_quiver(player, obj))
-		quiver = true;
+	quiver = object_is_in_quiver(player, obj);
 
 	/* Not too many */
 	if (amt > obj->number) amt = obj->number;
 
 	/* Take off equipment, don't combine */
-	if (object_is_equipped(player->body, obj))
+	if (object_is_equipped(player->body, obj)) {
+		equipped = true;
 		inven_takeoff(obj);
+	}
 
 	/* Get the object */
 	dropped = gear_object_for_use(player, obj, amt, false, &none_left);
@@ -961,18 +1075,27 @@ void inven_drop(struct object *obj, int amt)
 		object_desc(name, sizeof(name), dropped,
 			ODESC_FULL | ODESC_SINGULAR, player);
 		msg("You no longer have the %s (%c).", name, label);
-	} else if (none_left) {
-		/* Play silly games to get the right description */
-		int number = dropped->number;
-		dropped->number = 0;
-		object_desc(name, sizeof(name), dropped,
-			ODESC_PREFIX | ODESC_FULL, player);
-		msg("You have %s (%c).", name, label);
-		dropped->number = number;
 	} else {
-		object_desc(name, sizeof(name), obj,
-			ODESC_PREFIX | ODESC_FULL, player);
-		msg("You have %s (%c).", name, label);
+		struct object *first = NULL;
+		uint16_t total = (equipped) ?
+			((none_left) ? 0 : obj->number) :
+			object_pack_total(player, obj, false, &first);
+
+		object_desc(name, sizeof(name), dropped,
+			ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
+			(total << 16), player);
+		if (equipped || total == 0) {
+			msg("You have %s (%c).", name, label);
+		} else {
+			assert(first);
+
+			label = gear_to_label(player, first);
+			if (total > first->number) {
+				msg("You have %s (1st %c).", name, label);
+			} else {
+				msg("You have %s (%c).", name, label);
+			}
+		}
 	}
 
 	/* Drop it near the player */
@@ -1089,18 +1212,17 @@ void combine_pack(struct player *p)
 
 				if (inven_can_stack_partial(p, obj2, obj1,
 						stack_mode2, stack_mode1)) {
-					int oldn2 = obj2->number;
-					int oldn1 = obj1->number;
-
+					/*
+					 * Don't display a message for this
+					 * case:  shuffling items between
+					 * stacks isn't interesting to the
+					 * player.
+					 */
 					object_absorb_partial(obj2->known,
 						obj1->known, stack_mode2,
 						stack_mode1);
 					object_absorb_partial(obj2, obj1,
 						stack_mode2, stack_mode1);
-					if (obj2->number != oldn2 ||
-							obj1->number != oldn1) {
-						display_message = true;
-					}
 					/*
 					 * Ensure numbers align (should not be
 					 * necessary, but safer)

src/cmd-obj.c
@@ -406,7 +406,6 @@ static void use_aux(struct command *cmd, struct object *obj, enum use use,
 	bool known_aim = false;
 	bool none_left = false;
 	int dir = 5;
-	char label = gear_to_label(player, obj);
 	struct trap_kind *rune = lookup_trap("glyph of warding");
 
 	/* Get arguments */
@@ -448,9 +447,11 @@ static void use_aux(struct command *cmd, struct object *obj, enum use use,
 	if (can_use) {
 		int beam = beam_chance(obj->tval);
 		int boost, level, charges = 0;
-		int number = 0;
+		uint16_t number = 0;
 		bool ident = false, used;
 		struct object *work_obj;
+		struct object *first_remainder = NULL;
+		char label = '\0';
 
 		/* Get the level */
 		if (obj->artifact)
@@ -481,17 +482,27 @@ static void use_aux(struct command *cmd, struct object *obj, enum use use,
 		 * object to use for propagating knowledge.
 		 */
 		if (use == USE_SINGLE) {
+			/*
+			 * In either case, record number for messages after
+			 * use.
+			 */
 			if (object_is_carried(player, obj)) {
+				label = gear_to_label(player, obj);
 				work_obj = gear_object_for_use(player, obj, 1,
 					false, &none_left);
 				from_floor = false;
+				number = object_pack_total(player, work_obj,
+					false, &first_remainder);
+				if (first_remainder
+						&& first_remainder->number == number) {
+					first_remainder = NULL;
+				}
 			} else {
 				work_obj = floor_object_for_use(player, obj, 1,
 					false, &none_left);
 				from_floor = true;
+				number = (none_left) ? 0 : obj->number;
 			}
-			/* Record number for messages after use */
-			number = (none_left) ? 0 : obj->number;
 		} else  {
 			if (use == USE_CHARGE) {
 				charges = obj->pval;
@@ -571,20 +582,21 @@ static void use_aux(struct command *cmd, struct object *obj, enum use use,
 		/* Increase knowledge */
 		if (use == USE_SINGLE) {
 			char name[80];
-			int old_num = work_obj->number;
 
 			/* Single use items are automatically learned */
 			if (!was_aware) {
 				object_learn_on_use(player, work_obj);
 			}
 			/* Get a description */
-			work_obj->number = number + ((used) ? 0 : 1);
 			object_desc(name, sizeof(name), work_obj,
-				ODESC_PREFIX | ODESC_FULL, player);
-			work_obj->number = old_num;
+				ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
+				((number + ((used) ? 0 : 1)) << 16), player);
 			if (from_floor) {
 				/* Print a message */
 				msg("You see %s.", name);
+			} else if (first_remainder) {
+				label = gear_to_label(player, first_remainder);
+				msg("You have %s (1st %c).", name, label);
 			} else {
 				msg("You have %s (%c).", name, label);
 			}

src/obj-gear.h
@@ -41,6 +41,8 @@ int object_slot(struct player_body body, const struct object *obj);
 bool object_is_equipped(struct player_body body, const struct object *obj);
 bool object_is_carried(struct player *p, const struct object *obj);
 bool object_is_in_quiver(struct player *p, const struct object *obj);
+uint16_t object_pack_total(struct player *p, const struct object *obj,
+	bool ignore_inscrip, struct object **first);
 int pack_slots_used(const struct player *p);
 const char *equip_mention(struct player *p, int slot);
 const char *equip_describe(struct player *p, int slot);

commit 7e876dd775ac8702cb5e86d6706a06bf05b16913

For gear messages, don't show aggregate total across pack when staffs, wands, rods, or other items with timeouts are involved:  the charges/recharging notices aren't also aggregated across stacks leading to confusion.  Resolves https://github.com/angband/angband/issues/5275 , a regression introduced by https://github.com/angband/angband/pull/5245 .

src/obj-gear.c
@@ -539,7 +539,17 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 		if (message) {
 			uint16_t total;
 
-			if (object_is_equipped(p->body, obj)) {
+			/*
+			 * Don't show aggregate total in pack if equipped or
+			 * if the description could have a number of charges
+			 * or recharign notice specific to the stack (not
+			 * aggregating those quantities so there would be
+			 * confusion if aggregating the count).
+			 */
+			if (object_is_equipped(p->body, obj)
+					|| tval_can_have_charges(obj)
+					|| tval_is_rod(obj)
+					|| obj->timeout > 0) {
 				total = obj->number;
 			} else {
 				total = object_pack_total(p, obj, false,
@@ -559,9 +569,21 @@ struct object *gear_object_for_use(struct player *p, struct object *obj,
 				object_desc(name, sizeof(name), obj,
 					ODESC_FULL | ODESC_SINGULAR, p);
 			} else {
-				uint16_t total = (object_is_equipped(p->body, obj)) ?
-					obj->number :
-					object_pack_total(p, obj, false, &first_remainder);
+				uint16_t total;
+
+				/*
+				 * Use same logic as above for showing an
+				 * aggregate total.
+				 */
+				if (object_is_equipped(p->body, obj)
+						|| tval_can_have_charges(obj)
+						|| tval_is_rod(obj)
+						|| obj->timeout > 0) {
+					total = obj->number;
+				} else {
+					total = object_pack_total(p, obj,
+						false, &first_remainder);
+				}
 
 				assert(total >= num);
 				total -= num;
@@ -866,9 +888,20 @@ void inven_carry(struct player *p, struct object *obj, bool absorb,
 	if (message) {
 		char o_name[80];
 		struct object *first;
-		uint16_t total = object_pack_total(p, obj, false, &first);
+		uint16_t total;
 		char label;
 
+		/*
+		 * Show an aggregate total if the description doesn't have
+		 * a charge/charging notice that's specific to the stack.
+		 */
+		if (tval_can_have_charges(obj) || tval_is_rod(obj)
+				|| obj->timeout > 0) {
+			total = obj->number;
+			first = obj;
+		} else {
+			total = object_pack_total(p, obj, false, &first);
+		}
 		assert(first && total >= first->number);
 		object_desc(o_name, sizeof(o_name), obj,
 			ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
@@ -1087,19 +1120,36 @@ void inven_drop(struct object *obj, int amt)
 			ODESC_FULL | ODESC_SINGULAR, player);
 		msg("You no longer have the %s (%c).", name, label);
 	} else {
-		struct object *first = NULL;
-		uint16_t total = (equipped) ?
-			((none_left) ? 0 : obj->number) :
-			object_pack_total(player, obj, false, &first);
+		struct object *first;
+		struct object *desc_target;
+		uint16_t total;
 
-		object_desc(name, sizeof(name), dropped,
+		/*
+		 * Like gear_object_for_use(), don't show an aggregate total
+		 * if it was equipped or the item has charges/recharging
+		 * notice that is specific to the stack.
+		 */
+		if (equipped || tval_can_have_charges(obj) || tval_is_rod(obj)
+				|| obj->timeout > 0) {
+			first = NULL;
+			if (none_left) {
+				total = 0;
+				desc_target = dropped;
+			} else {
+				total = obj->number;
+				desc_target = obj;
+			}
+		} else {
+			total = object_pack_total(player, obj, false, &first);
+			desc_target = (total) ? obj : dropped;
+		}
+
+		object_desc(name, sizeof(name), desc_target,
 			ODESC_PREFIX | ODESC_FULL | ODESC_ALTNUM |
 			(total << 16), player);
-		if (equipped || total == 0) {
+		if (!first) {
 			msg("You have %s (%c).", name, label);
 		} else {
-			assert(first);
-
 			label = gear_to_label(player, first);
 			if (total > first->number) {
 				msg("You have %s (1st %c).", name, label);