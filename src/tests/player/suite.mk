0c3d1eedf6b63cc5fc5838b95c8bc3d028a0b207
Eric Branlund <ebranlund@fastmail.com>
2022-05-06 13:00:03 -0600

    Avoid platform-dependent right shift of negative values in player_adjust_hp_precise() and player_adjust_mana_precise().  Did trigger a runtime diagnostic message after a character death when the application was compiled with -fsanitize=undefined.  Add test cases for player_adjust_hp_precise() and player_adjust_mana_precise().

--- src/tests/player/suite.mk
+++ src/tests/player/suite.mk
@@ -5,4 +5,5 @@ TESTPROGS += player/birth \
              player/inven-carry-num \
              player/inven-wield \
              player/pathfind \
-             player/playerstat
+             player/playerstat \
+             player/util
