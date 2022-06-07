angband/angband

commit 3126eb4f289e4b6e6f1d714f72cec048d6c9254d

Allow more menus to use the rogue-like movement keys for navigation. … (#5316)
* Allow more menus to use the rogue-like movement keys for navigation.  Related to https://github.com/angband/angband/issues/5298 .

* Adjust end-to-end test for reindexing of birth screen race choices.

src/ui-options.c
@@ -303,6 +303,7 @@ static const menu_iter option_toggle_iter = {
  */
 static void option_toggle_menu(const char *name, int page)
 {
+	static const char selections[] = "abcdefgimopquvwzABCDEFGHIJKLMOPQUVWZ";
 	int i;
 	
 	struct menu *m = menu_new(MN_SKIN_SCROLL, &option_toggle_iter);
@@ -310,7 +311,7 @@ static void option_toggle_menu(const char *name, int page)
 	/* for all menus */
 	m->prompt = "Set option (y/n/t), select with movement keys or index";
 	m->cmd_keys = "YyNnTt";
-	m->selections = "abcdefghijklmopqrsuvwxz";
+	m->selections = selections;
 	m->flags = MN_DBL_TAP;
 
 	/* We add 10 onto the page amount to indicate we're at birth */
@@ -321,7 +322,6 @@ static void option_toggle_menu(const char *name, int page)
 	} else if (page == OPT_PAGE_BIRTH + 10 || page == OP_INTERFACE) {
 		m->prompt = "Set option (y/n/t), 's' to save, 'r' to restore, 'x' to reset";
 		m->cmd_keys = "YyNnTtSsRrXx";
-		m->selections = "abcdefghijklmopquvwzABC";
 		/* Provide a context menu for equivalents to 's', 'r', .... */
 		m->context_hook = use_option_context_menu;
 		if (page == OPT_PAGE_BIRTH + 10) {
@@ -1965,6 +1965,7 @@ void do_cmd_options_item(const char *title, int row)
 				 N_ELEMENTS(extra_item_options) + 1, NULL);
 
 	menu.title = title;
+	menu.selections = all_letters_nohjkl;
 	menu_layout(&menu, &SCREEN_REGION);
 
 	screen_save();
@@ -2003,8 +2004,8 @@ static menu_action option_actions[] =
 	{ 0, 't', "Save autoinscriptions to pref file", do_dump_autoinsc },
 	{ 0, 'u', "Save char screen options to pref file", do_dump_charscreen_opt },
 	{ 0, 0, NULL, NULL },
-	{ 0, 'l', "Load a user pref file", options_load_pref_file },
-	{ 0, 'k', "Edit keymaps (advanced)", do_cmd_keymaps },
+	{ 0, 'p', "Load a user pref file", options_load_pref_file },
+	{ 0, 'e', "Edit keymaps (advanced)", do_cmd_keymaps },
 	{ 0, 'c', "Edit colours (advanced)", do_cmd_colors },
 	{ 0, 'v', "Save visuals (advanced)", do_cmd_visuals },
 };

src/effect-handler-general.c
@@ -1908,7 +1908,8 @@ bool effect_handler_DISENCHANT(effect_handler_context_t *context)
 	/* Artifacts have a 60% chance to resist */
 	if (obj->artifact && (randint0(100) < 60)) {
 		/* Message */
-		msg("Your %s (%c) resist%s disenchantment!", o_name, I2A(i),
+		msg("Your %s (%c) resist%s disenchantment!", o_name,
+			gear_to_label(player, obj),
 			((obj->number != 1) ? "" : "s"));
 
 		return true;
@@ -1934,7 +1935,8 @@ bool effect_handler_DISENCHANT(effect_handler_context_t *context)
 	}
 
 	/* Message */
-	msg("Your %s (%c) %s disenchanted!", o_name, I2A(i),
+	msg("Your %s (%c) %s disenchanted!", o_name,
+		gear_to_label(player, obj),
 		((obj->number != 1) ? "were" : "was"));
 
 	/* Recalculate bonuses */

src/mon-blows.c
@@ -227,8 +227,9 @@ static void steal_player_item(melee_effect_handler_context_t *context)
 				(split ? "one of your" : "your"), o_name);
 		} else {
 			/* Message */
-			msg("%s %s (%c) was stolen!", (split ? "One of your" : "Your"),
-				o_name, I2A(index));
+			msg("%s %s (%c) was stolen!",
+				(split ? "One of your" : "Your"), o_name,
+				gear_to_label(context->p, obj));
 
 			/* Steal and carry */
 			stolen = gear_object_for_use(context->p, obj, 1,
@@ -805,12 +806,13 @@ static void melee_effect_handler_EAT_FOOD(melee_effect_handler_context_t *contex
 		if (obj->number == 1) {
 			object_desc(o_name, sizeof(o_name), obj, ODESC_BASE,
 				context->p);
-			msg("Your %s (%c) was eaten!", o_name, I2A(index));
+			msg("Your %s (%c) was eaten!", o_name,
+				gear_to_label(context->p, obj));
 		} else {
 			object_desc(o_name, sizeof(o_name), obj,
 				ODESC_PREFIX | ODESC_BASE, context->p);
 			msg("One of your %s (%c) was eaten!", o_name,
-				I2A(index));
+				gear_to_label(context->p, obj));
 		}
 
 		/* Steal and eat */

src/obj-gear.c
@@ -442,11 +442,14 @@ bool minus_ac(struct player *p)
  */
 char gear_to_label(struct player *p, struct object *obj)
 {
+	/* Skip rogue-like cardinal direction movement keys. */
+	const char labels[] =
+		 "abcdefgimnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	/* Equipment is easy */
 	if (object_is_equipped(p->body, obj)) {
-		return I2A(equipped_item_slot(p->body, obj));
+		return labels[equipped_item_slot(p->body, obj)];
 	}
 
 	/* Check the quiver */
@@ -459,7 +462,7 @@ char gear_to_label(struct player *p, struct object *obj)
 	/* Check the inventory */
 	for (i = 0; i < z_info->pack_size; i++) {
 		if (p->upkeep->inven[i] == obj) {
-			return I2A(i);
+			return labels[i];
 		}
 	}
 
@@ -991,7 +994,7 @@ void inven_wield(struct object *obj, int slot)
 		ODESC_PREFIX | ODESC_FULL, player);
 
 	/* Message */
-	msgt(MSG_WIELD, fmt, o_name, I2A(slot));
+	msgt(MSG_WIELD, fmt, o_name, gear_to_label(player, wielded));
 
 	/* Sticky flag geats a special mention */
 	if (of_has(wielded->flags, OF_STICKY)) {

src/ui-birth.c
@@ -501,9 +501,10 @@ static void init_birth_menu(struct menu *menu, int n_choices,
 	/* Initialise a basic menu */
 	menu_init(menu, MN_SKIN_SCROLL, &birth_iter);
 
-	/* A couple of behavioural flags - we want selections letters in
-	   lower case and a double tap to act as a selection. */
-	menu->selections = lower_case;
+	/* A couple of behavioural flags - we want selections as letters
+	   skipping the rogue-like cardinal direction movements and a
+	   double tap to act as a selection. */
+	menu->selections = all_letters_nohjkl;
 	menu->flags = MN_DBL_TAP;
 
 	/* Copy across the game's suggested initial selection, etc. */

src/ui-curse.c
@@ -122,7 +122,7 @@ static int curse_menu(struct object *obj, char *dice_string)
 			  format(" Remove which curse (spell strength %s)?", dice_string),
 			  sizeof(header));
 	m->header = header;
-	m->selections = lower_case;
+	m->selections = all_letters_nohjkl;
 	m->flags = (MN_PVT_TAGS);
 	m->browse_hook = curse_menu_browser;
 

src/ui-effect.c
@@ -40,7 +40,7 @@ static struct menu *effect_menu_new(struct effect *effect, int count,
 	char **ms;
 	char buf[80];
 
-	m->selections = lower_case;
+	m->selections = all_letters_nohjkl;
 
 	/* Collect a string for each effect. */
 	if (count > 0) {

src/ui-equip-cmp.c
@@ -311,16 +311,16 @@ void equip_cmp_display(void)
 		{ "Sorry, could not display.  Press any key.",
 			handle_input_bail, true, false },
 		/* EQUIP_CMP_MENU_NEW_PAGE */
-		{ "[Up/Down arrow, p/PgUp, n/PgDn to move; ? for help; ESC to "
+		{ "[k/up, j/down, p/PgUp, n/PgDn to move; ? for help; ESC to "
 			"exit]", handle_input_equip_cmp_general, true, true },
 		/* EQUIP_CMP_MENU_SAME_PAGE */
-		{ "[Up/Down arrow, p/PgUp, n/PgDn to move; ? for help; ESC to "
+		{ "[k/up, j/down, p/PgUp, n/PgDn to move; ? for help; ESC to "
 			"exit]", handle_input_equip_cmp_general, false, false },
 		/* EQUIP_CMP_MENU_SEL0 */
-		{ "[Up/Down arrow, p/PgUp, n/PgDn to move; return to accept]",
+		{ "[k/up, j/down, p/PgUp, n/PgDn to move; return to accept]",
 			handle_input_equip_cmp_select, true, true },
 		/* EQUIP_CMP_MENU_SEL1 */
-		{ "[Up/Down arrow, p/PgUp, n/PgDn to move; return to accept]",
+		{ "[k/up, j/down, p/PgUp, n/PgDn to move; return to accept]",
 			handle_input_equip_cmp_select, true, true },
 	};
 	int istate;
@@ -403,31 +403,31 @@ static void display_equip_cmp_help(void)
 	irow = 1;
 	prt("Movement/scrolling ---------------------------------", irow, 0);
 	++irow;
-	prt("Down arrow  one line down    Up arrow    one line up", irow, 0);
+	prt("j, down  one line down    k, up    one line up", irow, 0);
 	++irow;
-	prt("n, PgDn     one page down    p, PgUp     one page up", irow, 0);
+	prt("n, PgDn  one page down    p, PgUp  one page up", irow, 0);
 	++irow;
-	prt("space       one page down", irow, 0);
+	prt("space    one page down", irow, 0);
 	++irow;
 	prt("Filtering/searching/sorting ------------------------", irow, 0);
 	++irow;
-	prt("q           quick filter     !           use opposite quick", irow, 0);
+	prt("q        quick filter     !        use opposite quick", irow, 0);
 	++irow;
-	prt("c           cycle through sources of items", irow, 0);
+	prt("c        cycle through sources of items", irow, 0);
 	++irow;
-	prt("r           reverse", irow, 0);
+	prt("r        reverse", irow, 0);
 	++irow;
 	prt("Information ----------------------------------------", irow, 0);
 	++irow;
-	prt("v           cycle through attribute views", irow, 0);
+	prt("v        cycle through attribute views", irow, 0);
 	++irow;
-	prt("I, x        select one or two items for details", irow, 0);
+	prt("I, x     select one or two items for details", irow, 0);
 	++irow;
 	prt("Other ----------------------------------------------", irow, 0);
 	++irow;
-	prt("d           dump to file     R           reset display", irow, 0);
+	prt("d        dump to file     R        reset display", irow, 0);
 	++irow;
-	prt("ESC         exit", irow, 0);
+	prt("ESC      exit", irow, 0);
 	++irow;
 
 	Term_get_size(&wid, &hgt);
@@ -515,10 +515,12 @@ static int handle_input_equip_cmp_general(ui_event in, int istate,
 			action = ACT_CTX_EQUIPCMP_PREV_PAGE;
 			break;
 
+		case 'j':
 		case ARROW_DOWN:
 			action = ACT_CTX_EQUIPCMP_NEXT_LINE;
 			break;
 
+		case 'k':
 		case ARROW_UP:
 			action = ACT_CTX_EQUIPCMP_PREV_LINE;
 			break;
@@ -916,19 +918,19 @@ static void display_equip_cmp_sel_help(void)
 
 	Term_clear();
 	irow = 1;
-	prt("Down arrow  move selection one line down", irow, 0);
+	prt("j, down   move selection one line down", irow, 0);
 	++irow;
-	prt("Up arrow    move selection one line up", irow, 0);
+	prt("k, up     move selection one line up", irow, 0);
 	++irow;
-	prt("n, PgDn     move selection one page up", irow, 0);
+	prt("n, PgDn   move selection one page up", irow, 0);
 	++irow;
-	prt("p, PgUp     move selection one page up", irow, 0);
+	prt("p, PgUp   move selection one page up", irow, 0);
 	++irow;
-	prt("x           stop selection; if first item, escapes", irow, 0);
+	prt("x         stop selection; if first item, escapes", irow, 0);
 	++irow;
-	prt("return      select current item", irow, 0);
+	prt("return    select current item", irow, 0);
 	++irow;
-	prt("ESC         leave selection process", irow, 0);
+	prt("ESC       leave selection process", irow, 0);
 	++irow;
 
 	Term_get_size(&wid, &hgt);
@@ -971,10 +973,12 @@ static int handle_input_equip_cmp_select(ui_event in, int istate,
 			action = ACT_CTX_EQUIPCMP_SELECT_PREV_PAGE;
 			break;
 
+		case 'j':
 		case ARROW_DOWN:
 			action = ACT_CTX_EQUIPCMP_SELECT_NEXT_LINE;
 			break;
 
+		case 'k':
 		case ARROW_UP:
 			action = ACT_CTX_EQUIPCMP_SELECT_PREV_LINE;
 			break;

commit ec8e3b289cf1a883e8affb587d5bca586c243dcb

Use cleaner implementation of selectors for the item ignoring menu.  Fixes regression introduced by https://github.com/angband/angband/commit/3126eb4f289e4b6e6f1d714f72cec048d6c9254d .  Thanks to PowerWyrm for the report, http://angband.oook.cz/forum/showpost.php?p=158637&postcount=124 .

src/ui-options.c
@@ -1884,7 +1884,7 @@ static char tag_options_item(struct menu *menu, int oid)
 	size_t line = (size_t) oid;
 
 	if (line < N_ELEMENTS(sval_dependent))
-		return I2A(oid);
+		return all_letters_nohjkl[oid];
 
 	/* Separator - blank line. */
 	if (line == N_ELEMENTS(sval_dependent))
@@ -1980,7 +1980,6 @@ void do_cmd_options_item(const char *title, int row)
 				 N_ELEMENTS(extra_item_options) + 1, NULL);
 
 	menu.title = title;
-	menu.selections = all_letters_nohjkl;
 	menu_layout(&menu, &SCREEN_REGION);
 
 	screen_save();