angband/angband

commit 890a69605a911c6c5a4ed407e964553d07716b0e

Learn feather falling if equipped and exposed to fiery terrain.  See PowerWyrm's post here, http://angband.oook.cz/forum/showthread.php?t=11114&page=14 .

src/player-util.c
@@ -813,6 +813,7 @@ int player_check_terrain_damage(struct player *p, struct loc grid)
 		/* Feather fall makes one lightfooted. */
 		if (player_of_has(p, OF_FEATHER)) {
 			dam_taken /= 2;
+			equip_learn_flag(p, OF_FEATHER);
 		}
 	}
 

commit cbac1282035c546124a067320993fef6d1276fef

Only learn feather falling from fiery terrain when on the terrain.  Also learn fire resistance.  The first is a regression introduced by https://github.com/angband/angband/pull/5395 .

src/cmd-cave.c
@@ -1093,8 +1093,8 @@ void move_player(int dir, bool disarm)
 		if (square_isdamaging(cave, grid)) {
 			/* Some terrain can damage the player */
 			struct feature *feat = square_feat(cave, grid);
-			int dam_taken =
-				player_check_terrain_damage(player, grid);
+			int dam_taken = player_check_terrain_damage(player,
+				grid, false);
 
 			/*
 			 * Check if running, or going to cost more than a

src/player-util.c
@@ -797,9 +797,14 @@ void player_over_exert(struct player *p, int flag, int chance, int amount)
 
 
 /**
- * See how much damage the player will take from damaging terrain
+ * See how much damage the player will take from terrain.
+ *
+ * \param p is the player to check
+ * \param grid is the location of the terrain
+ * \param actual, if true, will cause the player to learn the appropriate
+ * runes if equipment or effects mitigate the damage.
  */
-int player_check_terrain_damage(struct player *p, struct loc grid)
+int player_check_terrain_damage(struct player *p, struct loc grid, bool actual)
 {
 	int dam_taken = 0;
 
@@ -808,12 +813,15 @@ int player_check_terrain_damage(struct player *p, struct loc grid)
 		int res = p->state.el_info[ELEM_FIRE].res_level;
 
 		/* Fire damage */
-		dam_taken = adjust_dam(p, ELEM_FIRE, base_dam, RANDOMISE, res, false);
+		dam_taken = adjust_dam(p, ELEM_FIRE, base_dam, RANDOMISE, res,
+			actual);
 
 		/* Feather fall makes one lightfooted. */
 		if (player_of_has(p, OF_FEATHER)) {
 			dam_taken /= 2;
-			equip_learn_flag(p, OF_FEATHER);
+			if (actual) {
+				equip_learn_flag(p, OF_FEATHER);
+			}
 		}
 	}
 
@@ -825,7 +833,7 @@ int player_check_terrain_damage(struct player *p, struct loc grid)
  */
 void player_take_terrain_damage(struct player *p, struct loc grid)
 {
-	int dam_taken = player_check_terrain_damage(p, grid);
+	int dam_taken = player_check_terrain_damage(p, grid, true);
 
 	if (!dam_taken) {
 		return;

src/player-util.h
@@ -77,7 +77,7 @@ void player_update_light(struct player *p);
 void player_over_exert(struct player *p, int flag, int chance, int amount);
 struct object *player_best_digger(struct player *p, bool forbid_stack);
 bool player_attack_random_monster(struct player *p);
-int player_check_terrain_damage(struct player *p, struct loc grid);
+int player_check_terrain_damage(struct player *p, struct loc grid, bool actual);
 void player_take_terrain_damage(struct player *p, struct loc grid);
 struct player_shape *lookup_player_shape(const char *name);
 int shape_name_to_idx(const char *name);