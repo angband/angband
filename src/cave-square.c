65d66c08212794460447a9c4dc1fc6093c18304d
Eric Branlund <ebranlund@fastmail.com>
2023-06-01 22:20:53 -0600

    Fix instances of "the the" or "for for" in comments

--- src/cave-square.c
+++ src/cave-square.c
@@ -393,7 +393,7 @@ bool square_isoccupied(struct chunk *c, struct loc grid) {
 }
 
 /**
- * True if the the player knows the terrain of the square
+ * True if the player knows the terrain of the square
  */
 bool square_isknown(struct chunk *c, struct loc grid) {
 	if (c != cave) return false;
@@ -402,7 +402,7 @@ bool square_isknown(struct chunk *c, struct loc grid) {
 }
 
 /**
- * True if the the player's knowledge of the terrain of the square is wrong
+ * True if the player's knowledge of the terrain of the square is wrong
  * or missing
  */
 bool square_isnotknown(struct chunk *c, struct loc grid) {
