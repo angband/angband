02c833779fa4b0ae1bba743dd198c12320ed4647
Adrian Siekierka <kontakt@asie.pl>
2023-07-17 23:18:26 +0200

    nds: add BlocksDS toolchain support (#5700)
    * nds: add BlocksDS toolchain support
    
    * blocksds: Add NDS_DEBUG flag
    
    * blocksds: Add missing icon file

--- src/main-nds-arm7.c
+++ src/main-nds-arm7.c
@@ -1,15 +1,32 @@
 /*---------------------------------------------------------------------------------
-	$Id: template.c,v 1.2 2005/09/07 20:06:06 wntrmute Exp $
 
-	Simple ARM7 stub (sends RTC, TSC, and X/Y data to the ARM 9)
+	angband ARM7 core
 
-	$Log: template.c,v $
-	Revision 1.2  2005/09/07 20:06:06  wntrmute
-	updated for latest libnds changes
+	modified from the default ARM7 core
 
-	Revision 1.8  2005/08/03 05:13:16  wntrmute
-	corrected sound code
+		Copyright (C) 2005 - 2010
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
 
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+
+	3.	This notice may not be removed or altered from any source
+		distribution.
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
@@ -21,28 +38,42 @@ void VblankHandler(void) {
 	inputGetAndSend();
 }
 
+volatile bool exitflag = false;
+
+//---------------------------------------------------------------------------------
+void powerButtonCB() {
+//---------------------------------------------------------------------------------
+	exitflag = true;
+}
+
 //---------------------------------------------------------------------------------
 int main(int argc, char ** argv) {
 //---------------------------------------------------------------------------------
 
 	// Setup the FIFO system
+	initClockIRQ();
 	irqInit();
 	fifoInit();
 
 	// Initialize touch (offset, scaling, etc.)
 	touchInit();
 
-	// Reset the clock if needed
-	rtcReset();
-
 	// Install the system FIFO handler (poweroff, etc.)
 	installSystemFIFO();
 
 	irqSet(IRQ_VBLANK, VblankHandler);
-	irqEnable(IRQ_VBLANK);
+	irqEnable(IRQ_VBLANK | IRQ_NETWORK);
 
-	// Keep the ARM7 out of main RAM
-	while (1) swiWaitForVBlank();
+	setPowerButtonCB(powerButtonCB);
+
+	// Keep the ARM7 mostly idle
+	while (!exitflag) {
+		if ( 0 == (REG_KEYINPUT & (KEY_SELECT | KEY_START | KEY_L | KEY_R))) {
+			exitflag = true;
+		}
+		swiWaitForVBlank();
+	}
+	return 0;
 }
 
 
