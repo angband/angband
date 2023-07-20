angband/angband

commit 7ca6c3760ed01c2bcfde11254daad04eee7b0986

Put constants for criticals in constants.txt Resolves part of https://github.com/angband/angband/issues/4803 . Fixes some mismatches between the critical calculations used for displayed object information and those used for combat.  In all instances the object information calculations were changed to match those for combat:     1) For non-O melee criticals, the normalization of the sums for the        additive term was wrong (500 instead of 1300).     2) For non-O melee criticals the to-hit value was scaled by 3 for        object information while the scale factor for combat was 1.     3) Non-O criticals summed over a range of powers that was w, the weight        of the missile or weapon, to w + a where a is 649 for melee criticals        and 499 for ranged criticals.  The range of powers in the combat        calculations is 1 + weight to 1 + weight + a.     4) For O criticals with melee weapons, the to-hit value was not        scaled down by a factor of three as it is in the combat calculations.     5) For O criticals with thrown weapons, the power of the critical        wasn't boosted by a factor of 1.5 for the object information        calculations. For the object information calculations, calculate_melee_crits(), o_calculate_melee_crits(), calculate_missile_crits(), and o_calculate_missile_crits() now report values rounded to the nearest integer.  However, since there is further scaling for those results, the final values reported to the player still aren't guaranteed to be rounded to the nearest tenth. Because of the corrections and the change to rounding, the reported damage for a level 1 warrior with 18/30 strength and 18/40 dexterity wielding a +0,+0 dagger and a +0,+0 x2 short bow with +0,+0 arrows changed as follows (that warrior had 3.84 blows/round with the dagger and 1 shot per round with the bow):                        old non-O     new non-O     old 0     new O                        ---------     ---------     -----     ----- dagger, wielded

src/obj-info.c
@@ -429,40 +429,114 @@ static bool describe_brands(textblock *tb, const struct object *obj)
 }
 
 /**
+ * Sum over the critical levels for O-combat to get the expected number of
+ * dice added when a critical happens.
+ */
+static struct my_rational sum_o_criticals(const struct o_critical_level *head)
+{
+	struct my_rational remaining_chance = my_rational_construct(1, 1);
+	struct my_rational added_dice = my_rational_construct(0, 1);
+
+	while (head) {
+		/* The last level of criticals takes all the remainder. */
+		struct my_rational level_added_dice = my_rational_construct(
+			head->added_dice, (head->next) ? head->chance : 1);
+
+		level_added_dice = my_rational_product(&level_added_dice,
+			&remaining_chance);
+		added_dice = my_rational_sum(&added_dice, &level_added_dice);
+		if (head->next) {
+			struct my_rational pr_not_this = my_rational_construct(
+				head->chance - 1, head->chance);
+
+			remaining_chance = my_rational_product(
+				&remaining_chance, &pr_not_this);
+		}
+		head = head->next;
+	}
+
+	return added_dice;
+}
+
+/**
  * Account for criticals in the calculation of melee prowess
  *
  * Note -- This relies on the criticals being an affine function
  * of previous damage, since we are used to transform the mean
  * of a roll.
- *
- * Also note -- rounding error makes this not completely accurate
- * (but for the big crit weapons like Grond an odd point of damage
- * won't be missed)
- *
- * This code written according to the KISS principle.  650 adds
- * are cheaper than a FOV call and get the job done fine.
  */
 static void calculate_melee_crits(struct player_state *state, int weight,
 		int plus, int *mult, int *add, int *div)
 {
-	int k, to_crit = weight + 5 * (state->to_h + plus) +
-		3 * state->skills[SKILL_TO_HIT_MELEE] - 60;
-	to_crit = MIN(5000, MAX(0, to_crit));
+	/*
+	 * Pessimistically assume that the target is not debuffed; otherwise
+	 * this must agree with the calculations in player-attack.c's
+	 * critical_melee().
+	 */
+	int crit_chance = z_info->m_crit_chance_weight_scl * weight
+		+ z_info->m_crit_chance_toh_scl * (state->to_h + plus)
+		+ z_info->m_crit_chance_level_scl * player->lev
+		+ z_info->m_crit_chance_toh_skill_scl
+			* state->skills[SKILL_TO_HIT_MELEE];
+	crit_chance = MIN(z_info->m_crit_chance_range, MAX(0, crit_chance));
 
-	*mult = *add = 0;
+	/* Reported results (*mult and *add) are scaled up by 100. */
+	*div = 100;
 
-	for (k = weight; k < weight + 650; k++) {
-		if (k <  400) { *mult += 4; *add += 10; continue; }
-		if (k <  700) { *mult += 4; *add += 20; continue; }
-		if (k <  900) { *mult += 6; *add += 30; continue; }
-		if (k < 1300) { *mult += 6; *add += 40; continue; }
-		                *mult += 8; *add += 40;
+	if (crit_chance > 0 && z_info->m_crit_level_head) {
+		/*
+		 * Now sum over the possible values of the critical power.
+		 */
+		const struct critical_level *this_l = z_info->m_crit_level_head;
+		int min_power = z_info->m_crit_power_weight_scl * weight + 1;
+		int max_power = min_power - 1 + z_info->m_crit_power_random;
+		int mult_sum = 0;
+		int add_sum = 0;
+		int scale;
+
+		while (min_power <= max_power) {
+			int w;
+
+			if (max_power < this_l->cutoff || !this_l->next) {
+				/*
+				 * All the remaining possible critical powers
+				 * fall in this band.
+				 */
+				w = max_power - min_power + 1;
+				min_power = max_power + 1;
+			} else  {
+				if (min_power >= this_l->cutoff) {
+					/*
+					 * This band doesn't overlap the
+					 * the possible powers.
+					 */
+					this_l = this_l->next;
+					continue;
+				}
+				/*
+				 * This band is either fully covered or has its
+				 * upper part covered by the possible powers.
+				 */
+				w = this_l->cutoff - min_power;
+				min_power = this_l->cutoff;
+			}
+			mult_sum += w * (this_l->mult - 1);
+			add_sum += w * this_l->add;
+			this_l = this_l->next;
+		}
+		/*
+		 * In other words, the result of no critical (multipler of 1
+		 * and no additive term) plus the scaled result of summing over
+		 * the possible criticals rounded to the nearest integer.
+		 */
+		scale = (z_info->m_crit_chance_range / *div)
+			* z_info->m_crit_power_random;
+		*mult = *div + (crit_chance * mult_sum + scale / 2) / scale;
+		*add = (crit_chance * add_sum + scale / 2) / scale;
+	} else {
+		*mult = 100;
+		*add = 0;
 	}
-
-	/* Scale the output to a reasonable size to prevent integer overflow. */
-	*mult = 100 + to_crit * (*mult - 1300) / (50 * 1300);
-	*add  = *add * to_crit / (500 * 50);
-	*div  = 100;
 }
 
 /**
@@ -471,14 +545,71 @@ static void calculate_melee_crits(struct player_state *state, int weight,
  *
  * Return value is 100x number of dice
  */
-static int o_calculate_melee_crits(struct player_state state,
-								   const struct object *obj)
+static unsigned int o_calculate_melee_crits(struct player_state *state,
+		const struct object *obj)
 {
-	int dice = 0;
-	int chance = BTH_PLUS_ADJ * (state.to_h + obj->known->to_h) +
-		state.skills[SKILL_TO_HIT_MELEE];
-	chance = (100 * chance) / (chance + 240);
-	dice = (537 * chance) / 240;
+	unsigned int dice;
+
+	if (z_info->o_m_crit_level_head) {
+		/*
+		 * Pessimistically assume that the target is not debuffed.
+		 * Otherwise, these calculations must agree with those in
+		 * player-attack.c's o_critical_melee().
+		 */
+		struct player_state old_state = player->state;
+		int power, chance_num, chance_den;
+
+		if (z_info->o_m_max_added.n == 0) {
+			z_info->o_m_max_added =
+				sum_o_criticals(z_info->o_m_crit_level_head);
+		}
+
+		player->state = *state;
+		power = chance_of_melee_hit_base(player, obj);
+		player->state = old_state;
+		power = (power * z_info->o_m_crit_power_toh_scl_num)
+			/ z_info->o_m_crit_power_toh_scl_den;
+		chance_num = power * z_info->o_m_crit_chance_power_scl_num;
+		chance_den = power * z_info->o_m_crit_chance_power_scl_den
+			+ z_info->o_m_crit_chance_add_den;
+		if (chance_den > 0 && chance_num > 0) {
+			unsigned int tr;
+
+			if (chance_num < chance_den) {
+				/*
+				 * Critical only happens some of the time.
+				 * Scale by the chance and 100.  Round to the
+				 * nearest integer.
+				 */
+				struct my_rational t = my_rational_construct(
+					chance_num, chance_den);
+
+				t = my_rational_product(&t,
+					&z_info->o_m_max_added);
+				dice = my_rational_to_uint(&t, 100, &tr);
+				if (dice < UINT_MAX && tr >= t.d / 2) {
+					++dice;
+				}
+			} else {
+				/*
+				 * Critical always happens.  Scale by 100 and
+				 * round to the nearest integer.
+				 */
+				dice = my_rational_to_uint(
+					&z_info->o_m_max_added, 100, &tr);
+				if (dice < UINT_MAX && tr >=
+						z_info->o_m_max_added.d / 2) {
+					++dice;
+				}
+			}
+		} else {
+			/* No chance of happening so no additional damage. */
+			dice = 0;
+		}
+	} else {
+		/* No critical levels defined so no additional damage. */
+		dice = 0;
+	}
 
 	return dice;
 }
@@ -487,43 +618,162 @@ static int o_calculate_melee_crits(struct player_state state,
  * Missile crits follow the same approach as melee crits.
  */
 static void calculate_missile_crits(struct player_state *state, int weight,
-		int plus, int *mult, int *add, int *div)
+		int plus, bool launched, int *mult, int *add, int *div)
 {
-	int k, to_crit = weight + 4 * (state->to_h + plus) + 2 * player->lev;
-	to_crit = MIN(5000, MAX(0, to_crit));
+	/*
+	 * Pessimistically assume that the target is not debuffed; otherwise
+	 * this must agree with the calculations in player-attack.c's
+	 * critical_shot().
+	 */
+	int crit_chance = z_info->r_crit_chance_weight_scl * weight
+		+ z_info->r_crit_chance_toh_scl * (state->to_h + plus)
+		+ z_info->r_crit_chance_level_scl * player->lev;
 
-	*mult = *add = 0;
-
-	for (k = weight; k < weight + 500; k++) {
-		if (k <  500) { *mult += 2; *add +=  5; continue; }
-		if (k < 1000) { *mult += 2; *add += 10; continue; }
-		                *mult += 3; *add += 15;
+	if (launched) {
+		crit_chance += z_info->r_crit_chance_launched_toh_skill_scl
+			* player->state.skills[SKILL_TO_HIT_BOW];
+	} else {
+		crit_chance += z_info->r_crit_chance_thrown_toh_skill_scl
+			* player->state.skills[SKILL_TO_HIT_THROW];
 	}
+	crit_chance = MIN(z_info->r_crit_chance_range, MAX(0, crit_chance));
 
-	*mult = 100 + to_crit * (*mult - 500) / (500 * 50);
-	*add  = *add * to_crit / (500 * 50);
-	*div  = 100;
+	/* Reported results (*mult and *add) are scaled up by 100. */
+	*div = 100;
+
+	if (crit_chance > 0 && z_info->r_crit_level_head) {
+		/*
+		 * Now sum over the possible values of the critical power.
+		 */
+		const struct critical_level *this_l = z_info->r_crit_level_head;
+		int min_power = z_info->r_crit_power_weight_scl * weight + 1;
+		int max_power = min_power - 1 + z_info->r_crit_power_random;
+		int mult_sum = 0;
+		int add_sum = 0;
+		int scale;
+
+		while (min_power <= max_power) {
+			int w;
+
+			if (max_power < this_l->cutoff || !this_l->next) {
+				/*
+				 * All the remaining possible critical powers
+				 * fall in this band.
+				 */
+				w = max_power - min_power + 1;
+				min_power = max_power + 1;
+			} else  {
+				if (min_power >= this_l->cutoff) {
+					/*
+					 * This band doesn't overlap the
+					 * the possible powers.
+					 */
+					this_l = this_l->next;
+					continue;
+				}
+				/*
+				 * This band is either fully covered or has its
+				 * upper part covered by the possible powers.
+				 */
+				w = this_l->cutoff - min_power;
+				min_power = this_l->cutoff;
+			}
+			mult_sum += w * (this_l->mult - 1);
+			add_sum += w * this_l->add;
+			this_l = this_l->next;
+		}
+		/*
+		 * In other words, the result of no critical (multipler of 1
+		 * and no additive term) plus the scaled result of summing over
+		 * the possible criticals rounded to the nearest integer.
+		 */
+		scale = (z_info->r_crit_chance_range / *div)
+			* z_info->r_crit_power_random;
+		*mult = *div + (crit_chance * mult_sum + scale / 2) / scale;
+		*add = (crit_chance * add_sum + scale / 2) / scale;
+	} else {
+		*mult = 100;
+		*add = 0;
+	}
 }
 
 /**
  * Missile crits follow the same approach as melee crits.
  */
-static int o_calculate_missile_crits(struct player_state state,
-									 const struct object *obj,
-									 const struct object *launcher)
+static unsigned int o_calculate_missile_crits(struct player_state *state,
+		const struct object *obj,
+		const struct object *launcher)
 {
-	int dice = 0;
-	int bonus = state.to_h + obj->known->to_h
-		+ (launcher ? launcher->known->to_h : 0);
-	int chance = BTH_PLUS_ADJ * bonus;
-	if (launcher) {
-		chance += state.skills[SKILL_TO_HIT_BOW];
+	unsigned int dice;
+
+	if (z_info->o_r_crit_level_head) {
+		/*
+		 * Pessimistically assume that the target is not debuffed.
+		 * Otherwise, these calculations must agree with those in
+		 * player-attack.c's o_critical_shot().
+		 */
+		struct player_state old_state = player->state;
+		int power, chance_num, chance_den;
+
+		if (z_info->o_r_max_added.n == 0) {
+			z_info->o_r_max_added =
+				sum_o_criticals(z_info->o_r_crit_level_head);
+		}
+
+		player->state = *state;
+		power = chance_of_missile_hit_base(player, obj, launcher);
+		player->state = old_state;
+		if (launcher) {
+			power = (power
+				* z_info->o_r_crit_power_launched_toh_scl_num)
+				/ z_info->o_r_crit_power_launched_toh_scl_den;
+		} else {
+			power = (power
+				* z_info->o_r_crit_power_thrown_toh_scl_num)
+				/ z_info->o_r_crit_power_thrown_toh_scl_den;
+		}
+		chance_num = power * z_info->o_r_crit_chance_power_scl_num;
+		chance_den = power * z_info->o_r_crit_chance_power_scl_den
+			+ z_info->o_r_crit_chance_add_den;
+		if (chance_den > 0 && chance_num > 0) {
+			unsigned int tr;
+
+			if (chance_num < chance_den) {
+				/*
+				 * Critical only happens some of the time.
+				 * Scale by the chance and 100.  Round to the
+				 * nearest integer.
+				 */
+				struct my_rational t = my_rational_construct(
+					chance_num, chance_den);
+
+				t = my_rational_product(&t,
+					&z_info->o_r_max_added);
+				dice = my_rational_to_uint(&t, 100, &tr);
+				if (dice < UINT_MAX && tr >= t.d / 2) {
+					++dice;
+				}
+			} else {
+				/*
+				 * Critical always happens.  Scale by 100
+				 * and round to the nearest integer.
+				 */
+				dice = my_rational_to_uint(
+					&z_info->o_r_max_added, 100,
+					&tr);
+				if (dice < UINT_MAX && tr >=
+						z_info->o_r_max_added.d / 2) {
+					++dice;
+				}
+			}
+		} else {
+			/* No chance of happening so no additional damage. */
+			dice = 0;
+		}
 	} else {
-		chance += state.skills[SKILL_TO_HIT_THROW];
-		chance *= 3 / 2;
+		/* No critical levels defined so no additional damage. */
+		dice = 0;
 	}
-	chance = (100 * chance) / (chance + 360);
-	dice = (569 * chance) / 500;
 
 	return dice;
 }
@@ -802,16 +1052,16 @@ static bool obj_known_damage(const struct object *obj, int *normal_damage,
 	} else if (ammo) {
 		plus += obj->known->to_h;
 
-		calculate_missile_crits(&player->state, obj->weight, plus, &crit_mult,
-								&crit_add, &crit_div);
+		calculate_missile_crits(&player->state, obj->weight, plus,
+			true, &crit_mult, &crit_add, &crit_div);
 
 		dam += (obj->known->to_d * 10);
 		dam += (bow->known->to_d * 10);
 	} else {
 		plus += obj->known->to_h;
 
-		calculate_missile_crits(&player->state, obj->weight, plus, &crit_mult,
-								&crit_add, &crit_div);
+		calculate_missile_crits(&player->state, obj->weight, plus,
+			false, &crit_mult, &crit_add, &crit_div);
 
 		dam += (obj->known->to_d * 10);
 		dam *= 2 + obj->weight / 12;
@@ -985,12 +1235,12 @@ static bool o_obj_known_damage(const struct object *obj, int *normal_damage,
 
 	/* Get the number of additional dice from criticals (x100) */
 	if (weapon)	{
-		dice += o_calculate_melee_crits(state, obj);
+		dice += o_calculate_melee_crits(&state, obj);
 		old_blows = state.num_blows;
 	} else if (ammo) {
-		dice += o_calculate_missile_crits(player->state, obj, bow);
+		dice += o_calculate_missile_crits(&player->state, obj, bow);
 	} else {
-		dice += o_calculate_missile_crits(player->state, obj, NULL);
+		dice += o_calculate_missile_crits(&player->state, obj, NULL);
 		dice *= 2 + obj->weight / 12;
 	}
 

src/init.c
@@ -35,6 +35,7 @@
 #include "generate.h"
 #include "hint.h"
 #include "init.h"
+#include "message.h"
 #include "mon-init.h"
 #include "mon-list.h"
 #include "mon-lore.h"
@@ -698,6 +699,253 @@ static enum parser_error parse_constants_player(struct parser *p) {
 	return PARSE_ERROR_NONE;
 }
 
+static enum parser_error parse_constants_melee_critical(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	const char *label = parser_getsym(p, "label");
+	int value = parser_getint(p, "value");
+
+	if (streq(label, "debuff-toh")) {
+		z->m_crit_debuff_toh = value;
+	} else if (streq(label, "chance-weight-scale")) {
+		z->m_crit_chance_weight_scl = value;
+	} else if (streq(label, "chance-toh-scale")) {
+		z->m_crit_chance_toh_scl = value;
+	} else if (streq(label, "chance-level-scale")) {
+		z->m_crit_chance_level_scl = value;
+	} else if (streq(label, "chance-toh-skill-scale")) {
+		z->m_crit_chance_toh_skill_scl = value;
+	} else if (streq(label, "chance-offset")) {
+		z->m_crit_chance_offset = value;
+	} else if (streq(label, "chance-range")) {
+		z->m_crit_chance_range = value;
+	} else if (streq(label, "power-weight-scale")) {
+		z->m_crit_power_weight_scl = value;
+	} else if (streq(label, "power-random")) {
+		z->m_crit_power_random = value;
+	} else {
+		return PARSE_ERROR_UNDEFINED_DIRECTIVE;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_melee_critical_level(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	struct critical_level *new_level;
+	const char *msgt_str = parser_getstr(p, "msg");
+	int msgt = message_lookup_by_name(msgt_str);
+
+	if (msgt < 0) {
+		return PARSE_ERROR_INVALID_MESSAGE;
+	}
+	new_level = mem_alloc(sizeof(*new_level));
+	new_level->next = NULL;
+	new_level->cutoff = parser_getint(p, "cutoff");
+	new_level->mult = parser_getint(p, "mult");
+	new_level->add = parser_getint(p, "add");
+	new_level->msgt = msgt;
+	/* Add it to the end of the linked list. */
+	if (z->m_crit_level_head) {
+		struct critical_level *cursor = z->m_crit_level_head;
+
+		while (cursor->next) {
+			cursor = cursor->next;
+		}
+		cursor->next = new_level;
+	} else {
+		z->m_crit_level_head = new_level;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_ranged_critical(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	const char *label = parser_getsym(p, "label");
+	int value = parser_getint(p, "value");
+
+	if (streq(label, "debuff-toh")) {
+		z->r_crit_debuff_toh = value;
+	} else if (streq(label, "chance-weight-scale")) {
+		z->r_crit_chance_weight_scl = value;
+	} else if (streq(label, "chance-toh-scale")) {
+		z->r_crit_chance_toh_scl = value;
+	} else if (streq(label, "chance-level-scale")) {
+		z->r_crit_chance_level_scl = value;
+	} else if (streq(label, "chance-launched-toh-skill-scale")) {
+		z->r_crit_chance_launched_toh_skill_scl = value;
+	} else if (streq(label, "chance-thrown-toh-skill-scale")) {
+		z->r_crit_chance_thrown_toh_skill_scl = value;
+	} else if (streq(label, "chance-offset")) {
+		z->r_crit_chance_offset = value;
+	} else if (streq(label, "chance-range")) {
+		z->r_crit_chance_range = value;
+	} else if (streq(label, "power-weight-scale")) {
+		z->r_crit_power_weight_scl = value;
+	} else if (streq(label, "power-random")) {
+		z->r_crit_power_random = value;
+	} else {
+		return PARSE_ERROR_UNDEFINED_DIRECTIVE;
+	}
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_ranged_critical_level(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	struct critical_level *new_level;
+	const char *msgt_str = parser_getstr(p, "msg");
+	int msgt = message_lookup_by_name(msgt_str);
+
+	if (msgt < 0) {
+		return PARSE_ERROR_INVALID_MESSAGE;
+	}
+	new_level = mem_alloc(sizeof(*new_level));
+	new_level->next = NULL;
+	new_level->cutoff = parser_getint(p, "cutoff");
+	new_level->mult = parser_getint(p, "mult");
+	new_level->add = parser_getint(p, "add");
+	new_level->msgt = msgt;
+	/* Add it to the end of the linked list. */
+	if (z->r_crit_level_head) {
+		struct critical_level *cursor = z->r_crit_level_head;
+
+		while (cursor->next) {
+			cursor = cursor->next;
+		}
+		cursor->next = new_level;
+	} else {
+		z->r_crit_level_head = new_level;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_o_melee_critical(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	const char *label = parser_getsym(p, "label");
+	int value = parser_getint(p, "value");
+
+	if (streq(label, "debuff-toh")) {
+		z->o_m_crit_debuff_toh = value;
+	} else if (streq(label, "power-toh-scale-numerator")) {
+		z->o_m_crit_power_toh_scl_num = value;
+	} else if (streq(label, "power-toh-scale-denominator")) {
+		z->o_m_crit_power_toh_scl_den = value;
+	} else if (streq(label, "chance-power-scale-numerator")) {
+		z->o_m_crit_chance_power_scl_num = value;
+	} else if (streq(label, "chance-power-scale-denominator")) {
+		z->o_m_crit_chance_power_scl_den = value;
+	} else if (streq(label, "chance-add-denominator")) {
+		z->o_m_crit_chance_add_den = value;
+	} else {
+		return PARSE_ERROR_UNDEFINED_DIRECTIVE;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_o_melee_critical_level(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	struct o_critical_level *new_level;
+	unsigned int chance = parser_getuint(p, "chance");
+	const char *msgt_str = parser_getstr(p, "msg");
+	int msgt = message_lookup_by_name(msgt_str);
+
+	if (chance == 0) {
+		return PARSE_ERROR_INVALID_VALUE;
+	}
+	if (msgt < 0) {
+		return PARSE_ERROR_INVALID_MESSAGE;
+	}
+	new_level = mem_alloc(sizeof(*new_level));
+	new_level->next = NULL;
+	new_level->chance = chance;
+	new_level->added_dice = parser_getuint(p, "dice");
+	new_level->msgt = msgt;
+	/* Add it to the end of the linked list. */
+	if (z->o_m_crit_level_head) {
+		struct o_critical_level *cursor = z->o_m_crit_level_head;
+
+		while (cursor->next) {
+			cursor = cursor->next;
+		}
+		cursor->next = new_level;
+	} else {
+		z->o_m_crit_level_head = new_level;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_o_ranged_critical(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	const char *label = parser_getsym(p, "label");
+	int value = parser_getint(p, "value");
+
+	if (streq(label, "debuff-toh")) {
+		z->o_r_crit_debuff_toh = value;
+	} else if (streq(label, "power-launched-toh-scale-numerator")) {
+		z->o_r_crit_power_launched_toh_scl_num = value;
+	} else if (streq(label, "power-launched-toh-scale-denominator")) {
+		z->o_r_crit_power_launched_toh_scl_den = value;
+	} else if (streq(label, "power-thrown-toh-scale-numerator")) {
+		z->o_r_crit_power_thrown_toh_scl_num = value;
+	} else if (streq(label, "power-thrown-toh-scale-denominator")) {
+		z->o_r_crit_power_thrown_toh_scl_den = value;
+	} else if (streq(label, "chance-power-scale-numerator")) {
+		z->o_r_crit_chance_power_scl_num = value;
+	} else if (streq(label, "chance-power-scale-denominator")) {
+		z->o_r_crit_chance_power_scl_den = value;
+	} else if (streq(label, "chance-add-denominator")) {
+		z->o_r_crit_chance_add_den = value;
+	} else {
+		return PARSE_ERROR_UNDEFINED_DIRECTIVE;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
+static enum parser_error parse_constants_o_ranged_critical_level(struct parser *p)
+{
+	struct angband_constants *z = parser_priv(p);
+	struct o_critical_level *new_level;
+	unsigned int chance = parser_getuint(p, "chance");
+	const char *msgt_str = parser_getstr(p, "msg");
+	int msgt = message_lookup_by_name(msgt_str);
+
+	if (chance == 0) {
+		return PARSE_ERROR_INVALID_VALUE;
+	}
+	if (msgt < 0) {
+		return PARSE_ERROR_INVALID_MESSAGE;
+	}
+	new_level = mem_alloc(sizeof(*new_level));
+	new_level->next = NULL;
+	new_level->chance = chance;
+	new_level->added_dice = parser_getuint(p, "dice");
+	new_level->msgt = msgt;
+	/* Add it to the end of the linked list. */
+	if (z->o_r_crit_level_head) {
+		struct o_critical_level *cursor = z->o_r_crit_level_head;
+
+		while (cursor->next) {
+			cursor = cursor->next;
+		}
+		cursor->next = new_level;
+	} else {
+		z->o_r_crit_level_head = new_level;
+	}
+
+	return PARSE_ERROR_NONE;
+}
+
 static struct parser *init_parse_constants(void) {
 	struct angband_constants *z = mem_zalloc(sizeof *z);
 	struct parser *p = parser_new();
@@ -712,6 +960,22 @@ static struct parser *init_parse_constants(void) {
 	parser_reg(p, "store sym label int value", parse_constants_store);
 	parser_reg(p, "obj-make sym label int value", parse_constants_obj_make);
 	parser_reg(p, "player sym label int value", parse_constants_player);
+	parser_reg(p, "melee-critical sym label int value",
+		parse_constants_melee_critical);
+	parser_reg(p, "melee-critical-level int cutoff int mult int add "
+		"str msg", parse_constants_melee_critical_level);
+	parser_reg(p, "ranged-critical sym label int value",
+		parse_constants_ranged_critical);
+	parser_reg(p, "ranged-critical-level int cutoff int mult int add "
+		"str msg", parse_constants_ranged_critical_level);
+	parser_reg(p, "o-melee-critical sym label int value",
+		parse_constants_o_melee_critical);
+	parser_reg(p, "o-melee-critical-level uint chance uint dice str msg",
+		parse_constants_o_melee_critical_level);
+	parser_reg(p, "o-ranged-critical sym label int value",
+		parse_constants_o_ranged_critical);
+	parser_reg(p, "o-ranged-critical-level uint chance uint dice str msg",
+		parse_constants_o_ranged_critical_level);
 	return p;
 }
 
@@ -719,14 +983,68 @@ static errr run_parse_constants(struct parser *p) {
 	return parse_file_quit_not_found(p, "constants");
 }
 
+static int check_critical_levels(const struct critical_level *head)
+{
+	/*
+	 * Reject if the cutoffs, except for the last one which is unused, do
+	 * not strictly increase.
+	 */
+	if (!head) {
+		return 0;
+	}
+	while (head->next) {
+		int prev_cutoff = head->cutoff;
+
+		head = head->next;
+		if (head->next && head->cutoff <= prev_cutoff) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
 static errr finish_parse_constants(struct parser *p) {
 	z_info = parser_priv(p);
 	parser_destroy(p);
+	if (check_critical_levels(z_info->m_crit_level_head)) {
+		plog("The cutoffs for melee criticals in constants.txt are "
+			"not strictly increasing.");
+		return PARSE_ERROR_NON_SEQUENTIAL_RECORDS;
+	}
+	if (check_critical_levels(z_info->r_crit_level_head)) {
+		plog("The cutoffs for ranged criticals in constants.txt are "
+			"not strictly increasing.");
+		return PARSE_ERROR_NON_SEQUENTIAL_RECORDS;
+	}
 	return 0;
+}
+
+static void cleanup_critical_levels(struct critical_level *head)
+{
+	while (head) {
+		struct critical_level *target = head;
+
+		head = head->next;
+		mem_free(target);
+	}
+}
+
+static void cleanup_o_critical_levels(struct o_critical_level *head)
+{
+	while (head) {
+		struct o_critical_level *target = head;
+
+		head = head->next;
+		mem_free(target);
+	}
 }
 
 static void cleanup_constants(void)
 {
+	cleanup_critical_levels(z_info->m_crit_level_head);
+	cleanup_critical_levels(z_info->r_crit_level_head);
+	cleanup_o_critical_levels(z_info->o_m_crit_level_head);
+	cleanup_o_critical_levels(z_info->o_r_crit_level_head);
 	mem_free(z_info);
 }
 

commit 865053a7adc9fc8a2242b8afbc685a2e59354153

Object information: have critical chance calculations include chance-offset
Fixes a regression introduced by 7ca6c3760ed01c2bcfde11254daad04eee7b0986 .  Caused chance of a critical (and therefore reported average damage) to be overstated for melee weapons with the O-combat option off.  There's no visible change for ranged weapons (the chance-offset from constants.txt is zero for those).

src/obj-info.c
@@ -477,7 +477,8 @@ static void calculate_melee_crits(struct player_state *state, int weight,
 		+ z_info->m_crit_chance_toh_scl * (state->to_h + plus)
 		+ z_info->m_crit_chance_level_scl * player->lev
 		+ z_info->m_crit_chance_toh_skill_scl
-			* state->skills[SKILL_TO_HIT_MELEE];
+			* state->skills[SKILL_TO_HIT_MELEE]
+		+ z_info->m_crit_chance_offset;
 	crit_chance = MIN(z_info->m_crit_chance_range, MAX(0, crit_chance));
 
 	/* Reported results (*mult and *add) are scaled up by 100. */
@@ -627,7 +628,8 @@ static void calculate_missile_crits(struct player_state *state, int weight,
 	 */
 	int crit_chance = z_info->r_crit_chance_weight_scl * weight
 		+ z_info->r_crit_chance_toh_scl * (state->to_h + plus)
-		+ z_info->r_crit_chance_level_scl * player->lev;
+		+ z_info->r_crit_chance_level_scl * player->lev
+		+ z_info->r_crit_chance_offset;
 
 	if (launched) {
 		crit_chance += z_info->r_crit_chance_launched_toh_skill_scl