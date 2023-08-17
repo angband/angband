ad4152e49091aa5dd017c18c25925d1e640b03f7
Eric Branlund <ebranlund@fastmail.com>
2023-08-17 16:10:29 -0600

    Add missing copyright comment to ui-effect.c

--- src/ui-effect.c
+++ src/ui-effect.c
@@ -2,6 +2,8 @@
  * \file ui-effect.c
  * \brief Implement functions for the text UI's handling of effects
  *
+ * Copyright (c) 2021 Eric Branlund
+ *
  * This work is free software; you can redistribute it and/or modify it
  * under the terms of either:
  *
