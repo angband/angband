angband/angband

commit f1cf6484d8b5d98b157f6db52a0826199f7e23a0

Latest pull requests from the borg (#5753)
* Define borg_perma_spell() as taking void as the argument

Avoids old-style function declaration warning from gcc.

* Borg: avoid redundant declaration warnings from gcc

Seen when compiling with Makefile.std.

* borg: replace strcpy()/strcat() with Angband's my_strcpy()/my_strcat()

Avoids warnings from clang on OpenBSD.

* Borg: fully reinitialize if left the game since the last initialization

Resolves https://github.com/angband/angband/issues/5742 .  Also plugs memory leaks from borg9.c's insert_msg() if the message duplicates another.

* Borg: plug memory leaks in borg_init_3()

* Avoid alignment warning from clang for borg_inkey_hack()

Resolves https://github.com/angband/angband/issues/5741 .

---------

Co-authored-by: Eric Branlund <ebranlund@fastmail.com>

src/borg/borg3.c
@@ -2401,8 +2401,8 @@ extern bool borg_inscribe_food(void)
             if (item->sval == sv_food_ration)
             {
                 /* get a name */
-                strcpy(name, food_syllable1[randint0(sizeof(food_syllable1) / sizeof(char*))]);
-                strcat(name, food_syllable2[randint0(sizeof(food_syllable2) / sizeof(char*))]);
+                my_strcpy(name, food_syllable1[randint0(sizeof(food_syllable1) / sizeof(char*))], sizeof(name));
+                my_strcat(name, food_syllable2[randint0(sizeof(food_syllable2) / sizeof(char*))], sizeof(name));
 
                 borg_send_inscribe(ii, name);
                 return (true);
@@ -2411,9 +2411,9 @@ extern bool borg_inscribe_food(void)
             if (item->sval == sv_food_slime_mold)
             {
                 /* get a name */
-                strcpy(name, mold_syllable1[randint0(sizeof(mold_syllable1) / sizeof(char*))]);
-                strcat(name, mold_syllable2[randint0(sizeof(mold_syllable2) / sizeof(char*))]);
-                strcat(name, mold_syllable3[randint0(sizeof(mold_syllable3) / sizeof(char*))]);
+                my_strcpy(name, mold_syllable1[randint0(sizeof(mold_syllable1) / sizeof(char*))], sizeof(name));
+                my_strcat(name, mold_syllable2[randint0(sizeof(mold_syllable2) / sizeof(char*))], sizeof(name));
+                my_strcat(name, mold_syllable3[randint0(sizeof(mold_syllable3) / sizeof(char*))], sizeof(name));
 
                 borg_send_inscribe(ii, name);
                 return (true);
@@ -3002,6 +3002,9 @@ void borg_init_3(void)
 
         size++;
 
+        mem_free(hack.brands);
+        mem_free(hack.slays);
+        mem_free(hack.curses);
     }
 
     /* Set the sort hooks */
@@ -3059,6 +3062,10 @@ void borg_init_3(void)
         text[size] = string_make(buf);
         what[size] = k;
         size++;
+
+        mem_free(hack.brands);
+        mem_free(hack.slays);
+        mem_free(hack.curses);
     }
 
     /* Analyze the "INSTA_ART" items */
@@ -3079,7 +3086,12 @@ void borg_init_3(void)
         /* Hack -- make an item */
         object_prep(&hack, &k_info[k], 10, MINIMISE);
 
-        if (!hack.known) continue;
+        if (!hack.known) {
+        	mem_free(hack.brands);
+        	mem_free(hack.slays);
+        	mem_free(hack.curses);
+		continue;
+	}
 
         /* Save the index */
         /* hack.name1 = i; */
@@ -3098,6 +3110,10 @@ void borg_init_3(void)
         text[size] = string_make(buf);
         what[size] = k;
         size++;
+
+        mem_free(hack.brands);
+        mem_free(hack.slays);
+        mem_free(hack.curses);
     }
 
     /* Set the sort hooks */
@@ -3177,6 +3193,59 @@ void borg_init_3(void)
     /* Save the entries */
     for (i = 0; i < size; i++) borg_artego_text[i] = text[i];
     for (i = 0; i < size; i++) borg_artego_what[i] = what[i];
+}
+
+/*
+ * Release resources allocated by borg_init_3().
+ */
+void borg_clean_3(void)
+{
+    int i;
+
+    mem_free(borg_artego_what);
+    borg_artego_what = NULL;
+    if (borg_artego_text) {
+        for (i = 0; i < borg_artego_size; ++i) {
+            string_free(borg_artego_text[i]);
+        }
+        mem_free(borg_artego_text);
+        borg_artego_text = NULL;
+    }
+    borg_artego_size = 0;
+    mem_free(borg_sv_art_text);
+    borg_sv_art_text = NULL;
+    mem_free(borg_single_what);
+    borg_single_what = NULL;
+    if (borg_single_text) {
+        for (i = 0; i < borg_single_size; ++i) {
+            string_free(borg_single_text[i]);
+        }
+        mem_free(borg_single_text);
+        borg_single_text = NULL;
+    }
+    borg_single_size = 0;
+    mem_free(borg_plural_what);
+    borg_plural_what = NULL;
+    if (borg_plural_text) {
+        for (i = 0; i < borg_plural_size; ++i) {
+            string_free(borg_plural_text[i]);
+        }
+        mem_free(borg_plural_text);
+        borg_plural_text = NULL;
+    }
+    borg_plural_size = 0;
+    mem_free(borg_sv_plural_text);
+    borg_sv_plural_text = NULL;
+    mem_free(safe_shops);
+    safe_shops = NULL;
+    mem_free(safe_home);
+    safe_home = NULL;
+    mem_free(safe_items);
+    safe_items = NULL;
+    mem_free(borg_shops);
+    borg_shops = NULL;
+    mem_free(borg_items);
+    borg_items = NULL;
 }
 
 const char* borg_prt_item(int item)

src/borg/borg1.c
@@ -2214,80 +2214,79 @@ char* borg_prt_formula(int* arg_formula)
         case BFO_NUMBER:
 
         strnfmt(tmpStr, sizeof(tmpStr), "%d ", *++arg_formula);
-        strcat(FormulaStr, tmpStr);
+        my_strcat(FormulaStr, tmpStr, sizeof(FormulaStr));
         break;
 
         /* Variable */
         case BFO_VARIABLE:
-        strcat(FormulaStr, "'");
-        strcat(FormulaStr, borg_prt_item(*++arg_formula));
-        strcat(FormulaStr, "'");
-        strcat(FormulaStr, " ");
+        my_strcat(FormulaStr, "'", sizeof(FormulaStr));
+        my_strcat(FormulaStr, borg_prt_item(*++arg_formula), sizeof(FormulaStr));
+        my_strcat(FormulaStr, "' ", sizeof(FormulaStr));
         break;
 
         /* Equal */
         case BFO_EQ:
-        strcat(FormulaStr, "== ");
+        my_strcat(FormulaStr, "== ", sizeof(FormulaStr));
         break;
 
         /* Not Equal */
         case BFO_NEQ:
-        strcat(FormulaStr, "!= ");
+        my_strcat(FormulaStr, "!= ", sizeof(FormulaStr));
         break;
 
         /* Less Than */
         case BFO_LT:
-        strcat(FormulaStr, "< ");
+        my_strcat(FormulaStr, "< ", sizeof(FormulaStr));
         break;
 
         /* Less Than Or Equal */
         case BFO_LTE:
-        strcat(FormulaStr, "<= ");
+        my_strcat(FormulaStr, "<= ", sizeof(FormulaStr));
         break;
 
         /* Greater Than */
         case BFO_GT:
-        strcat(FormulaStr, "> ");
+        my_strcat(FormulaStr, "> ", sizeof(FormulaStr));
         break;
 
         /* Greater Than Or Equal */
         case BFO_GTE:
-        strcat(FormulaStr, ">= ");
+        my_strcat(FormulaStr, ">= ", sizeof(FormulaStr));
         break;
 
         /* Logical And */
         case BFO_AND:
-        strcat(FormulaStr, "&& ");
+        my_strcat(FormulaStr, "&& ", sizeof(FormulaStr));
         break;
 
         /* Logical Or */
         case BFO_OR:
-        strcat(FormulaStr, "|| ");
+        my_strcat(FormulaStr, "|| ", sizeof(FormulaStr));
         break;
 
         /* Plus */
         case BFO_PLUS:
-        strcat(FormulaStr, "+ ");
+        my_strcat(FormulaStr, "+ ", sizeof(FormulaStr));
         break;
 
         /* Minus */
         case BFO_MINUS:
-        strcat(FormulaStr, "- ");
+        my_strcat(FormulaStr, "- ", sizeof(FormulaStr));
         break;
 
         /* Divide */
         case BFO_DIVIDE:
-        strcat(FormulaStr, "/ ");
+        my_strcat(FormulaStr, "/ ", sizeof(FormulaStr));
         break;
 
         /* Multiply */
         case BFO_MULT:
-        strcat(FormulaStr, "* ");
+        my_strcat(FormulaStr, "* ", sizeof(FormulaStr));
         break;
 
         /* Logical Not */
         case BFO_NOT:
-        strcat(FormulaStr, "! ");
+        my_strcat(FormulaStr, "! ", sizeof(FormulaStr));
         break;
         }
     }
@@ -2607,7 +2606,6 @@ int sv_potion_boldness;
 int sv_potion_detect_invis;
 int sv_potion_enlightenment;
 int sv_potion_slime_mold;
-int sv_potion_berserk;
 int sv_potion_infravision;
 int sv_potion_inc_exp;
 
@@ -3365,6 +3363,17 @@ static void borg_init_track(struct borg_track* track, int size)
     track->x = mem_zalloc(size * sizeof(int));
     track->y = mem_zalloc(size * sizeof(int));
 }
+
+static void borg_clean_track(struct borg_track* track)
+{
+    track->num = 0;
+    track->size = 0;
+    mem_free(track->x);
+    track->x = NULL;
+    mem_free(track->y);
+    track->y = NULL;
+}
+
 struct player* borg_p;
 /*
  * Initialize this file
@@ -3530,6 +3539,52 @@ void borg_init_1(void)
         borg_note("**STARTUP FAILURE** classes do not match");
         borg_init_failure = true;
     }
+}
+
+/*
+ * Release the resources allocated by borg_init_1().
+ */
+void borg_clean_1(void)
+{
+    int y;
+
+    mem_free(borg_race_death);
+    borg_race_death = NULL;
+    mem_free(borg_race_count);
+    borg_race_count = NULL;
+    mem_free(borg_kills);
+    borg_kills = NULL;
+    mem_free(borg_takes);
+    borg_takes = NULL;
+    borg_clean_track(&track_vein);
+    borg_clean_track(&track_closed);
+    borg_clean_track(&track_door);
+    borg_clean_track(&track_step);
+    borg_clean_track(&track_glyph);
+    borg_clean_track(&track_more);
+    borg_clean_track(&track_less);
+    mem_free(track_worn_name1);
+    track_worn_name1 = NULL;
+    mem_free(track_shop_y);
+    track_shop_y = NULL;
+    mem_free(track_shop_x);
+    track_shop_x = NULL;
+    mem_free(borg_data_icky);
+    borg_data_icky = NULL;
+    mem_free(borg_data_know);
+    borg_data_know = NULL;
+    mem_free(borg_data_hard);
+    borg_data_hard = NULL;
+    mem_free(borg_data_cost);
+    borg_data_cost = NULL;
+    mem_free(borg_data_flow);
+    borg_data_flow = NULL;
+    for (y = 0; y < AUTO_MAX_Y; ++y) {
+        mem_free(borg_grids[y]);
+        borg_grids[y] = NULL;
+    }
+    mem_free(borg_key_queue);
+    borg_key_queue = NULL;
 }
 
 /*** Object kind lookup functions ***/

src/borg/borg1.h
@@ -134,7 +134,6 @@ extern int sv_potion_boldness;
 extern int sv_potion_detect_invis;
 extern int sv_potion_enlightenment;
 extern int sv_potion_slime_mold;
-extern int sv_potion_berserk;
 extern int sv_potion_infravision;
 extern int sv_potion_inc_exp;
 
@@ -252,7 +251,6 @@ extern int sv_wand_drain_life;
 
 extern int sv_dagger;
 
-extern int sv_sling;
 extern int sv_sling;
 extern int sv_short_bow;
 extern int sv_long_bow;
@@ -2090,6 +2088,7 @@ extern bool borg_is_ammo(int tval);
  * Initialize this file
  */
 extern void borg_init_1(void);
+extern void borg_clean_1(void);
 
 #endif
 

src/borg/borg2.c
@@ -1313,7 +1313,13 @@ void borg_init_2(void)
     /* Nothing */
 }
 
-
+/*
+ * Release any resources allocated by borg_init_2().
+ */
+void borg_clean_2(void)
+{
+    /* Nothing */
+}
 
 #else
 

src/borg/borg2.h
@@ -74,6 +74,7 @@ extern void borg_update_view(void);
  * Initialize this file
  */
 extern void borg_init_2(void);
+extern void borg_clean_2(void);
 
 
 #endif

src/borg/borg3.h
@@ -492,6 +492,7 @@ extern int borg_first_empty_inventory_slot(void);
  * Initialize this file
  */
 extern void borg_init_3(void);
+extern void borg_clean_3(void);
 
 
 #endif

src/borg/borg4.c
@@ -619,8 +619,6 @@ static const int borg_adj_con_mhp[STAT_RANGE] =
 };
 
 
-extern const int adj_str_hold[STAT_RANGE];
-extern const int adj_str_blow[STAT_RANGE];
 int borg_calc_blows(int extra_blows);
 
 
@@ -10085,7 +10083,13 @@ void borg_init_4(void)
     /* Do nothing? */
 }
 
-
+/*
+ * Release resources allocated by borg_init_4().
+ */
+void borg_clean_4(void)
+{
+    /* Nothing */
+}
 
 #ifdef MACINTOSH
 static int HACK = 0;

src/borg/borg4.h
@@ -78,6 +78,7 @@ extern const char* borg_prepared(int depth);
  * Initialize this file
  */
 extern void borg_init_4(void);
+extern void borg_clean_4(void);
 
 
 #endif

commit f33a664e5dc27ac9bd3766f2e57e06236f9cbcd2

Fix whitespace
Handles a regression from f1cf6484d8b5d98b157f6db52a0826199f7e23a0

src/borg/borg3.c
@@ -3087,10 +3087,10 @@ void borg_init_3(void)
         object_prep(&hack, &k_info[k], 10, MINIMISE);
 
         if (!hack.known) {
-        	mem_free(hack.brands);
-        	mem_free(hack.slays);
-        	mem_free(hack.curses);
-		continue;
+            mem_free(hack.brands);
+            mem_free(hack.slays);
+            mem_free(hack.curses);
+            continue;
 	}
 
         /* Save the index */