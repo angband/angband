angband/angband

commit 4d76112f25e5287b9c2ebfdaef80c72d13928900

Refactor player-path
For calculating multiple paths from the same starting point, make the distance array calculation public and have it return its results rather than storing them in a static array.  In the internals of that calculation, replace the point set with a queue and explicitly have the edges of the cave as unreachable to simplify bounds checks.

With that distance calculation available, have the nearest staircase/nearest unexplored calculations in do_cmd_navigate_down(), do_cmd_navigate_up(), and do_cmd_explore() use the expected number of turns to navigate the path rather than distance().  Move the search for the nearest to path_nearest_known() and path_nearest_unknown() (declared in player-path.h; implemented in player-path.c).

Change the interface of find_path() so it returns the steps in the path rather than storing them in a static array.  Change struct player_upkeep so all the state for running with pathfinding is there.  Removes the running_with_pathfind element of that structure; test for a non-NULL steps field in that structure instead.

Change the internals of find_path() to use an A* algorithm.  Add a priority queue type in z-queue.{c,h} to use in that algorithm.

src/cmd-cave.c
@@ -1393,8 +1393,8 @@ void do_cmd_run(struct command *cmd)
  */
 void do_cmd_navigate_down(struct command *cmd)
 {
-	struct loc target_grid;
 	int visible_monster_count = 0;
+
 	/* cancel if confused */
 	if (player->timed[TMD_CONFUSED]) {
 		msg("You cannot explore while confused.");
@@ -1412,8 +1412,7 @@ void do_cmd_navigate_down(struct command *cmd)
 	}
 	
 
-	int min_dist = 9999;
-	/* Get location */
+	/* Screen for visible monsters */
 	for (int y = 0; y < cave->height; y++) {
 		for (int x = 0; x < cave->width; x++) {
 			struct loc grid = loc(x, y);
@@ -1428,13 +1427,6 @@ void do_cmd_navigate_down(struct command *cmd)
 					break;
 				}
 			}
-			if (square_isknown(cave, grid) && square_isdownstairs(player->cave, grid)) {
-				int d = distance(grid, player->grid);
-				if (d < min_dist) {
-					target_grid = grid;
-					min_dist = d;
-				}
-			}
 		}
 	}
 
@@ -1443,11 +1435,14 @@ void do_cmd_navigate_down(struct command *cmd)
 		return;
 	}
 
-	if (min_dist < 9999 && find_path(target_grid)) {
-		player->upkeep->running = 1000;
+	assert(!player->upkeep->steps);
+	player->upkeep->step_count = path_nearest_known(player, player->grid,
+		square_isdownstairs, &player->upkeep->path_dest,
+		&player->upkeep->steps);
+	if (player->upkeep->step_count > 0) {
+		player->upkeep->running = player->upkeep->step_count;
 		/* Calculate torch radius */
 		player->upkeep->update |= (PU_TORCH);
-		player->upkeep->running_withpathfind = true;
 		run_step(0);
 		return;
 	}
@@ -1462,7 +1457,6 @@ void do_cmd_navigate_down(struct command *cmd)
  */
 void do_cmd_navigate_up(struct command *cmd)
 {
-	struct loc target_grid;
 	int visible_monster_count = 0;
 	/* cancel if confused */
 	if (player->timed[TMD_CONFUSED]) {
@@ -1481,12 +1475,11 @@ void do_cmd_navigate_up(struct command *cmd)
 	}
 	
 
-	int min_dist = 9999;
-	/* Get location */
+	/* Screen for visible monsters */
 	for (int y = 0; y < cave->height; y++) {
 		for (int x = 0; x < cave->width; x++) {
 			struct loc grid = loc(x, y);
-			
+
 			if (loc_eq(grid, player->grid)) continue;
 
 			if (square_isoccupied(cave, grid)) {
@@ -1497,13 +1490,6 @@ void do_cmd_navigate_up(struct command *cmd)
 					break;
 				}
 			}
-			if (square_isknown(cave, grid) && square_isupstairs(player->cave, grid)) {
-				int d = distance(grid, player->grid);
-				if (d < min_dist) {
-					target_grid = grid;
-					min_dist = d;
-				}
-			}
 		}
 	}
 
@@ -1512,11 +1498,14 @@ void do_cmd_navigate_up(struct command *cmd)
 		return;
 	}
 
-	if (min_dist < 9999 && find_path(target_grid)) {
-		player->upkeep->running = 1000;
+	assert(!player->upkeep->steps);
+	player->upkeep->step_count = path_nearest_known(player, player->grid,
+		square_isupstairs, &player->upkeep->path_dest,
+		&player->upkeep->steps);
+	if (player->upkeep->step_count > 0) {
+		player->upkeep->running = player->upkeep->step_count;
 		/* Calculate torch radius */
 		player->upkeep->update |= (PU_TORCH);
-		player->upkeep->running_withpathfind = true;
 		run_step(0);
 		return;
 	}
@@ -1531,7 +1520,6 @@ void do_cmd_navigate_up(struct command *cmd)
  */
 void do_cmd_explore(struct command *cmd)
 {
-	struct loc target_grid;
 	bool visible_monster = false;
 	/* cancel if confused */
 	if (player->timed[TMD_CONFUSED]) {
@@ -1550,8 +1538,7 @@ void do_cmd_explore(struct command *cmd)
 	}
 	
 
-	int min_dist = 9999;
-	/* Get location */
+	/* Screen for visible monsters */
 	for (int y = 0; y < cave->height && !visible_monster; y++) {
 		for (int x = 0; x < cave->width; x++) {
 			struct loc grid = loc(x, y);
@@ -1566,19 +1553,6 @@ void do_cmd_explore(struct command *cmd)
 					break; /* only breaks the inner loop */
 				}
 			}
-			
-			/* only consider known locations which are passable and have unexplored neighbors */
-			if (square_isknown(cave, grid) &&
-				square_ispassable(player->cave, grid) &&
-				count_neighbors(NULL, cave, grid, square_isknown, false) != 8) {
-
-				int d = distance(grid, player->grid);
-
-				if (d < min_dist) {
-					target_grid = grid;
-					min_dist = d;
-				}
-			}
 		}
 	}
 
@@ -1587,60 +1561,13 @@ void do_cmd_explore(struct command *cmd)
 		return;
 	}
 
-	if (min_dist < 9999 && find_path(target_grid)) {
-		player->upkeep->running = 1000;
+	assert(!player->upkeep->steps);
+	player->upkeep->step_count = path_nearest_unknown(player, player->grid,
+		&player->upkeep->path_dest, &player->upkeep->steps);
+	if (player->upkeep->step_count > 0) {
+		player->upkeep->running = player->upkeep->step_count;
 		/* Calculate torch radius */
 		player->upkeep->update |= (PU_TORCH);
-		player->upkeep->running_withpathfind = true;
-		run_step(0);
-		return;
-	}
-
-	/* 
-	 * No unexplored location found, walk to a door/rubble grid position,
-	 * that has an explored and unexplored side.
-	 */
-	min_dist = 9999;
-	for (int y = 0; y < cave->height; y++) {
-		for (int x = 0; x < cave->width; x++) {
-			struct loc grid = loc(x, y);
-
-			/* only check known locations, which are either rubble or a closed door */
-			if (!square_isknown(cave, grid) ||
-			   (!square_iscloseddoor(player->cave, grid) && !square_isrubble(player->cave, grid))) {
-				continue;
-			}
-			
-			/* if no unexplored neighbors -> reject */
-			if (count_neighbors(NULL, cave, grid, square_isknown, false) == 8) {
-				continue;
-			}
-
-			struct loc explored_neighbor;
-
-			/* if no passable known neighbors -> reject */
-			if (count_neighbors(&explored_neighbor, cave, grid, square_isknownpassable, false) == 0) {
-				continue;
-			}
-
-			if (loc_eq(explored_neighbor, player->grid)) {
-				continue;
-			}
-			
-			/* found an interesting door/rubble location */
-			int d = distance(explored_neighbor, player->grid);
-			if (d < min_dist) {
-				target_grid = explored_neighbor;
-				min_dist = d;
-			}
-		}
-	}
-	/* try pathfinding there */
-	if (min_dist < 9999 && find_path(target_grid)) {
-		player->upkeep->running = 1000;
-		/* Calculate torch radius */
-		player->upkeep->update |= (PU_TORCH);
-		player->upkeep->running_withpathfind = true;
 		run_step(0);
 		return;
 	}
@@ -1664,11 +1591,14 @@ void do_cmd_pathfind(struct command *cmd)
 	if (player->timed[TMD_CONFUSED])
 		return;
 
-	if (find_path(grid)) {
-		player->upkeep->running = 1000;
+	assert(!player->upkeep->steps);
+	player->upkeep->step_count =
+		find_path(player, player->grid, grid, &player->upkeep->steps);
+	if (player->upkeep->step_count > 0) {
+		player->upkeep->path_dest = grid;
+		player->upkeep->running = player->upkeep->step_count;
 		/* Calculate torch radius */
 		player->upkeep->update |= (PU_TORCH);
-		player->upkeep->running_withpathfind = true;
 		run_step(0);
 	}
 }

src/player-path.h
@@ -22,8 +22,22 @@
 
 #include "z-type.h"
 
+struct pfdistances;
+
+struct pfdistances *prepare_pfdistances(struct player *p, struct loc start,
+		bool only_known, bool forbid_traps);
+int pfdistances_to_turncount(const struct pfdistances *a, struct loc grid);
+int pfdistances_to_path(const struct pfdistances *a, struct loc grid,
+		int16_t **step_dirs);
+void release_pfdistances(struct pfdistances *a);
+int path_nearest_known(struct player *p, struct loc start,
+		bool (*pred)(struct chunk*, struct loc),
+		struct loc *dest_grid, int16_t **step_dirs);
+int path_nearest_unknown(struct player *p, struct loc start,
+		struct loc *dest_grid, int16_t **step_dirs);
+int find_path(struct player *p, struct loc start, struct loc dest,
+		int16_t **step_dirs);
 int pathfind_direction_to(struct loc from, struct loc to);
-bool find_path(struct loc grid);
 void run_step(int dir);
 
 #endif /* !PLAYER_PATH_H */

src/player-util.c
@@ -1639,6 +1639,8 @@ void disturb(struct player *p)
 	/* Cancel running */
 	if (p->upkeep->running) {
 		p->upkeep->running = 0;
+		mem_free(p->upkeep->steps);
+		p->upkeep->steps = NULL;
 
 		/* Cancel queued commands */
 		cmdq_flush();

src/player.c
@@ -446,6 +446,7 @@ void player_cleanup_members(struct player *p)
 	if (p->upkeep) {
 		mem_free(p->upkeep->quiver);
 		mem_free(p->upkeep->inven);
+		mem_free(p->upkeep->steps);
 		mem_free(p->upkeep);
 		p->upkeep = NULL;
 	}

src/player.h
@@ -480,7 +480,6 @@ struct player_upkeep {
 	int resting;			/* Resting counter */
 
 	int running;				/* Running counter */
-	bool running_withpathfind;	/* Are we using the pathfinder ? */
 	bool running_firststep;		/* Is this our first step running? */
 
 	struct object **quiver;	/* Quiver objects */
@@ -490,6 +489,9 @@ struct player_upkeep {
 	int equip_cnt;			/* Number of items in equipment */
 	int quiver_cnt;			/* Number of items in the quiver */
 	int recharge_pow;		/* Power of recharge effect */
+	int step_count;			/* Pathfinding: number of steps left */
+	int16_t *steps;			/* Pathfinding: steps in reverse order */
+	struct loc path_dest;		/* Pathfinding: destination grid */
 };
 
 /**

src/tests/Makefile
@@ -22,6 +22,7 @@ SUITES = \
 	z-expression/suite.mk \
 	z-file/suite.mk \
 	z-quark/suite.mk \
+	z-queue/suite.mk \
 	z-textblock/suite.mk \
 	z-util/suite.mk \
 	z-virt/suite.mk

src/tests/unit-test-data.h
@@ -1052,7 +1052,6 @@ static struct player_upkeep TEST_DATA test_player_upkeep = {
 	.create_down_stair = 0,
 
 	.running = 0,
-	.running_withpathfind = 0,
 	.running_firststep = 0,
 
 	.quiver = NULL,
@@ -1062,6 +1061,9 @@ static struct player_upkeep TEST_DATA test_player_upkeep = {
 	.inven_cnt = 0,
 	.equip_cnt = 0,
 	.quiver_cnt = 0,
+	.step_count = 0,
+	.steps = NULL,
+	.path_dest = { 0, 0 },
 };
 
 static struct object TEST_DATA test_player_knowledge = {

commit 1963d1695f577bd4d32fbe19f2c24c421fa5d651

Mark commands used internally by pathfinding as background commands
Background commands now come in two flavors:  those that can trigger bloodlust and those that do not.  Generally gives better behavior than 4.2.5 when repeating the last command after an interrupted pathfinding command:  repeats the pathfinding command rather than the last run used to take a step along the path.  Fixes a regression introduced by 4d76112f25e5287b9c2ebfdaef80c72d13928900 : when compiled with -fsanitize=address and -fsanitize=undefined, repeating the last command after an interrupted pathfind when no normal run had been used in the session would trigger a crash in run_test() (in that case, the last command is run with a direction of zero and prev_run_dir is zero).

Has the command queue manage the lifetime of string arguments to commands.  Resolves a use after free if the command to inscribe an object is followed immediately by the command to repeat the last command.

src/cmd-cave.c
@@ -1175,9 +1175,10 @@ void move_player(int dir, bool disarm)
 			 * The autopickup is a side effect of the move:
 			 * whatever command triggered the move will be the
 			 * target for CMD_REPEAT rather than repeating the
-			 * autopickup.
+			 * autopickup, and the autopickup won't trigger
+			 * bloodlust.
 			 */
-			cmdq_peek()->is_background_command = true;
+			cmdq_peek()->background_command = 2;
 		} else {
 			/* No move made so no energy spent. */
 			player->upkeep->energy_use = 0;

src/cmd-core.c
@@ -174,6 +174,39 @@ static const struct command_info game_cmds[] =
 	{ CMD_WIZ_WIZARD_LIGHT, "wizard light the level", do_cmd_wiz_wizard_light, false, false, 0 },
 };
 
+/**
+ * Make a deep copy of a command and its arguments.
+ */
+void cmd_copy(struct command *dest, const struct command *src)
+{
+	int i;
+
+	*dest = *src;
+	/* String arguments require a deep copy. */
+	for (i = 0; i < CMD_MAX_ARGS; ++i) {
+		if (dest->arg[i].type == arg_STRING) {
+			dest->arg[i].data.string =
+				string_make(src->arg[i].data.string);
+		}
+	}
+}
+
+/**
+ * Release any resources for the command's arguments.
+ */
+void cmd_release(struct command *cmd)
+{
+	int i;
+
+	for (i = 0; i < CMD_MAX_ARGS; ++i) {
+		if (cmd->arg[i].type == arg_STRING) {
+			string_free((char*)(cmd->arg[i].data.string));
+			cmd->arg[i].name[0] = '\0';
+			cmd->arg[i].type = arg_NONE;
+		}
+	}
+}
+
 const char *cmd_verb(cmd_code cmd)
 {
 	size_t i;
@@ -211,41 +244,23 @@ static int cmd_head = 0;
 static int cmd_tail = 0;
 static struct command cmd_queue[CMD_QUEUE_SIZE];
 
+/*
+ * Remember last non-background command executed for use by CMD_REPEAT; either
+ * still in the queue at last_command_idx or copied out of the queue into
+ * last_command
+ */
+int last_command_idx = -1;
+static struct command last_command = {
+	.context = CTX_INIT,
+	.code = CMD_NULL,
+	.nrepeats = 0,
+	.background_command = 0,
+	.arg = { { 0 } }
+};
+
 static bool repeat_prev_allowed = false;
 static bool repeating = false;
 
-
-/**
- * Locate and return the index of the first previous command that is an
- * appropriate target for CMD_REPEAT.  If no such command is available,
- * return a negative value.
- */
-static int cmdq_find_repeat_target_index(void)
-{
-	int target = cmd_head - 1;
-
-	while (1) {
-		if (target < 0) target = CMD_QUEUE_SIZE - 1;
-
-		if (target == cmd_tail) {
-			break;
-		}
-		/*
-		 * Only repeat a command that has not been marked
-		 * as a background command (i.e. it is not a side
-		 * effect of something else).
-		 */
-		if (!cmd_queue[target].is_background_command) {
-			if (cmd_queue[target].code != CMD_NULL) {
-				return target;
-			}
-			break;
-		}
-		/* Keep looking. */
-		--target;
-	}
-	return -1;
-}
 
 struct command *cmdq_peek(void)
 {
@@ -255,6 +270,9 @@ struct command *cmdq_peek(void)
 
 /**
  * Insert the given command into the command queue.
+ *
+ * Makes a shallow copy of the command.  If the command has string arguments,
+ * the queue assumes ownership of those arguments.
  */
 errr cmdq_push_copy(struct command *cmd)
 {
@@ -264,20 +282,38 @@ errr cmdq_push_copy(struct command *cmd)
 
 	/* Insert command into queue. */
 	if (cmd->code != CMD_REPEAT) {
+		if (last_command_idx == cmd_head) {
+			/*
+			 * The last non-background command will be overwritten.
+			 * Copy out of the queue in case it is needed for
+			 * CMD_REPEAT.
+			 */
+			cmd_release(&last_command);
+			cmd_copy(&last_command, &cmd_queue[cmd_head]);
+			last_command_idx = -1;
+		}
+		cmd_release(&cmd_queue[cmd_head]);
 		cmd_queue[cmd_head] = *cmd;
 	} else if (!repeat_prev_allowed) {
 		return 1;
 	} else {
-		int idx_to_repeat = cmdq_find_repeat_target_index();
-
-		if (idx_to_repeat < 0) {
-			return 1;
-		}
 		/*
 		 * If we're repeating a command, we duplicate the previous
 		 * command in the next command "slot".
 		 */
-		cmd_queue[cmd_head] = cmd_queue[idx_to_repeat];
+		if (last_command_idx >= 0) {
+			assert(last_command_idx < CMD_QUEUE_SIZE);
+			if (last_command_idx != cmd_head) {
+				cmd_release(&cmd_queue[cmd_head]);
+				cmd_copy(&cmd_queue[cmd_head],
+					&cmd_queue[last_command_idx]);
+			}
+		} else if (last_command.code != CMD_NULL) {
+			cmd_release(&cmd_queue[cmd_head]);
+			cmd_copy(&cmd_queue[cmd_head], &last_command);
+		} else {
+			return 1;
+		}
 	}
 
 	/* Advance point in queue, wrapping around at the end */
@@ -322,11 +358,12 @@ static void process_command(cmd_context ctx, struct command *cmd)
 	/* Actually execute the command function */
 	if (game_cmds[idx].fn) {
 		/* Occasional attack instead for bloodlust-affected characters */
-		if (cmd->is_background_command) {
+		if (cmd->background_command > 1) {
 			/*
-			 * Background commands do not trigger bloodlust.  If
-			 * they can take energy, they also don't reset whether
-			 * the player's next command skips the bloodlust check.
+			 * Some background commands do not trigger bloodlust.
+			 * If they can take energy, they also don't reset
+			 * whether the player's next command skips the
+			 * bloodlust check.
 			 */
 			if (player->skip_cmd_coercion
 					&& game_cmds[idx].can_use_energy) {
@@ -378,6 +415,9 @@ bool cmdq_pop(cmd_context c)
 	}
 
 	/* Now process it */
+	if (!cmd->background_command) {
+		last_command_idx = prev_cmd_idx(cmd_tail);
+	}
 	process_command(c, cmd);
 	return true;
 }
@@ -392,7 +432,7 @@ errr cmdq_push_repeat(cmd_code c, int nrepeats)
 		.context = CTX_INIT,
 		.code = CMD_NULL,
 		.nrepeats = 0,
-		.is_background_command = false,
+		.background_command = 0,
 		.arg = { { 0 } }
 	};
 
@@ -429,6 +469,22 @@ void cmdq_execute(cmd_context ctx)
 void cmdq_flush(void)
 {
 	cmd_tail = cmd_head;
+}
+
+/**
+ * Remove all commands from the queue and release any allocated resources.
+ */
+void cmdq_release(void)
+{
+	int i;
+
+	cmdq_flush();
+	for (i = 0; i < CMD_QUEUE_SIZE; ++i) {
+		cmd_release(cmd_queue + i);
+	}
+	cmd_release(&last_command);
+	last_command.code = CMD_NULL;
+	last_command_idx = -1;
 }
 
 /**
@@ -555,8 +611,14 @@ static void cmd_set_arg(struct command *cmd, const char *name,
 
 	assert(first_empty != -1 || idx != -1);
 
-	if (idx == -1)
+	if (idx == -1) {
 		idx = first_empty;
+	} else {
+		/* Free allocated string if overwriting a string argument. */
+		if (cmd->arg[idx].type == arg_STRING) {
+			string_free((char*)(cmd->arg[idx].data.string));
+		}
+	}
 
 	cmd->arg[idx].type = type;
 	cmd->arg[idx].data = data;
@@ -755,6 +817,9 @@ void cmd_set_arg_string(struct command *cmd, const char *arg, const char *str)
 
 /**
  * Retrieve arg 'n' if a string
+ *
+ * Note that the command itself retains ownership of the pointer returned in
+ * *str:  the caller should not free that pointer.
  */
 int cmd_get_arg_string(struct command *cmd, const char *arg, const char **str)
 {
@@ -769,6 +834,9 @@ int cmd_get_arg_string(struct command *cmd, const char *arg, const char **str)
 
 /**
  * Get a string, first from the command or failing that prompt the user
+ *
+ * Note that the command itself retains ownership of the pointer returned in
+ * *str:  the caller should not free that pointer.
  */
 int cmd_get_string(struct command *cmd, const char *arg, const char **str,
 				   const char *initial, const char *title, const char *prompt)

src/cmd-core.h
@@ -245,10 +245,11 @@ struct command {
 	int nrepeats;
 
 	/*
-	 * Whether this command should be skipped when looking for CMD_REPEAT's
-	 * target.
+	 * 0: can be target for CMD_REPEAT and can trigger bloodlust
+	 * 1: can not be target for CMD_REPEAT and can trigger bloodlust
+	 * >1: can not be target for CMD_REPEAT and can not trigger bloodlust
 	 */
-	bool is_background_command;
+	int background_command;
 
 	/* Arguments */
 	struct cmd_arg arg[CMD_MAX_ARGS];
@@ -277,6 +278,9 @@ typedef void (*cmd_handler_fn)(struct command *cmd);
  * ------------------------------------------------------------------------
  * Command type functions
  * ------------------------------------------------------------------------ */
+
+void cmd_copy(struct command *dest, const struct command *src);
+void cmd_release(struct command *cmd);
 
 /* Return the verb that goes alongside the given command. */
 const char *cmd_verb(cmd_code cmd);
@@ -316,9 +320,11 @@ errr cmdq_push(cmd_code c);
 void cmdq_execute(cmd_context ctx);
 
 /**
- * Remove all commands from the queue.
+ * Remove all commands from the queue.  cmdq_release() also releases any
+ * resource allocated.
  */
 void cmdq_flush(void);
+void cmdq_release(void);
 
 /**
  * ------------------------------------------------------------------------

src/cmd-obj.c
@@ -201,7 +201,6 @@ void do_cmd_inscribe(struct command *cmd)
 		return;
 
 	obj->note = quark_add(str);
-	string_free((char *)str);
 
 	player->upkeep->notice |= (PN_COMBINE | PN_IGNORE);
 	player->upkeep->redraw |= (PR_INVEN | PR_EQUIP);

src/init.c
@@ -4534,7 +4534,7 @@ void cleanup_angband(void)
 
 	cleanup_game_constants();
 
-	cmdq_flush();
+	cmdq_release();
 
 	if (play_again) return;
 

src/obj-ignore.c
@@ -690,9 +690,10 @@ void ignore_drop(struct player *p)
 				 * This drop is a side effect:  whatever
 				 * command triggered it will be the target
 				 * for CMD_REPEAT rather than repeating the
-				 * drop.
+				 * drop, and the drop will not trigger
+				 * bloodlust.
 				 */
-				drop_cmd->is_background_command = true;
+				drop_cmd->background_command = 2;
 			}
 		}
 	}

src/player-birth.c
@@ -1214,8 +1214,6 @@ void do_cmd_choose_name(struct command *cmd)
 
 	/* Set player name */
 	my_strcpy(player->full_name, str, sizeof(player->full_name));
-
-	string_free((char *) str);
 }
 
 void do_cmd_choose_history(struct command *cmd)
@@ -1229,8 +1227,6 @@ void do_cmd_choose_history(struct command *cmd)
 	/* Get the new history */
 	cmd_get_arg_string(cmd, "history", &str);
 	player->history = string_make(str);
-
-	string_free((char *) str);
 }
 
 void do_cmd_accept_character(struct command *cmd)

src/player-path.c
@@ -1904,6 +1904,7 @@ void run_step(int dir)
 
 					disturb(player);
 					cmdq_push(CMD_OPEN);
+					cmdq_peek()->background_command = 1;
 					cmd_set_arg_direction(cmdq_peek(),
 						"direction", next_step_dir);
 					cmdq_push(CMD_PATHFIND);
@@ -1921,6 +1922,7 @@ void run_step(int dir)
 
 					disturb(player);
 					cmdq_push(CMD_TUNNEL);
+					cmdq_peek()->background_command = 1;
 					cmd_set_arg_direction(cmdq_peek(),
 						"direction", next_step_dir);
 					cmdq_push(CMD_PATHFIND);
@@ -2024,6 +2026,15 @@ void run_step(int dir)
 	/* Prepare the next step */
 	if (player->upkeep->running) {
 		cmdq_push(CMD_RUN);
+		if (player->upkeep->steps) {
+			/*
+			 * Running is a side effect of pathfinding.  Do allow
+			 * it to trigger bloodlust so pathfinding can not
+			 * be exploited as a way to move without the bloodlust
+			 * checks.
+			 */
+			cmdq_peek()->background_command = 1;
+		}
 		cmd_set_arg_direction(cmdq_peek(), "direction", 0);
 	} else if (player->upkeep->steps) {
 		mem_free(player->upkeep->steps);