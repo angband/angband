angband/angband

commit 2409d5d624149df3bd9d8b33fbb11fe679a30bfb

SDL2: direct mouse presses to menus then to starting moving/sizing
Resolves https://github.com/angband/angband/issues/6047 .

src/main-sdl2.c
@@ -3584,35 +3584,21 @@ static bool handle_mousebutton(struct my_app *a,
 		return false;
 	}
 
-	if (a->w_mouse->move_state.active || a->w_mouse->size_state.active) {
-		if (mouse->state == SDL_RELEASED) {
-			if (a->w_mouse->move_state.active
-					&& a->w_mouse->move_state.moving) {
-				a->w_mouse->move_state.moving = false;
-				return true;
-			} else if (a->w_mouse->size_state.active
-					&& a->w_mouse->size_state.sizing) {
-				a->w_mouse->size_state.sizing = false;
-				if (a->w_mouse->size_state.subwindow) {
-					resize_subwindow(a->w_mouse->size_state.subwindow);
-				}
-				return true;
-			}
+	/* Terminate moving/sizing on a mouse release. */
+	if (mouse->state == SDL_RELEASED
+			&& (a->w_mouse->move_state.moving
+			|| a->w_mouse->size_state.sizing)) {
+		if (a->w_mouse->move_state.moving) {
+			SDL_assert(a->w_mouse->move_state.active);
+			a->w_mouse->move_state.moving = false;
 		} else {
-			subwindow = get_subwindow_by_xy(a->w_mouse, mouse->x,
-				mouse->y);
-			if (subwindow && is_rect_in_rect(&subwindow->full_rect,
-					&a->w_mouse->inner_rect)) {
-				if (a->w_mouse->move_state.active
-						&& !a->w_mouse->move_state.moving) {
-					start_moving(a->w_mouse, subwindow, mouse);
-				} else if (a->w_mouse->size_state.active
-						&& !a->w_mouse->size_state.sizing) {
-					start_sizing(a->w_mouse, subwindow, mouse);
-				}
-				return true;
+			SDL_assert(a->w_mouse->size_state.active);
+			a->w_mouse->size_state.sizing = false;
+			if (a->w_mouse->size_state.subwindow) {
+				resize_subwindow(a->w_mouse->size_state.subwindow);
 			}
 		}
+		return true;
 	}
 
 	/* Have a menu or dialog handle the event if appropriate. */
@@ -3636,6 +3622,24 @@ static bool handle_mousebutton(struct my_app *a,
 		if (a->w_mouse->d_mouse->ftb->handle_mouseclick
 				&& (*a->w_mouse->d_mouse->ftb->handle_mouseclick)(
 				a->w_mouse->d_mouse, a->w_mouse, mouse)) {
+			return true;
+		}
+	}
+
+	/* If requested, start moving/sizing on a press. */
+	if (mouse->state != SDL_RELEASED
+			&& (a->w_mouse->move_state.active
+			|| a->w_mouse->size_state.active)) {
+		subwindow = get_subwindow_by_xy(a->w_mouse, mouse->x, mouse->y);
+		if (subwindow && is_rect_in_rect(&subwindow->full_rect,
+				&a->w_mouse->inner_rect)) {
+			if (a->w_mouse->move_state.active
+					&& !a->w_mouse->move_state.moving) {
+				start_moving(a->w_mouse, subwindow, mouse);
+			} else if (a->w_mouse->size_state.active
+					&& !a->w_mouse->size_state.sizing) {
+				start_sizing(a->w_mouse, subwindow, mouse);
+			}
 			return true;
 		}
 	}

commit bc63cd074619ad17ec83c6873aec77c21580929a

SDL2: improve responsiveness when moving/resizing subwindows
Resolves https://github.com/angband/angband/issues/6052 , a regression introduced by https://github.com/angband/angband/pull/6049 .

src/main-sdl2.c
@@ -936,7 +936,8 @@ static void redraw_window_while_menu_active(struct sdlpui_window *window)
 /* this function is mostly used while normally playing the game */
 static void redraw_window(struct sdlpui_window *window)
 {
-	if (window->d_mouse || window->d_key) {
+	if (window->move_state.moving || window->size_state.sizing
+			|| window->d_mouse || window->d_key) {
 		redraw_window_while_menu_active(window);
 		return;
 	}
@@ -3579,6 +3580,7 @@ static bool handle_mousebutton(struct my_app *a,
 	int button, col, row;
 	uint8_t mods;
 	term *old;
+	bool touched;
 
 	if (!a->w_mouse) {
 		return false;
@@ -3602,6 +3604,7 @@ static bool handle_mousebutton(struct my_app *a,
 	}
 
 	/* Have a menu or dialog handle the event if appropriate. */
+	touched = false;
 	if (a->w_mouse->d_mouse) {
 		/*
 		 * Press events outside of the dialog will act as if the dialog 
@@ -3617,9 +3620,8 @@ static bool handle_mousebutton(struct my_app *a,
 					a->w_mouse->d_mouse, a->w_mouse,
 					NULL, NULL);
 			}
-			return true;
-		}
-		if (a->w_mouse->d_mouse->ftb->handle_mouseclick
+			touched = true;
+		} else if (a->w_mouse->d_mouse->ftb->handle_mouseclick
 				&& (*a->w_mouse->d_mouse->ftb->handle_mouseclick)(
 				a->w_mouse->d_mouse, a->w_mouse, mouse)) {
 			return true;
@@ -3646,13 +3648,13 @@ static bool handle_mousebutton(struct my_app *a,
 
 	/* Otherwise only react to the button press and not the release. */
 	if (mouse->state == SDL_RELEASED) {
-		return false;
+		return touched;
 	}
 
 	subwindow = get_subwindow_by_xy(a->w_mouse, mouse->x, mouse->y);
 	if (subwindow == NULL) {
 		/* not an error, the user clicked in some random place */
-		return false;
+		return touched;
 	}
 	if (!subwindow->top) {
 		bring_to_top(a->w_mouse, subwindow);
@@ -3665,7 +3667,7 @@ static bool handle_mousebutton(struct my_app *a,
 	 * lives in the main window.
 	 */
 	if (a->w_mouse->index != MAIN_WINDOW) {
-		return false;
+		return touched;
 	}
 
 	/* all magic numbers are from ui-term.c and ui-context.c :) */
@@ -3678,11 +3680,11 @@ static bool handle_mousebutton(struct my_app *a,
 			break;
 		default:
 			/* XXX other buttons? */
-			return false;
+			return touched;
 	}
 
 	if (!get_colrow_from_xy(subwindow, mouse->x, mouse->y, &col, &row)) {
-		return false;
+		return touched;
 	}
 
 	mods = translate_key_mods(SDL_GetModState());