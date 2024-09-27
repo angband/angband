ed7623174938449a0c1f934fa4140aabb063064e
agoodman00 <goodman.adam@gmail.com>
2024-09-27 12:44:33 -0700

    borg: change command to revert form from m to {

--- src/borg/borg-think.c
+++ src/borg/borg-think.c
@@ -365,7 +365,9 @@ bool borg_think(void)
 
     /* Hack -- always revert shapechanged players to normal form */
     if (player_is_shapechanged(player)) {
-        borg_keypress('m');
+        /* it looks like inscribe is a good command that checks */
+        /* your form without a prerequisite check */
+        borg_keypress('{');
         borg_keypress('r');
         return true;
     }
