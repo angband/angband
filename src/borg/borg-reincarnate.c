0efe021793a4743880d97eb235a8c8145e6930a7
agoodman00 <goodman.adam@gmail.com>
2024-10-02 10:55:34 -0700

    borg: a second cave variable that should be not messed with during reincarnation

--- src/borg/borg-reincarnate.c
+++ src/borg/borg-reincarnate.c
@@ -421,8 +421,11 @@ void reincarnate_borg(void)
 
     /* save the existing dungeon.  It is cleared later but needs to */
     /* be blank when  creating the new player */
-    struct chunk *sv_cave = cave;
-    cave                  = NULL;
+    struct chunk* sv_cave = cave;
+    struct chunk* sv_player_cave = player->cave;
+
+    cave = NULL;
+    player->cave = NULL;
 
     /* Cheat death */
     borg.trait[BI_MAXDEPTH]  = 0;
@@ -582,6 +585,7 @@ void reincarnate_borg(void)
 
     /* restore the cave */
     cave = sv_cave;
+    player->cave = sv_player_cave;
 
     /* the new player is now ready */
     character_generated = true;
