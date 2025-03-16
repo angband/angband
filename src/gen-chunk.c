angband/angband

commit 5540d48fb9608dbb26cf0adff2b91cbc7f44851e

Add a chunk argument to monster deletion and moving functions

src/mon-make.c
@@ -311,13 +311,13 @@ struct monster_race *get_mon_num(int generated_level, int current_level)
  *
  * When a monster is deleted, all of its objects are deleted.
  */
-void delete_monster_idx(int m_idx)
+void delete_monster_idx(struct chunk *c, int m_idx)
 {
-	struct monster *mon = cave_monster(cave, m_idx);
+	struct monster *mon = cave_monster(c, m_idx);
 	struct loc grid;
 
 	assert(m_idx > 0);
-	assert(square_in_bounds(cave, mon->grid));
+	assert(square_in_bounds(c, mon->grid));
 	grid = mon->grid;
 
 	/* Hack -- Reduce the racial counter */
@@ -326,7 +326,7 @@ void delete_monster_idx(int m_idx)
 
 	/* Count the number of "reproducers" */
 	if (rf_has(mon->race->flags, RF_MULTIPLY)) {
-		cave->num_repro--;
+		c->num_repro--;
 	}
 
 	/* Affect light? */
@@ -347,8 +347,8 @@ void delete_monster_idx(int m_idx)
 	}
 
 	/* Monster is gone from square and group */
-	square_set_mon(cave, grid, 0);
-	monster_remove_from_groups(cave, mon);
+	square_set_mon(c, grid, 0);
+	monster_remove_from_groups(c, mon);
 
 	/* Delete objects */
 	struct object *obj = mon->held_obj;
@@ -370,42 +370,44 @@ void delete_monster_idx(int m_idx)
 			 * can skip the test and simply delist and delete
 			 * since any obj->known from a monster's inventory
 			 * will not be in a floor pile. */
-			if (loc_is_zero(obj->known->grid)) {
+			if (loc_is_zero(obj->known->grid) && (c == cave)) {
 				delist_object(player->cave, obj->known);
 				object_delete(player->cave, NULL, &obj->known);
 			}
 		}
-		delist_object(cave, obj);
-		object_delete(cave, player->cave, &obj);
+		delist_object(c, obj);
+		if (c == cave) {
+			object_delete(cave, player->cave, &obj);
+		}
 		obj = next;
 	}
 
 	/* Delete mimicked objects */
 	if (mon->mimicked_obj) {
-		square_delete_object(cave, mon->grid, mon->mimicked_obj, true, false);
+		square_delete_object(c, mon->grid, mon->mimicked_obj, true, false);
 	}
 
 	/* Wipe the Monster */
 	memset(mon, 0, sizeof(struct monster));
 
 	/* Count monsters */
-	cave->mon_cnt--;
+	c->mon_cnt--;
 
 	/* Visual update */
-	square_light_spot(cave, grid);
+	square_light_spot(c, grid);
 }
 
 
 /**
  * Deletes the monster, if any, at the given location.
  */
-void delete_monster(struct loc grid)
+void delete_monster(struct chunk *c, struct loc grid)
 {
-	assert(square_in_bounds(cave, grid));
+	assert(square_in_bounds(c, grid));
 
 	/* Delete the monster (if any) */
-	if (square(cave, grid)->mon > 0)
-		delete_monster_idx(square(cave, grid)->mon);
+	if (square(c, grid)->mon > 0)
+		delete_monster_idx(c, square(c, grid)->mon);
 }
 
 
@@ -414,7 +416,7 @@ void delete_monster(struct loc grid)
  *
  * This should only be called when there is an actual monster at i1
  */
-void monster_index_move(int i1, int i2)
+void monster_index_move(struct chunk *c, int i1, int i2)
 {
 	struct monster *mon;
 	struct object *obj;
@@ -423,19 +425,19 @@ void monster_index_move(int i1, int i2)
 	if (i1 == i2) return;
 
 	/* Old monster */
-	mon = cave_monster(cave, i1);
+	mon = cave_monster(c, i1);
 	if (!mon) return;
 
 	/* Update the cave */
-	square_set_mon(cave, mon->grid, i2);
+	square_set_mon(c, mon->grid, i2);
 
 	/* Update midx */
 	mon->midx = i2;
 
 	/* Update group */
-	if (!monster_group_change_index(cave, i2, i1)) {
+	if (!monster_group_change_index(c, i2, i1)) {
 		quit("Bad monster group info!") ;
-		monster_groups_verify(cave);
+		monster_groups_verify(c);
 	}
 
 	/* Repair objects being carried by monster */
@@ -448,19 +450,19 @@ void monster_index_move(int i1, int i2)
 
 	/* Update the target */
 	if (target_get_monster() == mon)
-		target_set_monster(cave_monster(cave, i2));
+		target_set_monster(cave_monster(c, i2));
 
 	/* Update the health bar */
 	if (player->upkeep->health_who == mon)
-		player->upkeep->health_who = cave_monster(cave, i2);
+		player->upkeep->health_who = cave_monster(c, i2);
 
 	/* Move monster */
-	memcpy(cave_monster(cave, i2),
-			cave_monster(cave, i1),
+	memcpy(cave_monster(c, i2),
+			cave_monster(c, i1),
 			sizeof(struct monster));
 
 	/* Wipe hole */
-	memset(cave_monster(cave, i1), 0, sizeof(struct monster));
+	memset(cave_monster(c, i1), 0, sizeof(struct monster));
 }
 
 
@@ -525,7 +527,7 @@ void compact_monsters(struct chunk *c, int num_to_compact)
 			if (randint0(100) < chance) continue;
 
 			/* Delete the monster */
-			delete_monster(mon->grid);
+			delete_monster(c, mon->grid);
 
 			/* Count the monster */
 			num_compacted++;
@@ -541,7 +543,7 @@ void compact_monsters(struct chunk *c, int num_to_compact)
 		if (mon->race) continue;
 
 		/* Move last monster into open hole */
-		monster_index_move(cave_monster_max(c) - 1, m_idx);
+		monster_index_move(c, cave_monster_max(c) - 1, m_idx);
 
 		/* Compress "c->mon_max" */
 		c->mon_max--;

src/cmd-wizard.c
@@ -467,7 +467,7 @@ void do_cmd_wiz_banish(struct command *cmd)
 		if (mon->cdis > d) continue;
 
 		/* Delete the monster */
-		delete_monster_idx(i);
+		delete_monster_idx(cave, i);
 	}
 
 	/* Update monster list window */

src/effect-handler-attack.c
@@ -1210,7 +1210,7 @@ bool effect_handler_DESTRUCTION(effect_handler_context_t *context)
 			if (loc_eq(grid, player->grid)) continue;
 
 			/* Delete the monster (if any) */
-			delete_monster(grid);
+			delete_monster(cave, grid);
 
 			/* Don't remove stairs */
 			if (square_isstairs(cave, grid)) continue;
@@ -1538,7 +1538,7 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 						 * Delete (not kill) "dead"
 						 * monsters.
 						 */
-						delete_monster(grid);
+						delete_monster(cave, grid);
 					} else {
 						if (display_dam) {
 							add_monster_message_show_damage(
@@ -1883,11 +1883,11 @@ bool effect_handler_SINGLE_COMBAT(effect_handler_context_t *context)
 			/* Do nothing */
 			;
 		} else if (cave_monster(cave, 1)->race) {
-			monster_index_move(old_idx, cave_monster_max(cave));
-			monster_index_move(1, old_idx);
-			monster_index_move(cave_monster_max(cave), 1);
+			monster_index_move(cave, old_idx, cave_monster_max(cave));
+			monster_index_move(cave, 1, old_idx);
+			monster_index_move(cave, cave_monster_max(cave), 1);
 		} else {
-			monster_index_move(old_idx, 1);
+			monster_index_move(cave, old_idx, 1);
 		}
 		target_set_monster(cave_monster(cave, 1));
 		player->upkeep->health_who = cave_monster(cave, 1);

src/effect-handler-general.c
@@ -2380,7 +2380,7 @@ bool effect_handler_BANISH(effect_handler_context_t *context)
 		}
 
 		/* Delete the monster */
-		delete_monster_idx(i);
+		delete_monster_idx(cave, i);
 
 		/* Take some damage */
 		dam += randint1(4);
@@ -2432,7 +2432,7 @@ bool effect_handler_MASS_BANISH(effect_handler_context_t *context)
 		if (mon->cdis > radius) continue;
 
 		/* Delete the monster */
-		delete_monster_idx(i);
+		delete_monster_idx(cave, i);
 
 		/* Take some damage */
 		dam += randint1(3);
@@ -2846,7 +2846,7 @@ bool effect_handler_TELEPORT_LEVEL(effect_handler_context_t *context)
 	if (t_mon) {
 		/* Monster is just gone */
 		add_monster_message(t_mon, MON_MSG_DISAPPEAR, false);
-		delete_monster_idx(t_mon->midx);
+		delete_monster_idx(cave, t_mon->midx);
 		return true;
 	}
 

src/load.c
@@ -346,7 +346,7 @@ static bool rd_monster(struct chunk *c, struct monster *mon)
 
 	/* Now delete the monster if necessary */
 	if (delete) {
-		delete_monster(mon->grid);
+		delete_monster(c, mon->grid);
 	}
 
 	return true;

src/mon-make.h
@@ -21,9 +21,9 @@
 
 #include "monster.h"
 
-void delete_monster_idx(int m_idx);
-void delete_monster(struct loc grid);
-void monster_index_move(int i1, int i2);
+void delete_monster_idx(struct chunk *c, int m_idx);
+void delete_monster(struct chunk *c, struct loc grid);
+void monster_index_move(struct chunk *c, int i1, int i2);
 void compact_monsters(struct chunk *c, int num_to_compact);
 void wipe_mon_list(struct chunk *c, struct player *p);
 int16_t mon_pop(struct chunk *c);

src/mon-move.c
@@ -1358,7 +1358,7 @@ static bool monster_turn_try_push(struct monster *mon, const char *m_name,
 
 		/* Monster ate another monster */
 		if (kill_ok)
-			delete_monster(new);
+			delete_monster(cave, new);
 
 		monster_swap(mon->grid, new);
 		return true;

src/mon-util.c
@@ -1128,7 +1128,7 @@ static void player_kill_monster(struct monster *mon, struct player *p,
 	}
 
 	/* Delete the monster */
-	delete_monster_idx(mon->midx);
+	delete_monster_idx(cave, mon->midx);
 }
 
 /**
@@ -1219,7 +1219,7 @@ bool mon_take_nonplayer_hit(int dam, struct monster *t_mon,
 		monster_death(t_mon, player, false);
 
 		/* Delete the monster */
-		delete_monster_idx(t_mon->midx);
+		delete_monster_idx(cave, t_mon->midx);
 		return true;
 	} else if (!monster_is_camouflaged(t_mon)) {
 		/* Give detailed messages if visible */

commit 7adf51b53c7d1f17752c448538045533ee4dbc31

Have delete_monster_idx() always call object_delete() on a carried object
Resolves a regression introduced by 5540d48fb9608dbb26cf0adff2b91cbc7f44851e which made delete_monster_idx() less similar to wipe_mon_list() and plugs a memory leak in FAangband when a skipped player ghost carries objects.  To avoid the opportunity for use after free or double frees, have chunk_copy() reset the carried or mimicked objects to NULL for a monster in the source chunk once those objects have been transferred to the destination.

src/gen-chunk.c
@@ -435,11 +435,13 @@ bool chunk_copy(struct chunk *dest, struct player *p, struct chunk *source,
 			for (obj = source_mon->held_obj; obj; obj = obj->next) {
 				obj->held_m_idx = dest_mon->midx;
 			}
+			source_mon->held_obj = NULL;
 		}
 		if (source_mon->mimicked_obj) {
 			dest_mon->mimicked_obj = source_mon->mimicked_obj;
 			dest_mon->mimicked_obj->mimicking_m_idx =
 				dest_mon->midx;
+			source_mon->mimicked_obj = NULL;
 		}
 	}
 

src/mon-make.c
@@ -376,9 +376,8 @@ void delete_monster_idx(struct chunk *c, int m_idx)
 			}
 		}
 		delist_object(c, obj);
-		if (c == cave) {
-			object_delete(cave, player->cave, &obj);
-		}
+		object_delete(c, (player && c == cave) ? player->cave : NULL,
+			&obj);
 		obj = next;
 	}
 