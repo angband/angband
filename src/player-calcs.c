40d68d25f961ca3fac2961bd1fa842266d300798
Jordan Philyaw <jordanphilyaw@gmail.com>
2025-06-24 21:48:06 -0400

    cumber_armor message clarity

--- src/player-calcs.c
+++ src/player-calcs.c
@@ -2445,9 +2445,9 @@ static void update_bonuses(struct player *p)
 		if (p->state.cumber_armor != state.cumber_armor) {
 			/* Message */
 			if (state.cumber_armor)
-				msg("The weight of your armor encumbers your movement.");
+				msg("The weight of your armor reduces your maximum SP.");
 			else
-				msg("You feel able to move more freely.");
+				msg("Your maximum SP is no longer reduced by armor weight.");
 		}
 	}
 
