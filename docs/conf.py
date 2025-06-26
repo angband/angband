473e10512564443a021bcf1ee813864623d4be6c
Jordan Philyaw <jordanphilyaw@gmail.com>
2025-06-25 21:29:26 -0400

    remove img tag from documentation browser tab title

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ if (html_theme == "".join(["@", "DOC_HTML_THEME", "@"]) or html_theme == ""):
             "headtextcolor": "rgb(253, 229, 164)",
         }
 
-html_title = "<img> The Angband Manual"
+html_title = "The Angband Manual"
 html_short_title = "Home"
 html_sidebars = {
     "**": ["localtoc.html", "searchbox.html"],
