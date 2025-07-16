d5f0f389a18c52eba0b31ed75b5ced0c78a36d6a
Eric Branlund <ebranlund@fastmail.com>
2025-07-16 10:56:19 -0600

    Resolve doxygen warnings for the projection code

--- src/project-obj.c
+++ src/project-obj.c
@@ -491,8 +491,7 @@ static const project_object_handler_f object_handlers[] = {
  *
  * \param origin is the origin of the effect
  * \param r is the distance from the centre of the effect
- * \param y the coordinates of the grid being handled
- * \param x the coordinates of the grid being handled
+ * \param grid is the coordinates of the grid being handled
  * \param dam is the "damage" from the effect at distance r from the centre
  * \param typ is the projection (PROJ_) type
  * \param protected_obj is an object that should not be affected by the
