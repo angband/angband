2eadcc2b0250b2d4c1e4b16385f6b13ad5011d98
Eric Branlund <ebranlund@fastmail.com>
2025-11-02 12:20:00 -0700

    Avoid more side effects from weapon swapping for digging/pathfinding
    Similar to https://github.com/angband/angband/pull/6371 .  The differences are:
    
    1. Affects tunneling as well as pathfinding.
    2. As long as calc_bonuses() works properly when the update argument is false, avoids more side effects (for instance, those on csp_frac).
    3. Includes a unit test case to check that hit points and mana are not affected by the weapon swapping for digging or pathfinding.

--- src/tests/player/suite.mk
+++ src/tests/player/suite.mk
@@ -1,6 +1,7 @@
 TESTPROGS += player/birth \
              player/calc-inventory \
              player/combine-pack \
+             player/digging \
              player/history \
              player/inven-carry-num \
              player/inven-wield \
